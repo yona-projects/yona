36bf4b25502dfc726260f9193a14d7a934329de1
Sangcheol Hwang <k16wire@nhn.com>
2012-06-28 19:50:05 +0900

    implementing board

---
+++ app/controllers/BoardApp.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import views.html.board.list;
+import views.html.board.newBoard;
+
+public class BoardApp extends Controller {
+	
+	public static Result boardList() {
+		return ok(list.render("Board List"));
+	}
+	
+	public static Result getNewBoard() {
+		return ok(newBoard.render("New Board"));
+	}
+
+	public static Result newBoard() {
+		return ok(newBoard.render("New Board"));
+	}
+
+}
