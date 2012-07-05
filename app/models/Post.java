bc507478eca218553a63ef4f82b5c10503e92d50
Sangcheol Hwang <k16wire@nhn.com>
2012-07-05 16:48:22 +0900

    update bootstrap link

---
+++ app/models/Post.java
@@ -0,0 +1,17 @@
+package models;
+
+import java.util.Date;
+
+import javax.persistence.Id;
+
+import play.db.ebean.Model;
+
+public class Post extends Model {
+	@Id
+	public Long id;
+	public Date date;
+	public String content;
+	public String title;
+	public int commentCount;
+	public Date modified;
+}
