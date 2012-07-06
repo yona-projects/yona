4d508fac74c33b41bd9c13ffb3d85c1f30ed1680
Hwi Ahn <hwi.ahn@gmail.com>
2012-07-06 17:24:19 +0900

    Synchronized my own git storage with nforge4java

--- app/controllers/ProjectApp.java
+++ app/controllers/ProjectApp.java
@@ -1,12 +1,37 @@
 package controllers;
 
+/*
+ * @author: Hwi Ahn
+ */
+
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.project.projectHome;
+import play.data.Form;
+
+import views.html.project.*;
+
+import models.Project;
 
 public class ProjectApp  extends Controller {
 	
+	final static Form<Project> newProjectForm = form(Project.class);
+
 	public static Result project(Long id) {
 		return ok(projectHome.render("Project Home"));
+	}
+
+	public static Result newProject(){
+		return ok(projectNewPage.render("Create a new project", newProjectForm));
+	}
+
+	public static Result getNewProject(){
+		Form<Project> filledNewProjectForm = newProjectForm.bindFromRequest();
+		if(filledNewProjectForm.hasErrors()){
+			return TODO;
+		}else{
+			return redirect(routes.ProjectApp.project(
+				Project.create(filledNewProjectForm.get()))
+			);
+		}
 	}
 }
---
+++ app/models/Project.java
@@ -0,0 +1,52 @@
+package models;
+
+/*
+ * @author: Hwi Ahn
+ */
+
+import javax.persistence.*;
+
+import play.data.validation.*;
+import play.db.ebean.*;
+
+import java.util.*;
+
+@Entity
+public class Project extends Model{
+	
+	@Id
+	public Long id;
+	
+	@Constraints.Required
+	public String name;
+	
+	public String overview; // 프로젝트 설명
+	
+	public boolean share_option; // 프로젝트 공개설정
+	
+	public String vcs; // Version Control System (vcs) 코드 관리 시스템
+
+/*
+	public Long owner; // 프로젝트 생성자 id - 나중에 연결
+	public List<Long> participants; // 프로젝트 참여자 id 리스트 - 나중에 연결
+*/
+
+	public static Finder<Long, Project> find = new Finder(Long.class, Project.class);
+	
+	public static List<String>	vcs_list(){
+		List<String> all = new ArrayList<String>();
+		all.add("GIT");
+		all.add("Subversion");
+
+		return all;
+	}
+
+	public static Long create(Project newProject){
+		newProject.save();
+		return newProject.id;
+	}
+
+	public static List<Project> all() {
+		return find.all();
+	}
+}
