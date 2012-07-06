c96de1bea4c482ec58eb0b6693c4c66400aec484
Sangcheol Hwang <k16wire@nhn.com>
2012-07-06 16:33:19 +0900

    project mng

--- app/controllers/ProjectApp.java
+++ app/controllers/ProjectApp.java
@@ -3,10 +3,15 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.project.projectHome;
+import views.html.project.setting;
 
 public class ProjectApp  extends Controller {
 	
 	public static Result project(Long id) {
 		return ok(projectHome.render("Project Home"));
+	}
+	
+	public static Result setting(Long id) {
+		return ok(setting.render("Setting"));
 	}
 }
