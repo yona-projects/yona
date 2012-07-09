d3535d5204518a22b8da46364f7b25be22962b8d
Sangcheol Hwang <k16wire@nhn.com>
2012-07-09 13:41:47 +0900

    remove redundent config

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,8 +3,6 @@
 
 # --- !Ups
 
-<<<<<<< HEAD
-=======
 create table article (
   article_num               integer not null,
   title                     varchar(255),
@@ -14,7 +12,6 @@ create table article (
   constraint pk_article primary key (article_num))
 ;
 
->>>>>>> 1a613652d20f431948c57a982b989228bed72b30
 create table issue (
   id                        bigint not null,
   title                     varchar(255),
@@ -22,10 +19,15 @@ create table issue (
   constraint pk_issue primary key (id))
 ;
 
-<<<<<<< HEAD
-create sequence issue_seq;
+create table project (
+  id                        bigint not null,
+  name                      varchar(255),
+  overview                  varchar(255),
+  share_option              boolean,
+  vcs                       varchar(255),
+  constraint pk_project primary key (id))
+;
 
-=======
 create table reply (
   reply_num                 integer not null,
   article_num               integer,
@@ -39,9 +41,10 @@ create sequence article_seq;
 
 create sequence issue_seq;
 
+create sequence project_seq;
+
 create sequence reply_seq;
 
->>>>>>> 1a613652d20f431948c57a982b989228bed72b30
 
 
 
@@ -49,17 +52,11 @@ create sequence reply_seq;
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
-<<<<<<< HEAD
-drop table if exists issue;
-
-SET REFERENTIAL_INTEGRITY TRUE;
-
-drop sequence if exists issue_seq;
-
-=======
 drop table if exists article;
 
 drop table if exists issue;
+
+drop table if exists project;
 
 drop table if exists reply;
 
@@ -69,6 +66,7 @@ drop sequence if exists article_seq;
 
 drop sequence if exists issue_seq;
 
+drop sequence if exists project_seq;
+
 drop sequence if exists reply_seq;
 
->>>>>>> 1a613652d20f431948c57a982b989228bed72b30
