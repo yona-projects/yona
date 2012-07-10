a446686b04ff1d7f7c2b5fd4ad6f0f386c33d84d
Hwi Ahn <hwi.ahn@gmail.com>
2012-07-10 15:41:39 +0900

    Made 'updating project' except logo

--- test/models/ProjectTest.java
+++ test/models/ProjectTest.java
@@ -25,24 +25,46 @@ public class ProjectTest {
 
 	@Test
 	public void testCreate() {
-		 running(fakeApplication(), new Runnable() {
-			public void run() {
-				Project prj = new Project();
-				prj.name = "prj_test";
-				prj.overview = "Overview for prj_test";
-				prj.share_option = false;
-				prj.vcs = "GIT";
-				Project.create(prj);
-				
-				Project actualProject = Project.findById(prj.id);
-				
-				assertEquals("prj_test", actualProject.name);
-		        assertEquals("Overview for prj_test", actualProject.overview);
-		        assertEquals(false, actualProject.share_option);
-		        assertEquals("GIT", actualProject.vcs);
-		        assertEquals("http://localhost:9000/project/" + Long.toString(actualProject.id), actualProject.url);
-			}
-		});
+		Project prj = new Project();
+		prj.name = "prj_test";
+		prj.overview = "Overview for prj_test";
+		prj.share_option = false;
+		prj.vcs = "GIT";
+		Project.create(prj);
+		
+		Project actualProject = Project.findById(prj.id);
+		
+		assertEquals("prj_test", actualProject.name);
+        assertEquals("Overview for prj_test", actualProject.overview);
+        assertEquals(false, actualProject.share_option);
+        assertEquals("GIT", actualProject.vcs);
+        assertEquals("http://localhost:9000/project/" + Long.toString(actualProject.id), actualProject.url);
+	}
+	
+	@Test
+	public void testUpdate(){
+		Project prj1 = new Project();
+		
+		prj1.name = "prj_test";
+		prj1.overview = "Overview for prj_test";
+		prj1.share_option = false;
+		prj1.vcs = "GIT";
+		Long id = Project.create(prj1);
+		
+		Project prj2 = new Project();
+		
+		prj2.name = "prj";
+		prj2.overview = "Overview for prj_test";
+		prj2.share_option = false;
+		prj2.vcs = "GIT";
+		
+		
+		Project.update(prj2, id);
+		
+		Project actualProject = Project.findById(id);
+		
+		assertEquals("prj", actualProject.name);
+		
 	}
 	
 	@AfterClass
