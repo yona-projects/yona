e6173cd7e07f7861900fb5394883cfb8f5b6c49b
Hwi Ahn <hwi.ahn@gmail.com>
2012-07-11 14:17:04 +0900

    Modified tab->spaces, Deleting a project doesn't work now.

--- app/controllers/ProjectApp.java
+++ app/controllers/ProjectApp.java
@@ -6,7 +6,6 @@ package controllers;
  * 
  */
 
-
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.data.*;
@@ -15,51 +14,71 @@ import views.html.project.*;
 
 import models.Project;
 
-public class ProjectApp  extends Controller {
-	
-	final static Form<Project> newProjectForm = form(Project.class);
+public class ProjectApp extends Controller {
 
-	public static Result project(Long id) {
-		return ok(projectHome.render("Project Home"));
-	}
+    final static Form<Project> newProjectForm = form(Project.class);
 
-	public static Result newProject() {
-		return ok(newProject.render("Create a new project", newProjectForm));
-	}
+    public static Result project(Long id) {
+        return ok(projectHome.render("Project Home"));
+    }
 
-	public static Result setting(Long id) {
-		Form<Project> projectForm = form(Project.class).fill(Project.findById(id));
-		return ok(setting.render("Setting", projectForm, id));
-	}
+    public static Result newProject() {
+        return ok(newProject.render("Create a new project", newProjectForm));
+    }
 
-	public static Result saveProject() {
-		Form<Project> filledNewProjectForm = newProjectForm.bindFromRequest();
-		
-		if(!"true".equals(filledNewProjectForm.field("accept").value())) {
-			filledNewProjectForm.reject("accept", "반드시 이용 약관에 동의하여야 합니다.");
-	    }
-		
-		if(filledNewProjectForm.hasErrors()) {
-			return badRequest(newProject.render("Create a new project", filledNewProjectForm));
-		}else {
-			return redirect(routes.ProjectApp.project(
-				Project.create(filledNewProjectForm.get()))
-			);
-		}
-	}
-	
-	public static Result saveSetting(Long id) {
-		Form<Project> filledUpdatedProjectForm = newProjectForm.bindFromRequest();
-		
-		if(!filledUpdatedProjectForm.field("url").value().startsWith("http://")) {
-			filledUpdatedProjectForm.reject("url", "사이트 URL은 http://로 시작하여야 합니다.");
-		}
-		
-		if(filledUpdatedProjectForm.hasErrors()) {
-			return badRequest(setting.render("Setting", filledUpdatedProjectForm, id));
-		}else {
-			return redirect(routes.ProjectApp.setting(Project.update(filledUpdatedProjectForm.get(), id)));
-		}
-	}
-	
+    public static Result setting(Long id) {
+        Form<Project> projectForm = form(Project.class).fill(
+                Project.findById(id));
+        return ok(setting.render("Setting", projectForm, id));
+    }
+
+    public static Result saveProject() {
+        Form<Project> filledNewProjectForm = newProjectForm.bindFromRequest();
+
+        if (!"true".equals(filledNewProjectForm.field("accept").value())) {
+            filledNewProjectForm.reject("accept", "반드시 이용 약관에 동의하여야 합니다.");
+        }
+
+        if (filledNewProjectForm.hasErrors()) {
+            return badRequest(newProject.render("Create a new project",
+                    filledNewProjectForm));
+        } else {
+            return redirect(routes.ProjectApp.project(Project
+                    .create(filledNewProjectForm.get())));
+        }
+    }
+
+    public static Result saveSetting(Long id) {
+        Form<Project> filledUpdatedProjectForm = newProjectForm
+                .bindFromRequest();
+
+        if (!filledUpdatedProjectForm.field("url").value()
+                .startsWith("http://")) {
+            filledUpdatedProjectForm.reject("url",
+                    "사이트 URL은 http://로 시작하여야 합니다.");
+        }
+
+        if (filledUpdatedProjectForm.hasErrors()) {
+            return badRequest(setting.render("Setting",
+                    filledUpdatedProjectForm, id));
+        } else {
+            return redirect(routes.ProjectApp.setting(Project.update(
+                    filledUpdatedProjectForm.get(), id)));
+        }
+    }
+
+    public static Result deleteProject(Long id) {
+        Form<Project> deletedProject = form(Project.class).bindFromRequest();
+
+        if (!"true".equals(deletedProject.field("acceptDeletion").value())) {
+            deletedProject.reject("acceptDeletion", "프로젝트 삭제에 동의하여야 합니다.");
+        }
+
+        if (deletedProject.hasErrors()) {
+            return badRequest(setting.render("Setting", deletedProject, id));
+        } else {
+            Project.delete(deletedProject.get());
+            return ok(views.html.index.render("Your new application is ready."));
+        }
+    }
 }
--- test/models/ProjectTest.java
+++ test/models/ProjectTest.java
@@ -5,7 +5,6 @@ package models;
  * 
  */
 
-
 import static org.junit.Assert.*;
 
 import org.junit.*;
@@ -14,50 +13,52 @@ import static play.test.Helpers.*;
 import play.test.*;
 import static org.fest.assertions.Assertions.*;
 
-public class ProjectTest extends ModelTest{
-	
-	@Test
-	public void testCreate() {
-		Project prj = new Project();
-		prj.name = "prj_test";
-		prj.overview = "Overview for prj_test";
-		prj.share_option = false;
-		prj.vcs = "GIT";
-		Project.create(prj);
-		
-		Project actualProject = Project.findById(prj.id);
-		
-		assertEquals("prj_test", actualProject.name);
+public class ProjectTest extends ModelTest {
+
+    @Test
+    public void testCreate() {
+        Project prj = new Project();
+        prj.name = "prj_test";
+        prj.overview = "Overview for prj_test";
+        prj.share_option = false;
+        prj.vcs = "GIT";
+        Project.create(prj);
+
+        Project actualProject = Project.findById(prj.id);
+
+        assertEquals("prj_test", actualProject.name);
         assertEquals("Overview for prj_test", actualProject.overview);
         assertEquals(false, actualProject.share_option);
         assertEquals("GIT", actualProject.vcs);
-        assertEquals("http://localhost:9000/project/" + Long.toString(actualProject.id), actualProject.url);
-	}
-	
-	@Test
-	public void testUpdate(){
-	    Long id = new Long(1);
-	    Project prj = new Project();
-	    prj.name = "modifiedProjectName";
-	    		
-		Project.update(prj, id);
-		
-		Project actualProject = Project.findById(id);
-		
-		assertEquals("modifiedProjectName", actualProject.name);
-		assertEquals("첫번째 프로젝트입니다.", actualProject.overview);
-		assertEquals("false", Boolean.toString(actualProject.share_option));
-		assertEquals("GIT", actualProject.vcs);
-		assertEquals("http://localhost:9000/project/1", actualProject.url);
-		assertEquals("1", Long.toString(actualProject.owner));
-	}
-	
-	@Test
-	public void testDelete(){
-		Long id = new Long(1);
-		Project prj = Project.findById(id);
-		Project.delete(prj);
-		
-		assertEquals(null, Project.findById(id));
-	}
+        assertEquals(
+                "http://localhost:9000/project/"
+                        + Long.toString(actualProject.id), actualProject.url);
+    }
+
+    @Test
+    public void testUpdate() {
+        Long id = new Long(1);
+        Project prj = new Project();
+        prj.name = "modifiedProjectName";
+
+        Project.update(prj, id);
+
+        Project actualProject = Project.findById(id);
+
+        assertEquals("modifiedProjectName", actualProject.name);
+        assertEquals("첫번째 프로젝트입니다.", actualProject.overview);
+        assertEquals("false", Boolean.toString(actualProject.share_option));
+        assertEquals("GIT", actualProject.vcs);
+        assertEquals("http://localhost:9000/project/1", actualProject.url);
+        assertEquals("1", Long.toString(actualProject.owner));
+    }
+
+    @Test
+    public void testDelete() {
+        Long id = new Long(1);
+        Project prj = Project.findById(id);
+        Project.delete(prj);
+
+        assertEquals(null, Project.findById(id));
+    }
 }
