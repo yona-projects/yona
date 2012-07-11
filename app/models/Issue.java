59513505151215775268ac69bf7956ed44432b14
Tae <dana.parkth@gmail.com>
2012-07-11 16:46:02 +0900

    Refactoring Issue model

--- app/models/Issue.java
+++ app/models/Issue.java
@@ -16,30 +16,6 @@ import play.db.ebean.Model;
 public class Issue extends Model {
     private static final long serialVersionUID = 1L;
 
-    @Id
-    public Long id;
-    public Long userId; // 글쓴이
-
-    @Constraints.Required
-    public String title; // 제목
-    @Constraints.Required
-    public String body; // 글 내용
-    @Constraints.Required
-    public int status; // 이슈 상태
-    @Formats.DateTime(pattern = "YYYY/MM/DD/hh/mm/ss")
-    public Date date; // 이슈 작성일
-    // 세부정보
-    public int issueType; // 이슈유형
-    public User reposibleMemberId; // 담당자
-    public String comp; // 컴포넌트
-    public Milestone milestone; // 적용된 마일스톤
-    public int importance;// 중요도
-    public int diagnosisType;// 진단유형
-
-    // public int replyNum;
-
-    // public String filePath;
-
     public static final int STATUS_ENROLLED = 1; // 등록
     public static final int STATUS_ASSINGED = 2; // 진행중
     public static final int STATUS_SOLVED = 3; // 해결
@@ -50,7 +26,42 @@ public class Issue extends Model {
     public static final int issueType_2 = 2; // 진행중
     public static final int issueType_3 = 3; // 해결
     public static final int issueType_4 = 4; // 닫힘
+    
+    @Id
+    public Long id;
+    public Long userId; // 글쓴이
+    @Constraints.Required
+    public String title; // 제목
+    @Constraints.Required
+    public String body; // 글 내용
+    @Constraints.Required
+    public int status; // 이슈 상태
+    @Formats.DateTime(pattern = "YYYY/MM/DD/hh/mm/ss")
+    public Date date; // 이슈 작성일
+    // 세부정보
+    public int issueType; // 이슈유형
+    public User responsibleMemberId; // 담당자
+    public String comp; // 컴포넌트
+    public Milestone milestone; // 적용된 마일스톤
+    public int importance;// 중요도
+    public int diagnosisType;// 진단유형
+    public int commentCount;
+    //TODO 첨부 파일이 여러개인경우는?
+    public String filePath;
 
+    public String status() {
+        if (this.status == STATUS_ENROLLED) {
+            return "등록";
+        } else if (this.status == STATUS_ASSINGED) {
+            return "진행중";
+        } else if (this.status == STATUS_SOLVED) {
+            return "해결";
+        } else if (this.status == STATUS_CLOSED) {
+            return "닫힘";
+        } else
+            return "등록";
+    }
+    
     private static Finder<Long, Issue> find = new Finder<Long, Issue>(
             Long.class, Issue.class);
 
@@ -71,28 +82,7 @@ public class Issue extends Model {
         return find.byId(id);
     }
 
-    public static List<Issue> findByTitle(String word) {
-        return find.where().contains("title", word).findList();
+    public static List<Issue> findByTitle(String keyword) {
+        return find.where().contains("title", keyword).findList();
     }
-
-    // TODO_추후에 Reply4Issue 모델에 추가하면서 기능 추가할것
-    public static void replyAdd(int issueId) {
-
-    }
-
-    public String printStatus() {
-
-        if (this.status == STATUS_ENROLLED) {
-            return "등록";
-        } else if (this.status == STATUS_ASSINGED) {
-            return "진행중";
-        } else if (this.status == STATUS_SOLVED) {
-            return "해결";
-        } else if (this.status == STATUS_CLOSED) {
-            return "닫힘";
-        } else
-            return "등록";
-    }
-    
-
 }
--- test/models/IssueTest.java
+++ test/models/IssueTest.java
@@ -6,58 +6,54 @@ import java.util.List;
 
 import org.junit.Test;
 
+import utils.JodaDateUtil;
+
 public class IssueTest extends ModelTest {
 
     @Test
     public void create() {
+        // Given
         Issue issue = new Issue();
         issue.title = "불필요한 로그 출력 코드 제거";
-        // issue.date = "";
+        issue.date = JodaDateUtil.today();
         issue.status = Issue.STATUS_ENROLLED;
         issue.userId = 1l;
+        // When
+        // Then
         assertThat(Issue.create(issue)).isEqualTo(5l);
     }
 
     @Test
     public void findById() throws Exception {
+        // Given
         Issue issue = new Issue();
         issue.title = "불필요한 로그 출력 코드 제거";
-        // issue.date = "";
+        issue.date = JodaDateUtil.today();
         issue.status = Issue.STATUS_ENROLLED;
         issue.userId = 1l;
-
         Long id = Issue.create(issue);
-
+        // When
         Issue issueTest = Issue.findById(id);
+        // Then
         assertThat(issueTest).isNotNull();
     }
 
     @Test
     public void delete() {
-        Issue issue = new Issue();
-        issue.title = "지우기 테스트";
-        // issue.date = "";
-        issue.status = Issue.STATUS_ENROLLED;
-        issue.userId = 2l;
-
-        Issue.create(issue);
-        Issue.delete(5l);
-
-        assertThat(Issue.findById(5l)).isNull();
+        // Given
+        // When
+        Issue.delete(4l);
+        // Then
+        assertThat(Issue.findById(4l)).isNull();
     }
 
     @Test
     public void findByTitle() {
-        Issue issue = new Issue();
-        issue.title = "findByTitle check";
-        // issue.date = "";
-        issue.status = Issue.STATUS_CLOSED;
-        issue.userId = 4l;
-
-        Long id = Issue.create(issue);
-
-        List<Issue> issueTest = Issue.findByTitle("check");
-        assertThat(issueTest).hasSize(1);
+        // Given
+        // When
+        List<Issue> issueTest = Issue.findByTitle("git");
+        // Then
+        assertThat(issueTest).hasSize(2);
     }
 
 }
