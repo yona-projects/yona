161585c7baeda349bab24790498f821b9959e1f0
Yoon <helolsajava@gmail.com>
2012-07-11 21:01:16 -0700

    Applied the coding style.

--- app/models/Milestone.java
+++ app/models/Milestone.java
@@ -14,45 +14,43 @@ import java.util.List;
  */
 @Entity
 public class Milestone extends Model {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
+    private static Finder<Long, Milestone> find = new Finder<Long, Milestone>(
+            Long.class, Milestone.class);
+    @Id
+    public Long id;
+    @Constraints.Required
+    public String versionName;
+    @Constraints.Required
+    @Formats.DateTime(pattern = "yyyy-MM-dd HH:mm:ss")
+    public Date dueDate;
+    @Constraints.Required
+    public String contents;
+    public Long projectId;
+    public int numClosedIssues;
+    public int numOpenIssues;
 
-	@Id
-	public Long id;
-	@Constraints.Required
-	public String versionName;
-	@Constraints.Required
-	@Formats.DateTime(pattern = "yyyy-MM-dd HH:mm:ss")
-	public Date dueDate;
-	@Constraints.Required
-	public String contents;
-	public Long projectId;
-	public int numClosedIssues;
-	public int numOpenIssues;
+    public static List<Milestone> findOnePage(int pageNum) {
+        return find.findPagingList(10).getPage(pageNum - 1).getList();
+    }
 
-	public static Finder<Long, Milestone> find = new Finder<Long, Milestone>(
-			Long.class, Milestone.class);
+    public static void create(Milestone milestone) {
+        milestone.save();
+    }
 
-	public static List<Milestone> findOnePage(int pageNum) {
-		return find.findPagingList(10).getPage(pageNum - 1).getList();
-	}
+    public static void update(Milestone milestone, Long id) {
+        milestone.update(id);
+    }
 
-	public static void write(Milestone milestone) {
-		milestone.save();
-	}
+    public static void delete(Long id) {
+        find.ref(id).delete();
+    }
 
-	public static void update(Milestone milestone, Long id) {
-		milestone.update(id);
-	}
+    public static Milestone findById(Long id) {
+        return find.byId(id);
+    }
 
-	public static void delete(Long id) {
-		find.ref(id).delete();
-	}
-
-	public static Milestone findById(Long id) {
-		return find.byId(id);
-	}
-
-	public static List<Milestone> findByProjectId(Long projectId) {
-		return find.where().eq("projectId", projectId).findList();
-	}
+    public static List<Milestone> findByProjectId(Long projectId) {
+        return find.where().eq("projectId", projectId).findList();
+    }
 }
