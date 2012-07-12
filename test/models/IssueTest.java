066ef426d63c91954c9d00394993e4a51212560e
Tae <dana.parkth@gmail.com>
2012-07-12 16:36:39 +0900

    Add tab functionality and test code for issue#2

--- test/models/IssueTest.java
+++ test/models/IssueTest.java
@@ -66,7 +66,7 @@ public class IssueTest extends ModelTest {
         // Then
         assertThat(issueTest.getTotalRowCount()).isEqualTo(2);
     }
-    
+
     @Test
     public void findOnlyClosedIssues() {
         // Given
