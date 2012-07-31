6d5e2d7fbe5b2de6610bcb06939aaa9a16a6cfd7
Sangcheol Hwang <k16wire@nhn.com>
2012-07-31 14:45:32 +0900

    initial commit

---
+++ app/utils/Constants.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class Constants {
+	public static final String WARNING = "warning";
+	public static final String INFO = "info";
+	public static final String SUCCESS = "success";
+}
