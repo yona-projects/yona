6b6fefe3968c06ae6bbab48eb67e87a4d4661103
Hwi Ahn <hwi.ahn@gmail.com>
2012-08-02 16:02:22 +0900

    refactor view code and titles

--- app/controllers/ProjectApp.java
+++ app/controllers/ProjectApp.java
@@ -27,24 +27,20 @@ import java.util.List;
  * @author "Hwi Ahn"
  */
 public class ProjectApp extends Controller {
-    public static final String PROJECT_HOME = "프로젝트 홈";
-    public static final String NEW_PROJECT = "새 프로젝트 생성";
-    public static final String SETTING = "프로젝트 설정";
-    public static final String MEMBER_LIST = "맴버";
 
     public static Result project(String projectName) {
-        return ok(projectHome.render(PROJECT_HOME,
+        return ok(projectHome.render("title.projectHome",
                 Project.findByName(projectName)));
     }
 
     public static Result newProject() {
-        return ok(newProject.render(NEW_PROJECT, form(Project.class)));
+        return ok(newProject.render("title.newProject", form(Project.class)));
     }
 
     public static Result setting(String projectName) {
         Form<Project> projectForm = form(Project.class).fill(
                 Project.findByName(projectName));
-        return ok(setting.render(SETTING, projectForm,
+        return ok(setting.render("title.projectSetting", projectForm,
                 Project.findByName(projectName)));
     }
 
@@ -52,12 +48,8 @@ public class ProjectApp extends Controller {
         Form<Project> filledNewProjectForm = form(Project.class)
                 .bindFromRequest();
 
-        if (!"true".equals(filledNewProjectForm.field("accept").value())) {
-            filledNewProjectForm.reject("accept", "반드시 이용 약관에 동의하여야 합니다.");
-        }
-
         if (filledNewProjectForm.hasErrors()) {
-            return badRequest(newProject.render(NEW_PROJECT,
+            return badRequest(newProject.render("title.newProject",
                     filledNewProjectForm));
         } else {
             Project project = filledNewProjectForm.get();
@@ -85,17 +77,11 @@ public class ProjectApp extends Controller {
         Project project = filledUpdatedProjectForm.get();
         MultipartFormData body = request().body().asMultipartFormData();
         FilePart filePart = body.getFile("logoPath");
-
-        if (!filledUpdatedProjectForm.field("url").value()
-                .startsWith("http://")) {
-            filledUpdatedProjectForm.reject("url",
-                    "사이트 URL은 http://로 시작하여야 합니다.");
-        }
-
+        
         if (filePart != null) {
             if (filePart.getFile().length() > 1048576) {
-                filledUpdatedProjectForm.reject("logoPath",
-                        "이미지 용량은 1MB 이하여야 합니다.");
+                flash(Constants.WARNING, "project.logo.fileSizeAlert");
+                filledUpdatedProjectForm.reject("logoPath");
             } else {
                 String string = filePart.getFilename();
                 string = string.substring(string.lastIndexOf("."));
@@ -110,7 +96,7 @@ public class ProjectApp extends Controller {
         }
 
         if (filledUpdatedProjectForm.hasErrors()) {
-            return badRequest(setting.render(SETTING, filledUpdatedProjectForm,
+            return badRequest(setting.render("title.projectSetting", filledUpdatedProjectForm,
                     Project.findByName(projectName)));
         } else {
             return redirect(routes.ProjectApp.setting(Project.update(project,
@@ -130,7 +116,7 @@ public class ProjectApp extends Controller {
         for (User user : users) {
             usersList.add(form(User.class).fill(user));
         }
-        return ok(memberList.render(MEMBER_LIST, usersList, project,
+        return ok(memberList.render("title.memberList", usersList, project,
                 Role.getAllProjectRoles()));
     }
 
