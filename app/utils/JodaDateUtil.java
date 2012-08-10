77e41923dfe0b76bf6b2a82dc30642748a333c19
Ahn Hyeok Jun <qa22ahj@gmail.com>
2012-08-10 14:28:05 +0900

    fix always show 'Just Now' in board

--- app/utils/JodaDateUtil.java
+++ app/utils/JodaDateUtil.java
@@ -16,11 +16,11 @@ public class JodaDateUtil {
     }
 
     public static Duration ago(DateTime time) {
-        return new Duration(DateTime.now(), time);
+        return new Duration(time, DateTime.now());
     }
     
     public static Duration ago(Date time) {
-        return new Duration(DateTime.now(), new DateTime(time));
+        return new Duration(new DateTime(time), DateTime.now());
     }
 
 }
