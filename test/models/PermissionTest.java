096ffae0dc088b1b248b2d26a001659ed6cff7f3
Hwi Ahn <byron1st@naver.com>
2012-08-10 19:17:26 +0900

    fix errors in test

--- test/models/PermissionTest.java
+++ test/models/PermissionTest.java
@@ -12,8 +12,8 @@ public class PermissionTest extends ModelTest<Permission> {
         // Given
         // When
         // Then
-        assertThat(Permission.permissionCheck(2l, 1l, PermissionResource.PROJECT.resource(), PermissionOperation.WRITE.operation())).isEqualTo(true);
-        assertThat(Permission.permissionCheck(2l, 2l, PermissionResource.PROJECT.resource(), PermissionOperation.WRITE.operation())).isEqualTo(false);
+        assertThat(Permission.permissionCheck(2l, 1l, PermissionResource.PROJECT, PermissionOperation.WRITE)).isEqualTo(true);
+        assertThat(Permission.permissionCheck(2l, 2l, PermissionResource.PROJECT, PermissionOperation.WRITE)).isEqualTo(false);
     }
     
     @Test
