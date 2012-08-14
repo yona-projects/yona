90c430e1476482454491189312e6507f51210191
Yoon <helolsjava@gmail.com>
2012-08-14 04:22:32 -0700

    Add js example.

--- public/javascripts/nforge.js
+++ public/javascripts/nforge.js
@@ -1,5 +1,6 @@
 var NForge = function () {
     var proto,
+      PERIOD = '.',
       nforge = this;
     proto = {
       init : function () {
@@ -37,6 +38,70 @@ var NForge = function () {
           }
         }
         return obj;
+      },
+
+      require : function () {
+        var args = Array.prototype.slice.call(arguments),
+          modules = args.shift(),
+          _modules,
+          defaultInitFn = 'init',
+          loadFn = function(module) {
+            module = module.replace('nforge.','');
+            if (module.indexOf(PERIOD) < 0) {
+              return nforge;
+            }
+            _modules = module.split(PERIOD);
+            var _tObj = nforge._objectDeepProp('get', module);
+            if (typeof _tObj === 'object') {
+              return _tObj;
+            } else if (typeof _tObj === 'undefined') {
+              var mLength = _modules.length - 1;
+              defaultInitFn = _modules[mLength];
+              module = _modules.slice(0, mLength).join('.');
+            }
+            return commandFn(module);
+          },
+          commandFn = function (module) {
+            var _obj = nforge._objectDeepProp('get', module);
+            if (typeof _obj === 'function') {
+              _obj = new _obj();
+              if (_obj.hasOwnProperty(defaultInitFn)) {
+                _obj = _obj[defaultInitFn].apply(nforge, args);
+                nforge._objectDeepProp('add', module, _obj);
+                return _obj;
+              }
+            }
+          };
+        if (!$.isArray(modules)) {
+          return loadFn(modules);
+        }else{
+          $.each(modules, function (idx, module) {
+            loadFn(module);
+          });
+          return this;
+        }
+      },
+
+      _objectDeepProp : function (act, name) {
+        var target = nforge,
+          _name = name;
+
+        if (name.indexOf(PERIOD) > 0) {
+          var i,
+            isNoThere = true;
+          names = name.split(PERIOD);
+          for (i = 0; i < names.length - 1; i++) {
+            if (target.hasOwnProperty([names[i]])) {
+              target = target[names[i]];
+              isNoThere = false;
+            }
+          }
+          if (isNoThere) {
+            return false;
+          }
+          _name = names[names.length - 1];
+        }
+        return (act === 'check' ? target.hasOwnProperty(_name) : (act === 'get' ? target[_name] || undefined : (target[_name] = arguments[2])));
       },
 
       stopEvent : function (e) {
