12c41c27cbb505da6e155eb9fa1fe23ff1f9f7b0
Yoon <helolsajava@gmail.com>
2012-08-16 01:21:41 -0700

    Add content search for projcet.

---
+++ app/controllers/SearchApp.java
@@ -0,0 +1,41 @@
+package controllers;
+
+import com.avaje.ebean.Page;
+import models.Issue;
+import models.Post;
+import models.Project;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.search.contentsSearch;
+
+public class SearchApp extends Controller {
+
+	public static class ContentSearchCondition {
+
+		public String filter;
+		public int page;
+		public int pageSize;
+
+		public ContentSearchCondition() {
+			this.filter = "";
+			this.page = 1;
+			this.pageSize = 10;
+		}
+	}
+
+	public static Result contentsSearch(String userName, String projectName) {
+		Project project = ProjectApp.getProject(userName, projectName);
+
+		if (project == null) {
+			notFound();
+		}
+
+		ContentSearchCondition condition = form(ContentSearchCondition.class).bindFromRequest().get();
+
+		Page<Issue> resultIssues = Issue.findIssues(project, condition);
+		Page<Post> resultPosts = Post.findPosts(project, condition);
+
+
+		return ok(contentsSearch.render("title.contentSearchResult", project, resultIssues, resultPosts));
+	}
+}
