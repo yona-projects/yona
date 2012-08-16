0ae964928802e7e0596cdab3e52d91d5d99f7b7b
Ahn Hyeok Jun <qa22ahj@gmail.com>
2012-08-16 15:33:44 +0900

    delete unuse view & js modularity

---
+++ public/javascripts/modules/board.js
@@ -0,0 +1,54 @@
+nforge.namespace("board");
+nforge.board.list = function() {
+	var that = {
+		init : function() {
+			that.setUpEventListener();
+		},
+		setUpEventListener : function() {
+			var $headers = $("th a");
+			$headers.click(that.onHeader);
+			var $pagination = $("pagination a");
+			$pagination.click(that.onPager);
+		},
+		onHeader : function() {
+			var key = $(this).attr("key");
+			var $input = $("#search-form input[name=key]");
+			if (key !== $input.val()) {
+				$input.val(key)
+			} else {
+				$input = $("#search-form input[name=order]");
+				if ($input.val() === "desc")
+					$input.val("asc");
+				else if ($input.val() === "asc")
+					$input.val("desc");
+			}
+			$("#search-form").submit();
+			return false;
+		},
+		onPager : function() {
+			var $input = $("#search-form input[name=pageNum]");
+			$input.val($(this).attr("pageNum"));
+			$("#search-form").submit();
+			return false;
+		}
+	};
+	return that;
+};
+nforge.board.vaildate = function() {
+	var that = {
+		init : function(){
+			$("form").submit(function(){
+		        if($("input#title").val() == "" || $("textarea#contents").val() == "")
+		        {
+		            $('#chk_vaildate').modal()
+		            return false;
+		        }
+		        else
+		        {
+		        	return true;
+		        }
+		    });
+		}
+	};
+	return that;
+}
