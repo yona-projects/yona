2ef255a24d611b4b50a844aa8531ea7cb8be3ca4
unknown <park3251@naver.com>
2012-08-24 14:00:44 +0900

    add null operation for anonymous in issue view

--- app/models/ProjectUser.java
+++ app/models/ProjectUser.java
@@ -128,6 +128,7 @@ public class ProjectUser extends Model {
      * @return
      */
     public static boolean isMember(Long userId, Long projectId) {
+        if(userId == null) return false;
         int findRowCount = find.where().eq("user.id", userId)
                 .eq("project.id", projectId).findRowCount();
         return (findRowCount != 0) ? true : false;
