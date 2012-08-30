af78fd70d3f37b34d429c86225638c21769bc875
Ahn Hyeok Jun <qa22ahj@gmail.com>
2012-08-30 11:41:36 +0900

    refactor root page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,12 +1,16 @@
 package controllers;
 
+import java.util.List;
+
+import models.Project;
 import play.mvc.*;
 import views.html.index;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        List<Project> projects = Project.findProjectsByMember(Long.parseLong(session().get("userId")));
+        return ok(index.render(projects));
     }
 
 }
