f978173c6c3d77db5226ca44c76c469a9b38ef59
Yi EungJun <semtlenori@gmail.com>
2012-09-03 12:17:52 +0900

    config: Return default if fail to load config.

--- app/utils/Config.java
+++ app/utils/Config.java
@@ -4,8 +4,13 @@ import org.apache.commons.lang.StringUtils;
 
 public class Config {
 
-
     public static String getHostport(String defaultValue) {
+        play.Configuration config = play.Configuration.root();
+
+        if (config == null) {
+            return defaultValue;
+        }
+
         String hostname = play.Configuration.root().getString("application.hostname");
 
         if (hostname != null && !hostname.isEmpty()) {
@@ -22,7 +27,13 @@ public class Config {
     }
 
     public static String getScheme(String defaultValue) {
-        String scheme = play.Configuration.root().getString("application.scheme");
+        play.Configuration config = play.Configuration.root();
+
+        if (config == null) {
+            return defaultValue;
+        }
+
+        String scheme = config.getString("application.scheme");
 
         if (scheme == null || scheme.isEmpty()) {
             return defaultValue;
