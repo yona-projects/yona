17df73a63fc91e775f82f20cdc5df8951690f823
Yi EungJun <semtlenori@gmail.com>
2012-09-05 18:56:54 +0900

    utils:  Add BasicAuthAction.
    BasicAuthAction is an action for HTTP Basic Authentication and it will
    be used to authenticate users to access Git or SVN repository.

---
+++ app/utils/BasicAuthAction.java
@@ -0,0 +1,91 @@
+package utils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import controllers.UserApp;
+import models.User;
+import play.mvc.Action;
+import play.mvc.Http;
+import play.mvc.Http.Context;
+import play.mvc.Result;
+
+public class BasicAuthAction extends Action<Object> {
+    private static final String REALM = "nforge4";
+    
+    public static Result unauthorized(Context context) {
+        // challenge   = "Basic" realm
+        // realm       = "realm" "=" realm-value
+        // realm-value = quoted-string
+
+        String challenge = "Basic realm=\"" + REALM + "\"";
+        context.response().setHeader(Http.HeaderNames.WWW_AUTHENTICATE, challenge);
+        return unauthorized();
+    }
+    
+    public static User parseCredentials(String credentials) throws MalformedCredentialsException, UnsupportedEncodingException {
+        // credentials = "Basic" basic-credentials
+        // basic-credentials = base64-user-pass
+        // base64-user-pass  = <base64 [4] encoding of user-pass,
+        // user-pass   = userid ":" password
+        // userid      = *<TEXT excluding ":">
+        // password    = *TEXT
+
+        if (credentials == null) {
+            return null;
+        }
+
+        String userpassBase64 = credentials.substring(6);
+        byte[] userpassBytes;
+        
+        try {
+            userpassBytes = new sun.misc.BASE64Decoder().decodeBuffer(userpassBase64);
+        } catch (IOException e) {
+            throw new MalformedCredentialsException("", e);
+        }
+        
+        // Use ISO-8859-1 only and not others, even if in RFC 2616, Section 2.2 "Basic Rules" allows
+        // TEXT to be encoded according to the rules of RFC 2047.
+        //
+        // Why: Julian Reschke, an editor for Authentication Scheme Registration of HTTPbis, said
+        // "RFC 2047 encoding doesn't apply in this case." and "the HTTPbis specs will not mention
+        // RFC 2047 anymore." See
+        // http://stackoverflow.com/questions/7242316/what-encoding-should-i-use-for-http-basic-authentication#comment11372987_9056877
+        String userpass = new String(userpassBytes, "ISO-8859-1");
+        
+        int colonAt = userpass.indexOf(':');
+        
+        if (colonAt < 0) {
+            throw new MalformedCredentialsException();
+        }
+
+        User authUser = new User();
+        authUser.loginId = userpass.substring(0, colonAt);
+        authUser.password = userpass.substring(colonAt + 1);
+        
+        return authUser;
+    }
+    
+    @Override
+    public Result call(Context context) throws Throwable {
+
+        String credential = context.request().getHeader(Http.HeaderNames.AUTHORIZATION);
+        User authUser = null;
+        
+        try {
+            authUser = parseCredentials(credential);
+        } catch (MalformedCredentialsException error) {
+            return badRequest();
+        } catch (UnsupportedEncodingException e) {
+            return internalServerError();
+        }
+        
+        if (authUser != null && User.authenticate(authUser)) {
+            context.session().put(UserApp.SESSION_USERID, String.valueOf(authUser.id));
+            context.session().put(UserApp.SESSION_USERNAME, authUser.name);
+            return delegate.call(context);
+        } else {
+            return unauthorized(context);
+        }
+    }
+}
---
+++ test/utils/BasicAuthActionTest.java
@@ -0,0 +1,82 @@
+package utils;
+
+import static org.junit.Assert.*;
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.io.UnsupportedEncodingException;
+
+import models.User;
+
+import org.junit.Test;
+
+
+import utils.BasicAuthAction;
+
+public class BasicAuthActionTest {
+
+    @Test
+    public void parseCredentials() {
+        // credentials = "Basic" basic-credentials
+        // basic-credentials = base64-user-pass
+        // base64-user-pass = <base64 [4] encoding of user-pass,
+        // user-pass = userid ":" password
+        // userid = *<TEXT excluding ":">
+        // password = *TEXT
+        
+        // ok
+        String userpass = "hello:world";
+        String basicCredentials = new sun.misc.BASE64Encoder().encode(userpass.getBytes());
+        try {
+            User user = BasicAuthAction.parseCredentials("Basic " + basicCredentials);
+            assertThat(user.loginId).isEqualTo("hello");
+            assertThat(user.password).isEqualTo("world");
+        } catch (UnsupportedEncodingException e) {
+            fail();
+        } catch (MalformedCredentialsException e) {
+            fail();
+        }
+        
+        // ok
+        userpass = ":";
+        basicCredentials = new sun.misc.BASE64Encoder().encode(userpass.getBytes());
+        try {
+            User user = BasicAuthAction.parseCredentials("Basic " + basicCredentials);
+            assertThat(user.loginId).isEqualTo("");
+            assertThat(user.password).isEqualTo("");
+        } catch (UnsupportedEncodingException e) {
+            fail();
+        } catch (MalformedCredentialsException e) {
+            fail();
+        }
+        
+        // malformed credentials.
+        String malformedUserpass = "helloworld";
+        String malformedCredentials = new sun.misc.BASE64Encoder().encode(malformedUserpass
+                .getBytes());
+        try {
+            BasicAuthAction.parseCredentials("Basic " + malformedCredentials);
+            fail();
+        } catch (UnsupportedEncodingException e) {
+            fail();
+        } catch (MalformedCredentialsException e) {
+            // success
+        }
+        
+        // username and password decoded by only ISO-8859-1
+        // NOTE: UnsupportedEncodingException is NOT thrown here. It should be thrown
+        // if and only if the Server does not support ISO-8859-1.
+        malformedUserpass = "안녕:세상";
+        malformedCredentials = new sun.misc.BASE64Encoder().encode(malformedUserpass
+                .getBytes());
+        try {
+            User user = BasicAuthAction.parseCredentials("Basic " + malformedCredentials);
+            assertThat(user.loginId).isNotEqualTo("안녕");
+            assertThat(user.password).isNotEqualTo("세상");
+        } catch (UnsupportedEncodingException e) {
+            fail();
+        } catch (MalformedCredentialsException e) {
+            fail();
+        }
+    }
+    
+}
