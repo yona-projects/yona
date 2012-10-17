06dc441c9d09476823d18f45144c37015a8db893
Yi EungJun <semtlenori@gmail.com>
2012-10-17 14:47:18 +0900

    shortcut: ctrl + enter to submit a form.
    Add this shortcut key for the Issue Tracker.
    
    How to enable the submitting shortcut key in the any other page
    ---------------------------------------------------------------
    
    Here is an example. The below makes all forms in the html page to
    support ctrl+enter as a submitting shortcut key.
    
        <script>
          nforge.require('shortcut.submit');
        </script>
    
    See app/views/issue/editIssue.scala.html for a real example.

---
+++ public/javascripts/modules/shortcut.js
@@ -0,0 +1,21 @@
+nforge.namespace('shortcut');
+
+// ctrl + enter to submit a form
+nforge.shortcut.submit = function () {
+  var that;
+
+  that = {
+    init : function () {
+      var eventHandler = function (event) {
+        if (event.ctrlKey && event.which == 13) {
+        $(event.target).parents('form').get(0).submit();
+        }
+      }
+
+      $('textarea').keydown(eventHandler);
+      $('input').keydown(eventHandler);
+    }
+  };
+
+  return that;
+};
