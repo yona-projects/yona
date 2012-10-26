e2de266c26a1c4745a630992f01b920b371ff86c
Yi EungJun <semtlenori@gmail.com>
2012-10-26 15:15:06 +0900

    uploader: Refactor and add comments.
    * Move many lines of code from AttachmentApp to Attachment model.
    * Add more comments.

--- app/controllers/AttachmentApp.java
+++ app/controllers/AttachmentApp.java
@@ -2,21 +2,14 @@ package controllers;
 
 import static play.libs.Json.toJson;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URLEncoder;
-import java.security.DigestInputStream;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.Formatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import javax.mail.internet.MimeUtility;
 
 import models.Attachment;
 import models.enumeration.Operation;
@@ -25,11 +18,8 @@ import models.enumeration.Resource;
 import org.apache.tika.Tika;
 import org.codehaus.jackson.JsonNode;
 
-import play.Logger;
 import play.mvc.Controller;
-import play.mvc.Http.MultipartFormData;
 import play.mvc.Http.MultipartFormData.FilePart;
-import play.mvc.Http.Request;
 import play.mvc.Result;
 import utils.AccessControl;
 import utils.RequestUtil;
@@ -37,82 +27,78 @@ import utils.RequestUtil;
 public class AttachmentApp extends Controller {
 
     public static Result newFile() throws NoSuchAlgorithmException, IOException {
-        FilePart filePart = request().body().asMultipartFormData().getFile("filePath");
+        // Get the file from request.
+        FilePart filePart =
+                request().body().asMultipartFormData().getFile("filePath");
+        if (filePart == null) {
+            return badRequest();
+        }
+        File file = filePart.getFile();
 
-        // Currently, anonymous cannot upload a file.
+        // Anonymous cannot upload a file.
         if (UserApp.currentUser() == UserApp.anonymous) {
             return forbidden();
         }
 
-        // Keep the file in the user's temporary area.
+        // Store the file in the user's temporary area.
         Attachment attach = new Attachment();
         attach.projectId = 0L;
         attach.containerType = Resource.USER;
         attach.containerId = UserApp.currentUser().id;
-        attach.mimeType = new Tika().detect(filePart.getFile());
+        attach.mimeType = new Tika().detect(file);
         attach.name = filePart.getFilename();
-
-        // Store the file in the filesystem.
-        String hash = AttachmentApp.saveFile(request());
-        attach.hash = hash;
-
-        Attachment sameAttach = Attachment.findBy(attach);
-
-        if (sameAttach == null) {
-            attach.save();
-        } else {
-            attach = sameAttach;
-        }
+        boolean isCreated = attach.save(file);
 
         // The request has been fulfilled and resulted in a new resource being
         // created. The newly created resource can be referenced by the URI(s)
         // returned in the entity of the response, with the most specific URI
         // for the resource given by a Location header field.
-        //     -- RFC 2616, 10.2.2. 201 Created
+        // -- RFC 2616, 10.2.2. 201 Created
         String url = routes.AttachmentApp.getFile(attach.id).url();
         response().setHeader("Location", url);
-
-        // The response SHOULD include an entity containing a list of resource
-        // characteristics and location(s) from which the user or user agent can
-        // choose the one most appropriate. -- RFC 2616, 10.2.2. 201 Created
-        Map<String, String> file = new HashMap<String, String>();
-        file.put("id", attach.id.toString());
-        file.put("mimeType", attach.mimeType);
-        file.put("name", attach.name);
-        file.put("url", url);
-        JsonNode responseBody = toJson(file);
 
         // The entity format is specified by the media type given in the
         // Content-Type header field. -- RFC 2616, 10.2.2. 201 Created
         response().setHeader("Content-Type", "application/json");
 
-        if (sameAttach == null) {
+        // The response SHOULD include an entity containing a list of resource
+        // characteristics and location(s) from which the user or user agent can
+        // choose the one most appropriate. -- RFC 2616, 10.2.2. 201 Created
+        Map<String, String> fileInfo = new HashMap<String, String>();
+        fileInfo.put("id", attach.id.toString());
+        fileInfo.put("mimeType", attach.mimeType);
+        fileInfo.put("name", attach.name);
+        fileInfo.put("url", url);
+        JsonNode responseBody = toJson(fileInfo);
+
+        if (isCreated) {
             // If an attachment has been created -- it does NOT mean that
             // a file is created in the filesystem -- return 201 Created.
             return created(responseBody);
         } else {
             // If the attachment already exists, return 200 OK.
             // Why not 204? -- Because 204 doesn't allow that response has body,
-            // we cannot tell what is same with the file you try to add.
+            // so we cannot tell what is same with the file you try to add.
             return ok(responseBody);
         }
     }
 
-    public static Result getFile(Long id)
-            throws NoSuchAlgorithmException, IOException {
+    public static Result getFile(Long id) throws NoSuchAlgorithmException, IOException {
         Attachment attachment = Attachment.findById(id);
 
         if (attachment == null) {
             return notFound();
         }
 
-        if (!AccessControl.isAllowed(UserApp.currentUser().id, attachment.projectId, attachment.containerType, Operation.READ, attachment.containerId)) {
+        if (!AccessControl.isAllowed(UserApp.currentUser().id, attachment.projectId,
+                attachment.containerType, Operation.READ, attachment.containerId)) {
             return forbidden();
         }
 
-        File file = new File("uploads/" + attachment.hash);
+        File file = attachment.getFile();
 
-        // RFC 2231; IE 8 or less, and Safari 5 or less are not supported.
+        // Encode the filename with RFC 2231; IE 8 or less, and Safari 5 or less
+        // are not supported. See
         String filename = attachment.name.replaceAll("[:\\x5c\\/{?]", "_");
         filename = URLEncoder.encode(filename, "UTF-8").replaceAll("\\+", "%20");
         filename = "filename*=UTF-8''" + filename;
@@ -124,10 +110,13 @@ public class AttachmentApp extends Controller {
         return ok(file);
     }
 
-    public static Result deleteFile(Long id, String filename) throws NoSuchAlgorithmException, IOException {
+    public static Result deleteFile(Long id, String filename)
+            throws NoSuchAlgorithmException, IOException {
         // _method must be 'delete'
-        Map<String, String[]> data = request().body().asMultipartFormData().asFormUrlEncoded();
-        if (!RequestUtil.getFirstValueFromQuery(data, "_method").toLowerCase().equals("delete")) {
+        Map<String, String[]> data =
+                request().body().asMultipartFormData().asFormUrlEncoded();
+        if (!RequestUtil.getFirstValueFromQuery(data, "_method").toLowerCase()
+                .equals("delete")) {
             return badRequest("_method must be 'delete'.");
         }
 
@@ -141,54 +130,21 @@ public class AttachmentApp extends Controller {
         // Delete the file matched with the attachment,
         // if and only if no attachment refer the file.
         if (Attachment.exists(attach.hash)) {
-            return ok("The attachment is removed successfully, but the origin file still exists because it is referred by somewhere.");
+            return ok("The attachment is removed successfully, but the origin" +
+                    "file still exists because it is referred by somewhere.");
         }
 
         boolean result = new File("public/uploadFiles/" + attach.hash).delete();
 
         if (result) {
-            return ok("The both of attachment and origin file is removed successfully.");
+            return ok("The both of attachment and origin file is removed" +
+                    "successfully.");
         } else {
-            return status(202, "The attachment is removed successfully, but the origin file still exists abnormally even if it is referred by nowhere.");
+            return status(202,
+                    "The attachment is removed successfully, but the origin" +
+                    "file still exists abnormally even if it is referred by" +
+                    "nowhere.");
         }
-    }
-
-    /**
-     * From BoardApp
-     *
-     * @param request
-     * @return
-     * @throws NoSuchAlgorithmException
-     * @throws IOException
-     */
-    static String saveFile(Request request) throws NoSuchAlgorithmException, IOException {
-        MultipartFormData body = request.body().asMultipartFormData();
-        FilePart filePart = body.getFile("filePath");
-
-        if (filePart != null) {
-            MessageDigest algorithm = MessageDigest.getInstance("SHA1");
-            DigestInputStream dis = new DigestInputStream(new BufferedInputStream(new FileInputStream(filePart.getFile())), algorithm);
-            while(dis.read() != -1);
-            Formatter formatter = new Formatter();
-            for (byte b : algorithm.digest()) {
-                formatter.format("%02x", b);
-            }
-            File uploads = new File("uploads");
-            if (!uploads.exists()) {
-                uploads.mkdir();
-            } else if (!uploads.isDirectory()) {
-                throw new RuntimeException();
-            }
-            File saveFile = new File("uploads/" + formatter.toString());
-            filePart.getFile().renameTo(saveFile);
-            String hash = formatter.toString();
-
-            formatter.close();
-            dis.close();
-
-            return hash;
-        }
-        return null;
     }
 
     public static Map<String, String> fileAsMap(Attachment attach) {
@@ -203,22 +159,27 @@ public class AttachmentApp extends Controller {
     }
 
     public static Result getFileList() {
-        Map<String, List<Map<String, String>>> files = new HashMap<String, List<Map<String, String>>>();
+        Map<String, List<Map<String, String>>> files =
+                new HashMap<String, List<Map<String, String>>>();
 
+        // Get files from the user's temporary area.
         List<Map<String, String>> tempFiles = new ArrayList<Map<String, String>>();
         for (Attachment attach : Attachment.findTempFiles(UserApp.currentUser().id)) {
             tempFiles.add(fileAsMap(attach));
         }
         files.put("tempFiles", tempFiles);
 
+        // Get the attached files only if the user has the proper permission.
         Map<String, String[]> query = request().queryString();
         String containerType = RequestUtil.getFirstValueFromQuery(query, "containerType");
         String containerId = RequestUtil.getFirstValueFromQuery(query, "containerId");
 
         if (containerType != null && containerId != null) {
             List<Map<String, String>> attachments = new ArrayList<Map<String, String>>();
-            for (Attachment attach : Attachment.findByContainer(Resource.valueOf(containerType), Long.parseLong(containerId))) {
-                if (!AccessControl.isAllowed(UserApp.currentUser().id, attach.projectId, attach.containerType, Operation.READ, attach.containerId)) {
+            for (Attachment attach : Attachment.findByContainer(Resource.valueOf(containerType),
+                    Long.parseLong(containerId))) {
+                if (!AccessControl.isAllowed(UserApp.currentUser().id, attach.projectId,
+                        attach.containerType, Operation.READ, attach.containerId)) {
                     return forbidden();
                 }
                 attachments.add(fileAsMap(attach));
@@ -226,10 +187,8 @@ public class AttachmentApp extends Controller {
             files.put("attachments", attachments);
         }
 
-        JsonNode responseBody = toJson(files);
-
+        // Return the list of files as JSON.
         response().setHeader("Content-Type", "application/json");
-
-        return ok(responseBody);
+        return ok(toJson(files));
     }
 }
--- app/models/Attachment.java
+++ app/models/Attachment.java
@@ -1,5 +1,13 @@
 package models;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -19,8 +27,8 @@ public class Attachment extends Model {
      *
      */
     private static final long serialVersionUID = 7856282252495067924L;
-    private static Finder<Long, Attachment> find = new Finder<Long, Attachment>(
-        Long.class, Attachment.class);
+    private static Finder<Long, Attachment> find = new Finder<Long, Attachment>(Long.class,
+            Attachment.class);
     @Id
     public Long id;
 
@@ -50,17 +58,8 @@ public class Attachment extends Model {
                 .eq("name", attach.name)
                 .eq("hash", attach.hash)
                 .eq("projectId", attach.projectId)
-                .eq("containerType",attach.containerType)
+                .eq("containerType", attach.containerType)
                 .eq("containerId", attach.containerId).findUnique();
-    }
-
-    public static Attachment findBy(String name, String hash, Long projectId, Resource containerType, Long containerId) {
-        return find.where()
-                .eq("name", name)
-                .eq("hash", hash)
-                .eq("projectId", projectId)
-                .eq("containerType",containerType)
-                .eq("containerId", containerId).findUnique();
     }
 
     public static boolean exists(String hash) {
@@ -70,20 +69,78 @@ public class Attachment extends Model {
     public static Attachment findById(Long id) {
         return find.byId(id);
     }
-   public static List<Attachment> findByContainer(Resource containerType, Long containerId) {
-       return find.where()
-               .eq("containerType", containerType)
-               .eq("containerId", containerId).findList();
-   }
 
-    public static void moveTempFiles(Long userId, Long projectId, Resource containerType, Long containerId) {
+    public static List<Attachment> findByContainer(Resource containerType, Long containerId) {
+        return find.where()
+                .eq("containerType", containerType)
+                .eq("containerId", containerId).findList();
+    }
+
+    public static void moveTempFiles(Long userId, Long projectId, Resource containerType,
+            Long containerId) {
         // Move the attached files in the temporary area to the issue area.
         List<Attachment> attachments = Attachment.findTempFiles(userId);
-        for(Attachment attachment : attachments) {
+        for (Attachment attachment : attachments) {
             attachment.projectId = projectId;
             attachment.containerType = containerType;
             attachment.containerId = containerId;
             attachment.save();
         }
+    }
+
+    public static String storeFile(File file) throws NoSuchAlgorithmException, IOException {
+        // Compute sha1 checksum.
+        MessageDigest algorithm = MessageDigest.getInstance("SHA1");
+        DigestInputStream dis = new DigestInputStream(
+                new BufferedInputStream(new FileInputStream(file)), algorithm);
+        while (dis.read() != -1)
+            ;
+        Formatter formatter = new Formatter();
+        for (byte b : algorithm.digest()) {
+            formatter.format("%02x", b);
+        }
+        String hash = formatter.toString();
+
+        // Store the file.
+        File uploads = new File("uploads");
+        if (!uploads.exists()) {
+            uploads.mkdir();
+        } else if (!uploads.isDirectory()) {
+            throw new RuntimeException();
+        }
+        File saveFile = new File("uploads/" + formatter.toString());
+        file.renameTo(saveFile);
+
+        // Close all resources
+        formatter.close();
+        dis.close();
+
+        return hash;
+    }
+
+    @Override
+    public void save() {
+        throw new UnsupportedOperationException("Use save(File file)");
+    }
+
+    // Return true only if the attachment record is created because there was
+    // no same record.
+    public boolean save(File file) throws NoSuchAlgorithmException, IOException {
+        // Store the file in the filesystem and compute SHA1 hash.
+        this.hash = Attachment.storeFile(file);
+
+        // Add the attachment into the Database only if there is no same record.
+        Attachment sameAttach = Attachment.findBy(this);
+        if (sameAttach == null) {
+            super.save();
+            return true;
+        } else {
+            this.id = sameAttach.id;
+            return false;
+        }
+    }
+
+    public File getFile() {
+        return new File("uploads/" + this.hash);
     }
 }
