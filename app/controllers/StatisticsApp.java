ccf7e8cbd1d15a8a5755776d09bdf44bee6e5019
Sangcheol Hwang <k16wire@nhn.com>
2012-11-22 01:23:06 +0900

    add site setting url

--- app/controllers/StatisticsApp.java
+++ app/controllers/StatisticsApp.java
@@ -1,10 +1,13 @@
 package controllers;
 
+import models.Project;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.project.statistics;
 
 public class StatisticsApp extends Controller {
-	public static Result statistics() {
-		return ok(views.html.underconstruction.render("statistics"));
+	public static Result statistics(String userName, String projectName) {
+		Project project = ProjectApp.getProject(userName, projectName);
+		return ok(statistics.render("statistics", project));
 	}
 }
