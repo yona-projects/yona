38ede074802496bea693329d724eceacd50fa9f7
Youngje Yun <youngje.yun@gmail.com>
2012-11-22 17:02:16 +0900

    fix deleteUser

--- app/models/Project.java
+++ app/models/Project.java
@@ -126,14 +126,12 @@ public class Project extends Model {
                                         .eq("projectUser.user.id", userId)
                                         .eq("projectUser.role.id", RoleType.MANAGER.roleType())
                                     .findList();
-        synchronized (java.lang.Object.class) {
         Iterator<Project> iterator = projects.iterator();
         while(iterator.hasNext()){
             Project project = iterator.next();
             if(ProjectUser.checkOneMangerPerOneProject(project.id)) {
                 iterator.remove();
             }
-        }
         }
         
         return projects;
