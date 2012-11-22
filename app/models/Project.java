029e36e1363e800b42e39b68f6dfc098291e6823
Youngje Yun <youngje.yun@gmail.com>
2012-11-22 16:50:42 +0900

    fix deleteUser @ SiteApp

--- app/models/Project.java
+++ app/models/Project.java
@@ -126,13 +126,14 @@ public class Project extends Model {
                                         .eq("projectUser.user.id", userId)
                                         .eq("projectUser.role.id", RoleType.MANAGER.roleType())
                                     .findList();
-        
+        synchronized (java.lang.Object.class) {
         Iterator<Project> iterator = projects.iterator();
         while(iterator.hasNext()){
             Project project = iterator.next();
             if(ProjectUser.checkOneMangerPerOneProject(project.id)) {
-                projects.remove(project);
+                iterator.remove();
             }
+        }
         }
         
         return projects;
