df5c18424ae7242d1d050b96762abfe46b5e55a4
Sangcheol Hwang <k16wire@nhn.com>
2012-11-24 14:38:35 +0900

    initial commit

--- app/controllers/HelpApp.java
+++ app/controllers/HelpApp.java
@@ -2,9 +2,10 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.help.*;
 
 public class HelpApp extends Controller {
 	public static Result help() {
-		return ok(views.html.underconstruction.render("help"));
+		return ok(toc.render("help"));
 	}
 }
