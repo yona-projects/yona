e907d6f3ffb7c5d84a59996c9cd0aee493531269
BlueMir <qa22ahj@naver.com>
2012-11-27 11:33:38 +0900

    display selected menu

---
+++ app/utils/MenuType.java
@@ -0,0 +1,12 @@
+package utils;
+
+public enum MenuType {
+	SITE_HOME(1), NEW_PROJECT(2), PROJECTS(3), HELP(4), SITE_SETTING(5), USER(6),
+    PROJECT_HOME(100), BOARD(101), CODE(102), ISSUE(103), TASK(104), PROJECT_SETTING(105), NONE(0); 
+    
+    private int type;
+    
+    private MenuType(int type) {
+        this.type = type;
+    }
+}
