075c1ecf3544c47f80c0aba4611fb64177c761f4
Yi EungJun <semtlenori@gmail.com>
2013-01-11 17:07:21 +0900

    uploader: Fix dynamic form doesn't work in IE.
    In IE, HTML form sholud be included in HTML body if its method is POST.

--- public/javascripts/modules/markdown.js
+++ public/javascripts/modules/markdown.js
@@ -90,8 +90,10 @@ var fileUploader = function (textarea, action) {
       var form = $('<form>')
         .attr('method', 'post')
         .attr('enctype', 'multipart/form-data')
-        .attr('action', file.url);
+        .attr('action', file.url)
+        .css('display', 'none');
       form.append('<input type="hidden" name="_method" value="delete">');
+      $('body').append(form);
       form.ajaxForm({
         success: function() {
           fileitem.remove();
@@ -222,8 +224,10 @@ var fileUploader = function (textarea, action) {
         var form = $('<form>')
           .attr('method', 'post')
           .attr('enctype', 'multipart/form-data')
-          .attr('action', action);
+          .attr('action', action)
+          .css('display', 'none');
         form.append(attachment);
+        $('body').append(form);
         form.ajaxForm(fileUploadOptions);
         form.submit();
       }
