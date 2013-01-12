77365b3cd6385f02d14a21b2a9b352f4ddc642be
doortts <doortts@gmail.com>
2013-01-13 01:35:39 +0900

    Refactoring for fast restart   - main reason is for speed up application restart on dev mode   - move init data from onStart to Application.init   - add link to naviBar for manual init   - and so on..

--- app/Global.java
+++ app/Global.java
@@ -1,27 +1,11 @@
-import java.io.File;
-import java.util.List;
-import java.util.Map;
-
-import models.Project;
-import models.User;
+import controllers.routes;
 import play.Application;
 import play.GlobalSettings;
-import play.Logger;
 import play.api.mvc.Handler;
-import play.libs.Yaml;
 import play.mvc.Http.RequestHeader;
-import playRepository.RepositoryService;
-
-import com.avaje.ebean.Ebean;
-
-import controllers.routes;
 
 public class Global extends GlobalSettings {
     public void onStart(Application app) {
-        InitialData.insert(app);
-        InitialData.makeUploadFolder();
-        InitialData.makeTestRepository();
-        // UserApp.anonymous = User.findByLoginId("anonymous");
     }
 
     @Override
@@ -37,55 +21,6 @@ public class Global extends GlobalSettings {
         return super.onRouteRequest(request);
     }
 
-    static class InitialData {
-
-        public static void insert(Application app) {
-            if (Ebean.find(User.class).findRowCount() == 0) {
-                @SuppressWarnings("unchecked")
-                Map<String, List<Object>> all = (Map<String, List<Object>>) Yaml
-                        .load("initial-data.yml");
-
-                Ebean.save(all.get("users"));
-                Ebean.save(all.get("projects"));
-                Ebean.save(all.get("milestones"));
-                Ebean.save(all.get("issues"));
-                Ebean.save(all.get("issueComments"));
-                Ebean.save(all.get("posts"));
-                Ebean.save(all.get("comments"));
-                Ebean.save(all.get("permissions"));
-
-                Ebean.save(all.get("roles"));
-                for (Object role : all.get("roles")) {
-                    Ebean.saveManyToManyAssociations(role, "permissions");
-                }
-                Ebean.save(all.get("projectUsers"));
-                
-                Ebean.save(all.get("taskBoards"));
-                Ebean.save(all.get("lines"));
-                Ebean.save(all.get("cards"));
-                Ebean.save(all.get("labels"));
-                Ebean.save(all.get("checkLists"));
-            }
-        }
-
-        public static void makeUploadFolder() {
-            new File("public/uploadFiles/").mkdir();
-        }
-
-        public static void makeTestRepository() {
-            for (Project project : Project.find.all()) {
-                Logger.debug("makeTestRepository: " + project.name);
-                try {
-                    RepositoryService.createRepository(project);
-                } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
     public void onStop(Application app) {
-
     }
 }
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,21 @@
 package controllers;
 
+import java.io.File;
 import java.util.List;
+import java.util.Map;
 
+import com.avaje.ebean.Ebean;
 import models.Project;
+import models.User;
 
 import org.h2.util.StringUtils;
 
+import play.Logger;
 import play.cache.Cached;
+import play.libs.Yaml;
 import play.mvc.Controller;
 import play.mvc.Result;
+import playRepository.RepositoryService;
 import views.html.index;
 
 public class Application extends Controller {
@@ -28,7 +35,61 @@ public class Application extends Controller {
         return ok(index.render(null));
     }
 
+    public static Result init() {
+        insertDefaults();
+        makeUploadFolder();
+        makeTestRepository();
+        return redirect(routes.Application.index());
+    }
+
     public static Result jsMessages() {
         return ok(jsmessages.JsMessages.generate("Messages")).as("application/javascript");
     }
+
+    private static void insertDefaults() {
+        if (Ebean.find(User.class).findRowCount() == 0) {
+            @SuppressWarnings("unchecked")
+            Map<String, List<Object>> all = (Map<String, List<Object>>) Yaml
+                    .load("initial-data.yml");
+
+            Ebean.save(all.get("users"));
+            Ebean.save(all.get("projects"));
+            Ebean.save(all.get("milestones"));
+            Ebean.save(all.get("issues"));
+            Ebean.save(all.get("issueComments"));
+            Ebean.save(all.get("posts"));
+            Ebean.save(all.get("comments"));
+            Ebean.save(all.get("permissions"));
+
+            Ebean.save(all.get("roles"));
+            for (Object role : all.get("roles")) {
+                Ebean.saveManyToManyAssociations(role, "permissions");
+            }
+            Ebean.save(all.get("projectUsers"));
+
+            Ebean.save(all.get("taskBoards"));
+            Ebean.save(all.get("lines"));
+            Ebean.save(all.get("cards"));
+            Ebean.save(all.get("labels"));
+            Ebean.save(all.get("checkLists"));
+        }
+    }
+
+    private static void makeUploadFolder() {
+        new File("public/uploadFiles/").mkdir();
+    }
+
+    private static void makeTestRepository() {
+        for (Project project : Project.find.all()) {
+            Logger.debug("makeTestRepository: " + project.name);
+            try {
+                RepositoryService.createRepository(project);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
 }
