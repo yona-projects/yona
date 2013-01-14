ad039a6f9eb5a304f22bf27017f394fd5ca04378
Yi EungJun <semtlenori@gmail.com>
2013-01-14 10:54:40 +0900

    A comment for Webdav HTTP methods.
    Add a comment to explain why we route Webdav methods in OnRouteRequest,
    not conf/routes.

--- app/Global.java
+++ app/Global.java
@@ -10,11 +10,13 @@ public class Global extends GlobalSettings {
 
     @Override
     public Handler onRouteRequest(RequestHeader request) {
-        String[] arr = { "PROPFIND", "REPORT", "PROPPATCH", "COPY", "MOVE", "LOCK", "UNLOCK",
-                "MKCOL", "VERSION-CONTROL", "MKWORKSPACE", "MKACTIVITY", "CHECKIN", "CHECKOUT",
-                "MERGE", "TRACE" };
-        for (String key : arr) {
-            if (request.method().equalsIgnoreCase(key)) {
+        // Route here these webdav methods to be used for serving Subversion
+        // repositories, because Play2 cannot route them.
+        String[] webdavMethods = { "PROPFIND", "REPORT", "PROPPATCH", "COPY", "MOVE", "LOCK",
+                "UNLOCK", "MKCOL", "VERSION-CONTROL", "MKWORKSPACE", "MKACTIVITY", "CHECKIN",
+                "CHECKOUT", "MERGE", "TRACE" };
+        for (String method : webdavMethods) {
+            if (request.method().equalsIgnoreCase(method)) {
                 return routes.ref.SvnApp.service().handler();
             }
         }
