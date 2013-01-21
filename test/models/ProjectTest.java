148440359f50764607564831f35b5094e9fc1d1c
Sangcheol Hwang <k16wire@gmail.com>
2013-01-21 13:53:56 +0900

    remove redundant test case

--- test/models/ProjectTest.java
+++ test/models/ProjectTest.java
@@ -147,35 +147,4 @@ public class ProjectTest extends ModelTest<Project> {
         assertThat(result1).isEqualTo(false);
         assertThat(result2).isEqualTo(true);
     }
-    
-    @Test
-    public void lastUpdate() throws Exception {
-        // given
-        String userName = "hobi";
-        String projectName = "testProject";
-        String wcPath = GitRepository.getRepoPrefix() + userName + "/" + projectName;
-
-        String repoPath = wcPath + "/.git";
-        File repoDir = new File(repoPath);
-        Repository repo = new RepositoryBuilder().setGitDir(repoDir).build();
-        repo.create(false);
-
-        Git git = new Git(repo);
-        String testFilePath = wcPath + "/readme.txt";
-        BufferedWriter out = new BufferedWriter(new FileWriter(testFilePath));
-
-        out.write("hello 1");
-        out.flush();
-        git.add().addFilepattern("readme.txt").call();
-        git.commit().setMessage("commit 1").call();
-
-        GitRepository gitRepo = new GitRepository(userName, projectName + "/");
-        
-//    	Project project = Project.findByNameAndOwner(userName, projectName);
-    	// When
-    	List<Commit> history = gitRepo.getHistory(0, 2, "HEAD");
-    	Commit commit = history.get(0);
-    	// Then
-    	assertThat(commit.getAuthorDate()).isNotNull();
-    }
 }
