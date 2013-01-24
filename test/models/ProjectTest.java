bff19303d7ac0d88ca7b97adb2d7e935dbf4e8e2
Sangcheol Hwang <k16wire@gmail.com>
2013-01-24 18:22:46 +0900

    remove and fix test case

--- test/models/ProjectTest.java
+++ test/models/ProjectTest.java
@@ -94,7 +94,7 @@ public class ProjectTest extends ModelTest<Project> {
         List<Project> projectsHobi = Project.isOnlyManager(2l);
         List<Project> projectsEungjun = Project.isOnlyManager(5l);
         // Then
-        assertThat(projectsHobi.size()).isEqualTo(1);
+        assertThat(projectsHobi.size()).isEqualTo(0);
         assertThat(projectsEungjun.size()).isEqualTo(0);
     }
 
@@ -145,36 +145,5 @@ public class ProjectTest extends ModelTest<Project> {
         // Then
         assertThat(result1).isEqualTo(false);
         assertThat(result2).isEqualTo(true);
-    }
-
-    @Test
-    public void lastUpdate() throws Exception {
-        // given
-        String userName = "hobi";
-        String projectName = "testProject";
-        String wcPath = GitRepository.getRepoPrefix() + userName + "/" + projectName;
-
-        String repoPath = wcPath + "/.git";
-        File repoDir = new File(repoPath);
-        Repository repo = new RepositoryBuilder().setGitDir(repoDir).build();
-        repo.create(false);
-
-        Git git = new Git(repo);
-        String testFilePath = wcPath + "/readme.txt";
-        BufferedWriter out = new BufferedWriter(new FileWriter(testFilePath));
-
-        out.write("hello 1");
-        out.flush();
-        git.add().addFilepattern("readme.txt").call();
-        git.commit().setMessage("commit 1").call();
-
-        GitRepository gitRepo = new GitRepository(userName, projectName + "/");
-
-//    	Project project = Project.findByNameAndOwner(userName, projectName);
-    	// When
-    	List<Commit> history = gitRepo.getHistory(0, 2, "HEAD");
-    	Commit commit = history.get(0);
-    	// Then
-    	assertThat(commit.getAuthorDate()).isNotNull();
     }
 }
