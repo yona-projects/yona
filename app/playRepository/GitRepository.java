7b5df74fd9dff854eb1627ccd49a988516363102
doortts <doortts@gmail.com>
2013-02-12 11:40:27 +0900

    Code refactoring  - remove duplicate code

--- app/playRepository/GitRepository.java
+++ app/playRepository/GitRepository.java
@@ -25,6 +25,7 @@ import org.eclipse.jgit.diff.DiffEntry;
 
 import controllers.ProjectApp;
 
+import org.tmatesoft.svn.core.SVNException;
 import play.Logger;
 import play.libs.Json;
 import utils.FileUtil;
@@ -63,6 +64,11 @@ public class GitRepository implements PlayRepository {
         this.repository.create(true); // create bare repository
     }
 
+    @Override
+    public ObjectNode findFileInfo(String path) throws IOException, NoHeadException, GitAPIException, SVNException {
+        return findFileInfo(null, path);
+    }
+
     /**
      * path를 받아 파일 정보 JSON객체를 return 하는 함수 일단은 HEAD 만 가능하다.
      *
@@ -73,75 +79,18 @@ public class GitRepository implements PlayRepository {
      * @throws NoHeadException
      * @throws GitAPIException
      */
-    @Override
-    public ObjectNode findFileInfo(String path) throws IOException, NoHeadException,
-            GitAPIException {
-        // 파일 정보를 찾아서 Json으로 리턴
-        Git git = new Git(repository);
-
-        ObjectId headCommit = repository.resolve(Constants.HEAD);
-        // 만약 특정 커밋을 얻오오고싶다면 바꾸어 주면 된다.
-
-        if (headCommit == null) {
-            Logger.debug("GitRepository : init Project - No Head commit");
-            return null;
-        }
-
-        RevWalk revWalk = new RevWalk(repository);
-        RevTree revTree = revWalk.parseTree(headCommit);
-        TreeWalk treeWalk = new TreeWalk(repository);
-        treeWalk.addTree(revTree);
-
-        try {
-            PathFilter pathFilter = PathFilter.create(path);
-
-            treeWalk.setFilter(pathFilter);
-
-            while (treeWalk.next()) {
-                if (pathFilter.isDone(treeWalk)) {
-                    break;
-                } else if (treeWalk.isSubtree()) {
-                    treeWalk.enterSubtree();
-                }
-            }
-        } catch (IllegalArgumentException e) {
-            // root
-            return folderList(git, treeWalk, headCommit);
-        }
-
-        if (treeWalk.isSubtree()) {
-            treeWalk.enterSubtree();
-            return folderList(git, treeWalk, headCommit);
-        } else {
-            // 파일 내려주기
-            ObjectId objectId = treeWalk.getObjectId(0);
-
-            RevCommit commit = git.log().addPath(path).call().iterator().next();
-            ObjectNode result = Json.newObject();
-
-            result.put("type", "file");
-            result.put("msg", commit.getShortMessage());
-            result.put("author", commit.getAuthorIdent().getName());
-            result.put("date", new Date(commit.getCommitTime() * 1000L).toString());
-
-            result.put("commitMessage", commit.getShortMessage());
-            result.put("commiter", commit.getAuthorIdent().getName());
-
-            result.put("commitDate", new Date(commit.getCommitTime() * 1000l).toString());
-
-            String str = new String(repository.open(objectId).getBytes());
-            result.put("data", str);
-            return result;
-        }
-    }
 
     @Override
     public ObjectNode findFileInfo(String branch, String path) throws AmbiguousObjectException, IOException, NoHeadException, GitAPIException {
         Git git = new Git(repository);
+        ObjectId headCommit;
 
-        ObjectId headCommit = repository.resolve(branch);
+        if (branch == null){
+            headCommit = repository.resolve(Constants.HEAD);
+        } else {
+            headCommit = repository.resolve(branch);
+        }
         // 만약 특정 커밋을 얻오오고싶다면 바꾸어 주면 된다.
-
         if (headCommit == null) {
             Logger.debug("GitRepository : init Project - No Head commit");
             return null;
@@ -177,22 +126,26 @@ public class GitRepository implements PlayRepository {
             ObjectId objectId = treeWalk.getObjectId(0);
 
             RevCommit commit = git.log().addPath(path).call().iterator().next();
-            ObjectNode result = Json.newObject();
-
-            result.put("type", "file");
-            result.put("msg", commit.getShortMessage());
-            result.put("author", commit.getAuthorIdent().getName());
-            result.put("date", new Date(commit.getCommitTime() * 1000L).toString());
-
-            result.put("commitMessage", commit.getShortMessage());
-            result.put("commiter", commit.getAuthorIdent().getName());
-
-            result.put("commitDate", new Date(commit.getCommitTime() * 1000l).toString());
-
-            String str = new String(repository.open(objectId).getBytes());
-            result.put("data", str);
-            return result;
+            return getJsonNodes("file", objectId, commit);
         }
+    }
+
+    private ObjectNode getJsonNodes(String type, ObjectId objectId, RevCommit commit) throws IOException {
+        ObjectNode result = Json.newObject();
+
+        result.put("type", type);
+        result.put("msg", commit.getShortMessage());
+        result.put("author", commit.getAuthorIdent().getName());
+        result.put("date", new Date(commit.getCommitTime() * 1000L).toString());
+
+        result.put("commitMessage", commit.getShortMessage());
+        result.put("commiter", commit.getAuthorIdent().getName());
+
+        result.put("commitDate", new Date(commit.getCommitTime() * 1000l).toString());
+
+        String str = new String(repository.open(objectId).getBytes());
+        result.put("data", str);
+        return result;
     }
 
     private ObjectNode folderList(Git git, TreeWalk treeWalk, AnyObjectId commitId) throws MissingObjectException,
