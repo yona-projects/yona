9979417af587323807aed33bbc4e14c987803e25
Yi EungJun <semtlenori@gmail.com>
2013-04-11 16:48:28 +0900

    Fix an erratum from 6.sql.

--- conf/evolutions/default/6.sql
+++ conf/evolutions/default/6.sql
@@ -1,4 +1,4 @@
 # --- !Ups
 
 UPDATE project SET last_issue_number = 0 WHERE last_issue_number IS NULL;
-UPDATE project SET last_posting_number = 0 WHERE last_issue_number IS NULL;
+UPDATE project SET last_posting_number = 0 WHERE last_posting_number IS NULL;
