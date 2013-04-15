c4c693b22bc4f933f6eb4d502349e191e4f3e16d
Jihan Kim <laziel@gmail.com>
2013-04-15 17:50:10 +0900

    change UI component name

---
+++ public/javascripts/common/hive.ui.Dropdown.js
@@ -0,0 +1,128 @@
+/**
+ * @(#)hive.ui.Dropdown.js 2013.04.11
+ *
+ * Copyright NHN Corporation.
+ * Released under the MIT license
+ * 
+ * http://hive.dev.naver.com/license
+ */
+
+/**
+ * bootstrap-dropdown.js 에서는 단순히 목록 토글 기능만 제공하므로
+ * hive.Dropdown 로 해당 영역을 지정하면 <select> 의 기능을 하도록 만든다
+ * @example 
+ * var oSelect = new hive.Dropdown({
+ *     "elContainer": ".btn-group",
+ *     "fOnChange"  : function(){},
+ *     "
+ * });
+ */
+(function(ns){
+	
+	var oNS = $hive.createNamespace(ns);
+	oNS.container[oNS.name] = function(htOptions){
+
+		var htVar = {};
+		var htElement = {};
+		
+		/**
+		 * 초기화
+		 */
+		function _init(htOptions){
+			_initElement(htOptions);
+			_attachEvent();
+	
+			htVar.fOnChange = htOptions.fOnChange;
+			
+			_selectDefault();
+		}
+		
+		/**
+		 * 엘리먼트 변수
+		 */
+		function _initElement(htOptions){
+			htElement.welContainer = $(htOptions.elContainer);
+			htElement.welSelectedLabel = htElement.welContainer.find("button.d-label");
+			htElement.waItems = htElement.welContainer.find(".dropdown-menu li");
+		}
+		
+		/**
+		 * 이벤트 처리
+		 */
+		function _attachEvent(){
+			htElement.waItems.click(_onClickItem);
+		}
+	
+		/**
+		 * 항목 선택시 이벤트 핸들러
+		 */
+		function _onClickItem(){
+			var welTarget = $(this);
+			
+			_setItemSelected(welTarget);	// display
+			_setFormValue(welTarget);		// form value
+	
+			_onChange();
+		}
+		
+		/**
+		 * 선택한 항목을 선택된 상태로 보이게 만드는 함수
+		 * @param {Wrapped Element} welTarget
+		 */
+		function _setItemSelected(welTarget){
+			htElement.welSelectedLabel.text(welTarget.text());
+			htElement.waItems.removeClass("active");
+			welTarget.addClass("active");		
+		}
+		
+		/**
+		 * 선택한 항목의 값을 input (type=hidden) 형태로 실제 폼 엘리먼트 값으로 반영
+		 * @param {Wrapped Element} welTarget
+		 */
+		function _setFormValue(welTarget){
+			var sFieldValue = welTarget.attr("data-value") || welTarget.text();
+			var sFieldName	= htElement.welContainer.attr("data-name");
+			var welInput	= htElement.welContainer.find("input[name='" + sFieldName +"']");
+			
+			if(typeof sFieldName == "undefined"){
+				return;
+			}
+	
+			if(welInput.length === 0){
+				welInput = $('<input type="hidden" name="' + sFieldName + '">');
+				htElement.welContainer.append(welInput);
+			}
+			
+			welInput.val(sFieldValue);
+		}
+		
+		/**
+		 * 항목 값이 변경되면 실행될 함수 
+		 */
+		function _onChange(){
+			if(typeof htVar.fOnChange == "function"){
+				setTimeout(htVar.fOnChange, 0);
+			}
+		}
+	
+		/**
+		 * 기본값 지정이 있으면 선택된 상태로 만들기
+		 */
+		function _selectDefault(){
+			var waFind = htElement.welContainer.find("li[data-selected=true]");
+			if(waFind.length <= 0){
+				return;
+			}
+			
+			var welTarget = $(waFind[0]);
+			_setItemSelected(welTarget);
+			_setFormValue(welTarget);
+		}
+		
+		_init(htOptions);
+	};
+
+})("hive.ui.Dropdown");
+
+ 
+	
