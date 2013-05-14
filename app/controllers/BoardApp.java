bceba0fe25cfbcc45245d6978959770c9da07051
kjkmadness <kjkmadness@nhn.com>
2013-05-14 19:16:50 +0900

    bug fixed - BoardApp.post(user, project, postId) throws NullPointerException if the postId does not exist.

--- app/controllers/BoardApp.java
+++ app/controllers/BoardApp.java
@@ -151,17 +151,16 @@ public class BoardApp extends AbstractPostingApp {
     public static Result post(String userName, String projectName, Long postId) {
         Project project = ProjectApp.getProject(userName, projectName);
         Posting post = Posting.finder.byId(postId);
+        
+        if (post == null) {
+            return notFound(notExistingPage.render("title.post.notExistingPage", project));
+        }
 
         if (!AccessControl.isAllowed(UserApp.currentUser(), post.asResource(), Operation.READ)) {
             return forbidden(unauthorized.render(project));
         }
 
-        if (post == null) {
-            return notFound(notExistingPage.render("title.post.notExistingPage", project));
-        }
-
         Form<PostingComment> commentForm = new Form<PostingComment>(PostingComment.class);
-
         return ok(views.html.board.post.render(post, commentForm, project));
     }
 
