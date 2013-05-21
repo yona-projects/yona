2ac7692684f5459f5276bf01c0c2fa83636fdd1a
Keesun Baik <whiteship@epril.com>
2013-05-21 17:41:54 +0900

    javadoc Assignee class

--- app/models/Assignee.java
+++ app/models/Assignee.java
@@ -11,6 +11,9 @@ import javax.persistence.OneToMany;
 import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
 
+/**
+ * 특정 프로젝트의 이슈 담당자를 나타낸 클래스
+ */
 @Entity
 public class Assignee extends Model {
 
@@ -30,7 +33,7 @@ public class Assignee extends Model {
     @OneToMany(mappedBy = "assignee", cascade = CascadeType.ALL)
     public Set<Issue> issues;
 
-    public static Model.Finder<Long, Assignee> finder = new Finder<Long, Assignee>(Long.class,
+    public static Model.Finder<Long, Assignee> finder = new Finder<>(Long.class,
             Assignee.class);
 
     public Assignee(Long userId, Long projectId) {
@@ -38,6 +41,16 @@ public class Assignee extends Model {
         project = Project.find.byId(projectId);
     }
 
+    /**
+     * 이슈를 새로 만드거나 수정할 때 담당자가 지정되어 있다면 해당 담당자 정보를 저장한다.
+     *
+     * {@code projectId}에 해당하는 프로젝트에 {@code userId}에 해당하는 담당자가
+     * 없을 때에만 새로 추가한다.
+     *
+     * @param userId
+     * @param projectId
+     * @return
+     */
     public static Assignee add(Long userId, Long projectId) {
         Assignee assignee = finder.where()
                 .eq("user.id", userId).eq("project.id", projectId).findUnique();
