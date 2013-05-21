fe306e6dc7d8ab67ae87833d77637c5294702d5e
Keesun Baik <whiteship@epril.com>
2013-05-21 17:22:19 +0900

    javadoc AbstractPosting

--- app/models/AbstractPosting.java
+++ app/models/AbstractPosting.java
@@ -17,6 +17,9 @@ import java.util.List;
 
 import static com.avaje.ebean.Expr.contains;
 
+/**
+ * {@link Posting}과 {@link Issue}의 공통 속성과 메서드를 모아둔 클래스
+ */
 @MappedSuperclass
 abstract public class AbstractPosting extends Model {
     public static final int FIRST_PAGE_NUMBER = 0;
@@ -52,6 +55,11 @@ abstract public class AbstractPosting extends Model {
     // Ebean does NOT sort entities by transient field.
     public int numOfComments;
 
+    /**
+     * {@link Comment} 개수를 반환한다.
+     *
+     * @return
+     */
     abstract public int computeNumOfComments();
 
     public AbstractPosting() {
@@ -60,12 +68,25 @@ abstract public class AbstractPosting extends Model {
 
     protected abstract Finder<Long, ? extends AbstractPosting> getFinder();
 
+    /**
+     * {@link Project}에 속한 {@link Issue} 또는 {@link Posting} 번호를 올린다.
+     *
+     * @return
+     * @see models.Issue#increaseNumber()
+     * @see models.Posting#increaseNumber()
+     */
     protected abstract Long increaseNumber();
 
     public Long getNumber() {
         return number;
     }
 
+    /**
+     * 저장할 때 번호가 설정되어 있지 않다면 번호를 저장하고 댓글 개수를 설정한다.
+     *
+     * @see #increaseNumber()
+     * @see #computeNumOfComments()
+     */
     @Transactional
     public void save() {
         if (number == null) {
@@ -75,11 +96,27 @@ abstract public class AbstractPosting extends Model {
         super.save();
     }
 
+    /**
+     * 갱신할 때 댓글 개수를 설정한다.
+     */
     public void update() {
         numOfComments = computeNumOfComments();
         super.update();
     }
 
+    /**
+     * 특정 {@link Project}에 속한 {@link AbstractPosting} 중에서
+     * title과 body에 특정한 값을 포함하고 있는 것들을 반환한다.
+     *
+     * when: {@link SearchApp#contentsSearch(String, String, int)}에서
+     * 특정 프로젝트에 속한 이슈와 글을 검색할 때 사용한다.
+     *
+     * @param finder
+     * @param project
+     * @param condition
+     * @param <T>
+     * @return
+     */
     public static <T> Page<T> find(Finder<Long, T> finder, Project project, SearchApp.ContentSearchCondition condition) {
         String filter = condition.filter;
         return finder.where().eq("project.id", project.id)
@@ -89,6 +126,7 @@ abstract public class AbstractPosting extends Model {
 
     /**
      * 현재 글을 쓴지 얼마나 되었는지를 얻어내는 함수
+     *
      * @return
      */
     public Duration ago() {
@@ -121,6 +159,13 @@ abstract public class AbstractPosting extends Model {
 	    };
     }
 
+    /**
+     * 이슈나 글쓴이에 관한 정보를 저장한다.
+     *
+     * when: 새 글이나 새 이슈를 등록할 때 사용한다.
+     *
+     * @param user
+     */
     @Transient
     public void setAuthor(User user) {
         authorId = user.id;
@@ -128,8 +173,16 @@ abstract public class AbstractPosting extends Model {
         authorName = user.name;
     }
 
+    /**
+     * 이슈나 글에 달려있는 댓글을 반환한다.
+     *
+     * @return
+     */
     abstract public List<? extends Comment> getComments();
 
+    /**
+     * 삭제할 때 이슈나 글에 달려있는 댓글과 첨부파일을 삭제한다.
+     */
     public void delete() {
         for (Comment comment: getComments()) {
             comment.delete();
