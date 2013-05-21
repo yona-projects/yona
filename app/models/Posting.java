b0a804ba6d59218b66325f4c7ab3a19c7ffbf20d
Keesun Baik <whiteship@epril.com>
2013-05-21 17:46:30 +0900

    javadoc Posting class

--- app/models/Posting.java
+++ app/models/Posting.java
@@ -13,6 +13,9 @@ import java.util.*;
 
 import static com.avaje.ebean.Expr.eq;
 
+/**
+ * 게시물을 나타내는 클래스
+ */
 @Entity
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = {"project_id", "number"}))
 public class Posting extends AbstractPosting {
@@ -25,15 +28,29 @@ public class Posting extends AbstractPosting {
     @OneToMany(cascade = CascadeType.ALL)
     public List<PostingComment> comments;
 
+    /**
+     * @return
+     * @see models.AbstractPosting#getFinder()
+     */
     public Finder<Long, ? extends AbstractPosting> getFinder() {
         return finder;
     }
 
+    /**
+     * {@link Project}의 최근 게시물 번호를 반환한다.
+     *
+     * @return
+     * @see models.Project#increaseLastPostingNumber()
+     */
     @Override
     protected Long increaseNumber() {
         return project.increaseLastPostingNumber();
     }
 
+    /**
+     * @return
+     * @see models.AbstractPosting#computeNumOfComments()
+     */
     public int computeNumOfComments() {
         return comments.size();
     }
@@ -46,6 +63,14 @@ public class Posting extends AbstractPosting {
         return asResource(ResourceType.BOARD_POST);
     }
 
+    /**
+     * 게시물 중에서 공지글 목록을 반환한다.
+     *
+     * when: 게시물 목록 화면을 보여줄 때 사용한다.
+     *
+     * @param project
+     * @return
+     */
     public static List<Posting> findNotices(Project project) {
         return Posting.finder.where()
                 .eq("project.id", project.id)
@@ -54,6 +79,15 @@ public class Posting extends AbstractPosting {
                 .findList();
     }
 
+    /**
+     * {@link Project}에 게시된 최근 게시물을 {@code size} 만큼 조회한다.
+     *
+     * when: 프로젝트 overview 화면에서 최근 활동 내역을 보여줄 때 사용한다.
+     *
+     * @param project
+     * @param size
+     * @return
+     */
     public static List<Posting> findRecentlyUpdated(Project project, int size) {
         return Posting.finder.where()
                 .eq("project.id", project.id)
@@ -62,6 +96,10 @@ public class Posting extends AbstractPosting {
                 .getList();
     }
 
+    /**
+     * @return
+     * @see models.AbstractPosting#getComments()
+     */
     @Transient
     public List<? extends Comment> getComments() {
         return comments;
