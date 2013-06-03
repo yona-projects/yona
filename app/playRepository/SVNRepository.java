yona-projects/yona

commit 575192db2bb4f189a4b18cb37b53f0fa7435f368

code/svn: Fix a bug that history cannot be shown.

app/playRepository/SVNRepository.java
@@ -216,7 +216,7 @@ public class SVNRepository implements PlayRepository {
         long startRevision = repository.getLatestRevision();
         long endRevision = startRevision - limit;
         if (endRevision < 1) {
-            return new ArrayList<Commit>();
+            endRevision = 1;
         }
 
         // Get the logs

commit 6bd6f3baf2fb518a4b5e9ec3ba11f495fee6a069

project: Fix an error in svn project's Overview.
The error, is caused by 575192d, occurs while accessing Overview page of
a project which has empty svn repository.

This fixes #146.

app/playRepository/SVNRepository.java
@@ -219,6 +219,11 @@ public class SVNRepository implements PlayRepository {
             endRevision = 1;
         }
 
+        // No log to return.
+        if (startRevision < endRevision) {
+            return new ArrayList<Commit>();
+        }
+
         // Get the logs
         List<Commit> result = new ArrayList<Commit>();
         for(Object entry : repository.log(paths, null, startRevision, endRevision, false, false)) {