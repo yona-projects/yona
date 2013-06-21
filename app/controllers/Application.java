yona-projects/yona

commit a35f74d1e1f422c73e8c61cbbf8bb54fd0ca670e

remove trailing slash in url path
If there is a trailing slash, then remove it and redirect

app/controllers/Application.java
@@ -30,6 +30,15 @@ public class Application extends Controller {
         return ok(index.render(null, null));
     }
 
+    public static Result RemoveTrailer(String paths){
+        String path = request().path();
+        if( path.charAt(path.length()-1) == '/' ) {
+            path = path.substring(0, path.length() - 1);
+        }
+        Logger.debug("trailing slash removed and redirected: " + request().path() + " to " + path );
+        return redirect(path);
+    }
+
     public static Result init() {
         makeUploadFolder();
         makeTestRepository();

conf/routes
@@ -6,7 +6,6 @@ GET     /navitest                                       controllers.Application.
 
 # Home page
 GET     /                                               controllers.Application.index()
-
 # Map static resources from the /public folder to the /assets URL path
 GET     /assets/*file                                   controllers.Assets.at(path="/public", file)
 GET     /messages.js                                    controllers.Application.jsMessages()
@@ -80,7 +79,6 @@ GET     /projectform                                    controllers.ProjectApp.n
 POST    /projects                                       controllers.ProjectApp.newProject()
 GET     /projects                                       controllers.ProjectApp.projects(filter:String ?= "", state: String ?= "all", pageNum: Int ?= 1)
 GET     /:user/:project                                 controllers.ProjectApp.project(user, project)
-GET     /:user/:project/                                controllers.ProjectApp.project(user, project)
 GET     /:user/:project/settingform                     controllers.ProjectApp.settingForm(user, project)
 POST    /:user/:project/setting                         controllers.ProjectApp.settingProject(user, project)
 GET     /:user/:project/deleteform                      controllers.ProjectApp.deleteForm(user, project)
@@ -177,8 +175,10 @@ GET     /help                                           controllers.HelpApp.help
 # Statistics
 GET     /:user/:project/statistics                      controllers.StatisticsApp.statistics(user, project)
 GET     /:user                                          controllers.UserApp.userInfo(user)
-GET     /:user/                                         controllers.UserApp.userInfo(user)
 
 # Watching Projects
 POST    /:user/:project/watch                           controllers.WatchProjectApp.watch(user, project)
 POST    /:user/:project/unwatch                         controllers.WatchProjectApp.unwatch(user, project)
+
+# remove trailing slash
+GET     /*paths                                         controllers.Application.RemoveTrailer(paths)

commit f34a51e3542b0c2637dbf354b4c1b231973ea434

fix: url redirec loop which caused by a35f74d

app/controllers/Application.java
@@ -7,6 +7,7 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import playRepository.RepositoryService;
 //import views.html.code.codeNavi;
+import views.html.error.notfound_default;
 import views.html.index;
 
 import java.io.File;
@@ -34,8 +35,11 @@ public class Application extends Controller {
         String path = request().path();
         if( path.charAt(path.length()-1) == '/' ) {
             path = path.substring(0, path.length() - 1);
+        } else {
+            Logger.error("Unexpected url call : " + request().path());
+            return notFound(notfound_default.render("error.notfound", path));
         }
-        Logger.debug("trailing slash removed and redirected: " + request().path() + " to " + path );
+        Logger.debug("Trailing slash removed and redirected: " + request().path() + " to " + path );
         return redirect(path);
     }
 