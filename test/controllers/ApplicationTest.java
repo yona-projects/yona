552bdab388b14a07e0733249b8a37faedd300d1b
Yi EungJun <semtlenori@gmail.com>
2013-06-27 10:28:36 +0900

    Remove 'Powered By' test from ApplicationTest.
    The root page may not have the phrase.

--- test/controllers/ApplicationTest.java
+++ test/controllers/ApplicationTest.java
@@ -23,7 +23,7 @@ public class ApplicationTest {
 		        assertThat(status(result)).isEqualTo(OK);
 		        assertThat(contentType(result)).isEqualTo("text/html");
 		        assertThat(charset(result)).isEqualTo("utf-8");
-		        assertThat(contentAsString(result)).contains("Powered by");
+                assertThat(status(result)).isEqualTo(OK);
     		}
     	});
     }
