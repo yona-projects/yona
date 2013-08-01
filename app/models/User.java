c822260452f5751f3bc3f8aa59a2bbe951250626
whiteship <whiteship@epril.com>
2013-08-01 17:13:20 +0900

    user: sort member list by name asc

--- app/models/User.java
+++ app/models/User.java
@@ -271,6 +271,7 @@ public class User extends Model {
     public static List<User> findUsersByProject(Long projectId) {
         return find.where().eq("projectUser.project.id", projectId)
                 .ne("projectUser.role.id", RoleType.SITEMANAGER.roleType())
+                .orderBy().asc("name")
                 .findList();
     }
 
