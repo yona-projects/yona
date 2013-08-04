7b6b048c4e2c818dd439e58113600e22e6ad727d
Suwon Chae <doortts@gmail.com>
2013-08-05 03:56:22 +0900

    remove unused lib

--- project/Build.scala
+++ project/Build.scala
@@ -17,7 +17,6 @@ object ApplicationBuild extends Build {
     javaJdbc,
     javaEbean,
       // Add your project dependencies here,
-      "mysql" % "mysql-connector-java" % "5.1.18",
       "postgresql" % "postgresql" % "9.1-901.jdbc4",
       // Core Library
       "org.eclipse.jgit" % "org.eclipse.jgit" % "2.3.1.201302201838-r",
