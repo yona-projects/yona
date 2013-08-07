5bdc48e4c16c31a73db43ff3d826b4779a0b6a3d
Yi EungJun <semtlenori@gmail.com>
2013-08-07 14:11:52 +0900

    notification: Add More button for pagination.

---
+++ app/controllers/NotificationApp.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import models.NotificationEvent;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static play.libs.Json.toJson;
+
+public class NotificationApp extends Controller {
+    public static Result notifications(int from, int limit) {
+        return ok(views.html.partial_notifications.render(from, limit));
+    }
+}
