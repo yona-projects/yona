yona-projects/yona

commit 5e47389fcedcdf2adda1f17a12ca69ce3406aecb

Introduce scheduled notification by Akka.

app/controllers/AbstractPostingApp.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import info.schleichardt.play2.mailplugin.Mailer;
+import models.enumeration.NotificationType;
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.HtmlEmail;
@@ -24,6 +25,7 @@ import utils.Config;
 import utils.Constants;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.Callable;
@@ -88,8 +90,9 @@ public class AbstractPostingApp extends Controller {
         }
     }
 
-    protected static class NotificationFactory {
-        public static Notification create(final Set<User> receivers, final String title, final String message, final String urlToView) {
+    public static class NotificationFactory {
+        public static Notification create(final Set<User> receivers, final String title,
+                                          final String message, final String urlToView) {
             return new AbstractNotification() {
                 public String getTitle() {
                     return title;
@@ -111,7 +114,6 @@ public class AbstractPostingApp extends Controller {
             };
         }
     }
-
 
     /**
      * 새 댓글 저장 핸들러
@@ -142,10 +144,20 @@ public class AbstractPostingApp extends Controller {
         AbstractPosting post = comment.getParent();
         String title = String.format("Re: [%s] %s (#%d)", post.project.name, post.title, post.getNumber());
         Set<User> watchers = post.getWatchers();
-        watchers.addAll(getMentionedUsers(comment.contents));
-        Notification noti = NotificationFactory.create(watchers, title, comment.contents,
-                toView.absoluteURL(request()));
-        sendNotification(noti);
+        watchers.remove(UserApp.currentUser());
+
+        NotificationEvent notiEvent = new NotificationEvent();
+        notiEvent.created = new Date();
+        notiEvent.title = title;
+        notiEvent.message = comment.contents;
+        notiEvent.receivers = watchers;
+        notiEvent.urlToView = toView.absoluteURL(request());
+        notiEvent.resourceId = comment.id;
+        notiEvent.resourceType = comment.asResource().getType();
+        notiEvent.type = NotificationType.NEW_COMMENT;
+        notiEvent.oldValue = null;
+        notiEvent.newValue = comment.contents;
+        notiEvent.save();
 
         return redirect(toView);
     }
@@ -156,17 +168,15 @@ public class AbstractPostingApp extends Controller {
      * @param noti
      * @see <a href="https://github.com/nforge/hive/blob/master/docs/technical/watch.md>watch.md</a>
      */
-    protected static void sendNotification(Notification noti) {
+    public static void sendNotification(Notification noti) {
         final HtmlEmail email = new HtmlEmail();
 
         try {
-            User sender = UserApp.currentUser();
-            email.setFrom(sender.email, sender.name);
             play.Configuration config = play.Configuration.root();
+            email.setFrom(config.getString("smtp.user") + "@" + config.getString("smtp" + ".domain"));
             email.addTo(config.getString("smtp.user") + "@" + config.getString("smtp.domain"));
             Set<User> receivers = noti.getReceivers();
             receivers.remove(User.anonymous);
-            receivers.remove(UserApp.currentUser());
             if(receivers.isEmpty()) {
                 return;
             }
@@ -177,24 +187,14 @@ public class AbstractPostingApp extends Controller {
             email.setHtmlMsg(noti.getHtmlMessage());
             email.setTextMsg(noti.getPlainMessage());
             email.setCharset("utf-8");
+            Mailer.send(email);
+            String escapedTitle = email.getSubject().replace("\"", "\\\"");
+            String logEntry = String.format("\"%s\" %s", escapedTitle, email.getBccAddresses());
+            play.Logger.of("mail").info(logEntry);
         } catch (Exception e) {
             Logger.warn("Failed to send a notification: "
                     + email + "\n" + ExceptionUtils.getStackTrace(e));
         }
-
-        Callable<Object> sendingMail = new Callable<Object>() {
-            public Object call() throws EmailException {
-                try {
-                    Mailer.send(email);
-                } catch (Exception e) {
-                    Logger.warn("Failed to send a notification: "
-                            + email + "\n" + ExceptionUtils.getStackTrace(e));
-                }
-                return null;
-            }
-        };
-
-        Akka.future(sendingMail);
     }
 
     /**

app/Global.java
@@ -1,37 +1,33 @@
 import java.io.File;
-import java.io.FileInputStream;
 import java.lang.reflect.Method;
 import java.math.BigInteger;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.security.SecureRandom;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 import com.typesafe.config.ConfigFactory;
-import models.Issue;
-import models.Posting;
-import models.Project;
-import models.User;
+import controllers.AbstractPostingApp;
+import models.*;
 
 import com.avaje.ebean.Ebean;
 
 import controllers.routes;
+import org.joda.time.DateTime;
 import play.Application;
 import play.GlobalSettings;
 import play.Configuration;
 import play.api.mvc.Handler;
-import play.i18n.Messages;
+import play.libs.Akka;
 import play.libs.Yaml;
 import play.mvc.Action;
 import play.mvc.Http;
 import play.mvc.Http.RequestHeader;
 import play.mvc.Result;
 
+import scala.concurrent.duration.Duration;
 import utils.AccessLogger;
 
 import play.data.DynamicForm;
@@ -61,6 +57,53 @@ public class Global extends GlobalSettings {
         if (app.isTest()) {
             insertTestData();
         }
+
+        scheduleNotificationByMail();
+    }
+
+    private void scheduleNotificationByMail() {
+        final Long MAIL_NOTIFICATION_INITDELAY_IN_MILLIS = Configuration.root()
+                .getMilliseconds("application.notification.bymail.initdelay", 5000L);
+        final Long MAIL_NOTIFICATION_INTERVAL_IN_MILLIS = Configuration.root()
+                .getMilliseconds("application.notification.bymail.interval", 60000L);
+        final int MAIL_NOTIFICATION_DELAY_IN_MILLIS = Configuration.root()
+                .getMilliseconds("application.notification.bymail.delay", 180000L).intValue();
+
+        Akka.system().scheduler().schedule(
+            Duration.create(MAIL_NOTIFICATION_INITDELAY_IN_MILLIS, TimeUnit.MILLISECONDS),
+            Duration.create(MAIL_NOTIFICATION_INTERVAL_IN_MILLIS, TimeUnit.MILLISECONDS),
+            new Runnable() {
+                public void run() {
+                    try {
+                        sendMail();
+                    } catch (Exception e) {
+                        play.Logger.warn(e.getMessage());
+                    }
+                }
+
+                private void sendMail() {
+                    Date sinceDate = DateTime.now().minusMillis
+                            (MAIL_NOTIFICATION_DELAY_IN_MILLIS).toDate();
+                    List<NotificationMail> mails = NotificationMail.find.where()
+                                    .lt("notificationEvent.created", sinceDate)
+                                    .orderBy("notificationEvent.created ASC").findList();
+
+                    for (NotificationMail mail: mails) {
+                        if (mail.notificationEvent.resourceExists()) {
+                            AbstractPostingApp.sendNotification(
+                                    AbstractPostingApp.NotificationFactory.create(
+                                            mail.notificationEvent.receivers,
+                                            mail.notificationEvent.title,
+                                            mail.notificationEvent.message,
+                                            mail.notificationEvent.urlToView
+                                    ));
+                        }
+                        mail.delete();
+                    }
+                }
+            },
+            Akka.system().dispatcher()
+        );
     }
 
     private void validateSecret() {

app/models/NotificationEvent.java
@@ -1,0 +1,82 @@
+package models;
+
+import models.enumeration.NotificationType;
+import models.enumeration.ResourceType;
+import play.db.ebean.Model;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nori
+ * Date: 13. 7. 3
+ * Time: 오전 11:20
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+public class NotificationEvent extends Model {
+    @Id
+    public Long id;
+
+    public static Finder<Long, NotificationEvent> find = new Finder<Long,
+            NotificationEvent>(Long.class, NotificationEvent.class);
+
+    public String title;
+    public String message;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    public Set<User> receivers;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    public Date created;
+
+    public String urlToView;
+
+    @Enumerated(EnumType.STRING)
+    public ResourceType resourceType;
+
+    public Long resourceId;
+
+    @Enumerated(EnumType.STRING)
+    public NotificationType type;
+
+    public String oldValue;
+
+    public String newValue;
+
+    @OneToOne(mappedBy="notificationEvent", cascade = CascadeType.ALL)
+    public NotificationMail notificationMail;
+
+    public boolean resourceExists() {
+        //To change body of created methods use File | Settings | File Templates.
+
+        Finder<Long, ? extends Model> finder = null;
+
+        switch(resourceType) {
+            case ISSUE_COMMENT:
+                finder = IssueComment.find;
+                break;
+            case NONISSUE_COMMENT:
+                finder = IssueComment.find;
+                break;
+            case ISSUE_POST:
+                finder = Issue.finder;
+                break;
+            default:
+                play.Logger.warn("Unknown resource type " + resourceType);
+        }
+
+        return finder.byId(resourceId) != null;
+    }
+
+    @Override
+    public void save() {
+        if (notificationMail == null) {
+            notificationMail = new NotificationMail();
+            notificationMail.notificationEvent = this;
+        }
+        super.save();
+    }
+}

app/models/NotificationMail.java
@@ -1,0 +1,26 @@
+package models;
+
+import play.db.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nori
+ * Date: 13. 7. 10
+ * Time: 오전 11:19
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+public class NotificationMail extends Model {
+    @Id
+    public Long id;
+
+    @OneToOne
+    public NotificationEvent notificationEvent;
+
+    public static Finder<Long, NotificationMail> find = new Finder<>(Long.class,
+            NotificationMail.class);
+}

app/models/User.java
@@ -136,6 +136,9 @@ public class User extends Model {
     )
     public List<Project> enrolledProjects;
 
+    @ManyToMany(mappedBy = "receivers")
+    public Set<NotificationEvent> notificationEvents;
+
     public User(){}
 
     public User(Long id){

app/models/enumeration/NotificationType.java
@@ -1,0 +1,12 @@
+package models.enumeration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nori
+ * Date: 13. 7. 3
+ * Time: 오전 11:26
+ * To change this template use File | Settings | File Templates.
+ */
+public enum NotificationType {
+    NEW_COMMENT
+}

conf/application-logger.xml
@@ -16,6 +16,13 @@
         </encoder>
     </appender>
 
+    <appender name="MAIL_LOG_FILE" class="ch.qos.logback.core.FileAppender">
+        <file>${application.home}/logs/mail.log</file>
+        <encoder>
+            <pattern>%date - [%level] - from %logger in %thread %n%message%n%xException%n</pattern>
+        </encoder>
+    </appender>
+
     <appender name="ROOT_LOG_FILE" class="ch.qos.logback.core.FileAppender">
         <file>${application.home}/logs/root.log</file>
         <encoder>
@@ -44,6 +51,10 @@
 
     <logger name="access" level="DEBUG">
         <appender-ref ref="ACCESS_LOG_FILE" />
+    </logger>
+
+    <logger name="mail" level="DEBUG">
+        <appender-ref ref="MAIL_LOG_FILE" />
     </logger>
 
 </configuration>

conf/evolutions/default/22.sql
@@ -1,0 +1,51 @@
+# --- !Ups
+
+create table notification_event (
+  id                        bigint not null,
+  title                     varchar(255),
+  message                   varchar(255),
+  created                   timestamp,
+  url_to_view               varchar(255),
+  resource_type             varchar(16),
+  resource_id               bigint,
+  type                      varchar(11),
+  old_value                 varchar(255),
+  new_value                 varchar(255),
+  constraint ck_notification_event_resource_type check (resource_type in ('ISSUE_POST','ISSUE_ASSIGNEE','ISSUE_STATE','ISSUE_CATEGORY','ISSUE_MILESTONE','ISSUE_LABEL','BOARD_POST','BOARD_CATEGORY','BOARD_NOTICE','CODE','MILESTONE','WIKI_PAGE','PROJECT_SETTING','SITE_SETTING','USER','USER_AVATAR','PROJECT','ATTACHMENT','ISSUE_COMMENT','NONISSUE_COMMENT','LABEL','PROJECT_LABELS','FORK')),
+  constraint ck_notification_event_type check (type in ('NEW_COMMENT')),
+  constraint pk_notification_event primary key (id));
+
+create table notification_event_n4user (
+  notification_event_id          bigint not null,
+  n4user_id                      bigint not null,
+  constraint pk_notification_event_n4user primary key (notification_event_id, n4user_id));
+
+create table notification_mail (
+  id                        bigint not null,
+  notification_event_id     bigint,
+  constraint pk_notification_mail primary key (id))
+;
+
+create sequence notification_event_seq;
+
+alter table notification_event_n4user add constraint fk_notification_event_n4user__01 foreign key (notification_event_id) references notification_event (id) on delete restrict on update restrict;
+
+alter table notification_event_n4user add constraint fk_notification_event_n4user__02 foreign key (n4user_id) references n4user (id) on delete restrict on update restrict;
+
+create sequence notification_mail_seq;
+
+alter table notification_mail add constraint fk_notification_mail_notificat_9 foreign key (notification_event_id) references notification_event (id) on delete restrict on update restrict;
+
+create index ix_notification_mail_notificat_9 on notification_mail (notification_event_id);
+
+# --- !Downs
+
+drop table if exists notification_event;
+
+drop table if exists notification_event_n4user;
+
+drop sequence if exists notification_event_seq;
+
+drop table if exists notification_mail;
+
+drop sequence if exists notification_mail_seq;

commit 738e98e680449239f0b0030013a66d8be1de29c3

Fix sending a notification to mentioned users.
The feature was removed by 5e47389 accidentally.

app/controllers/AbstractPostingApp.java
@@ -145,6 +145,7 @@ public class AbstractPostingApp extends Controller {
     private static void addNotificationEventFromNewComment(Comment comment, Call toView) {
         AbstractPosting post = comment.getParent();
         Set<User> watchers = post.getWatchers();
+        watchers.addAll(getMentionedUsers(comment.contents));
         watchers.remove(UserApp.currentUser());
 
         NotificationEvent notiEvent = new NotificationEvent();