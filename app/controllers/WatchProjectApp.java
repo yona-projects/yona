yona-projects/yona

commit c81c0b3fb9a20dc84ac2f8c46fc7f445c9bdfdb5

issue: Introduce Issue Event.
An issue event is a log for assigning or state-changing. The events are
listed in order by date along with comments for an issue you see.

* Add IssueEvent model
* Add TimelineItem interface and make Comment and IssueEvent to implement
  it to treat them in the same way while listing them for an issue.
* Add "application.issue-event.draft-time" configuration property.

conf/evolutions/default/33.sql
@@ -1,5 +1,23 @@
 # --- !Ups
-ALTER TABLE notification_event ALTER COLUMN notification_type RENAME TO event_type;
+create table issue_event (
+  id                        bigint not null,
+  created                   timestamp,
+  sender_login_id           varchar(255),
+  issue_id                  bigint,
+  event_type         varchar(26),
+  old_value                 varchar(255),
+  new_value                 varchar(255),
+  constraint ck_issue_event_event_type check (event_type in ('NEW_ISSUE','NEW_POSTING','ISSUE_ASSIGNEE_CHANGED','ISSUE_STATE_CHANGED','NEW_COMMENT','NEW_PULL_REQUEST','NEW_SIMPLE_COMMENT','PULL_REQUEST_STATE_CHANGED')),
+  constraint pk_issue_event primary key (id))
+;
+
+create sequence issue_event_seq;
+
+alter table issue_event add constraint fk_issue_event_issue_8 foreign key (issue_id) references issue (id) on delete restrict on update restrict;
+
+create index ix_issue_event_issue_8 on issue_event (issue_id);
 
 # --- !Downs
-ALTER TABLE notification_event ALTER COLUMN event_type RENAME TO notification_type;
+drop table if exists issue_event;
+
+drop sequence if exists issue_event_seq;

app/assets/stylesheets/less/_page.less
@@ -2271,6 +2271,15 @@
                 margin-top: 10px;
             }
         }
+
+        .event {
+            padding: 10px 20px 10px 69px; /* 69px for aligning with comments */
+            border-bottom:1px solid #ececec;
+            .date {
+                font-size: 11px;
+                color: #aaa;
+            }
+        }
     }
 
 }

app/controllers/IssueApp.java
@@ -644,6 +644,8 @@ public class IssueApp extends AbstractPostingApp {
         notiEvent.eventType = EventType.ISSUE_STATE_CHANGED;
 
         NotificationEvent.add(notiEvent);
+
+        IssueEvent.addFromNotificationEvent(notiEvent, updatedIssue, UserApp.currentUser().loginId);
     }
 
     /**
@@ -681,6 +683,8 @@ public class IssueApp extends AbstractPostingApp {
         notiEvent.eventType = EventType.ISSUE_ASSIGNEE_CHANGED;
 
         NotificationEvent.add(notiEvent);
+
+        IssueEvent.addFromNotificationEvent(notiEvent, updatedIssue, UserApp.currentUser().loginId);
     }
 
     /*

app/models/Comment.java
@@ -19,7 +19,7 @@ import java.util.*;
  * 이슈 혹은 게시글의, 댓글
  */
 @MappedSuperclass
-abstract public class Comment extends Model {
+abstract public class Comment extends Model implements TimelineItem {
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -116,5 +116,9 @@ abstract public class Comment extends Model {
                 return o1.createdDate.compareTo(o2.createdDate);
             }
         };
+    }
+
+    public Date getDate() {
+        return createdDate;
     }
 }

app/models/Issue.java
@@ -71,6 +71,9 @@ public class Issue extends AbstractPosting implements LabelOwner {
     @OneToMany(cascade = CascadeType.ALL, mappedBy="issue")
     public List<IssueComment> comments;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy="issue")
+    public List<IssueEvent> events;
+
     /**
      * @return
      * @see models.AbstractPosting#computeNumOfComments()
@@ -429,5 +432,27 @@ public class Issue extends AbstractPosting implements LabelOwner {
     @Override
     public Set<IssueLabel> getLabels() {
         return labels;
+    }
+
+    /**
+     * 이 이슈의 타임라인을 얻는다.
+     *
+     * 타임라인이란 어떤 이슈에 대한 댓글들과 변경 내역(상태 변경, 담당자 변경)들의 시간순으로 정렬된 목록이다.
+     *
+     * when: 이슈 하나의 내용을 보여줄 때
+     *
+     * @return 타임라인
+     */
+    public List<TimelineItem> getTimeline() {
+        List<TimelineItem> timelineItems = new ArrayList<>();
+        timelineItems.addAll(comments);
+        timelineItems.addAll(events);
+        Collections.sort(timelineItems, new Comparator<TimelineItem>() {
+            @Override
+            public int compare(TimelineItem o1, TimelineItem o2) {
+                return o1.getDate().compareTo(o2.getDate());
+            }
+        });
+        return timelineItems;
     }
 }

app/models/IssueEvent.java
@@ -1,0 +1,107 @@
+package models;
+
+import controllers.UserApp;
+import models.enumeration.EventType;
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+import play.Configuration;
+import play.db.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+@Entity
+public class IssueEvent extends Model implements TimelineItem {
+    @Id
+    public Long id;
+
+    public Date created;
+
+    public String senderLoginId;
+
+    @ManyToOne
+    public Issue issue;
+
+    public EventType eventType;
+
+    public String oldValue;
+
+    public String newValue;
+
+    private static final int DRAFT_TIME_IN_MILLIS = Configuration.root()
+        .getMilliseconds("application.issue-event.draft-time", 30 * 1000L).intValue();
+
+    public static Finder<Long, IssueEvent> find = new Finder<Long, IssueEvent>(Long.class,
+            IssueEvent.class);
+
+    /**
+     * {@code event}를 추가한다.
+     *
+     * 직전에 추가된 이벤트가 현재 추가하려고 하는 이벤트가 같은 종류이며, 직전 이벤트가 추가된 지
+     * {@link #DRAFT_TIME_IN_MILLIS}밀리초가 지나지 않았다면 불필요한 이벤트를 최대한 줄이기 위해, 이전
+     * 이벤트와 이 이벤트를 합치거나 상황에 따라서는 두 이벤트를 모두 삭제한다.
+     *
+     * - 대체되는 경우의 예: 담당자가 A에서 B로 바뀐 뒤, 다시 B에서 C로 바뀌었다면,
+     *                       두 이벤트는 담당자가 A에서 C로 바뀐 이벤트로 합쳐진다.
+     * - 삭제되는 경우의 예: 담당자가 A에서 B로 바뀐 뒤, 다시 B에서 A로 바뀌었다면, 두 이벤트는 삭제된다.
+     *
+     * 비고: 이 메소드는 {@link NotificationEvent#add(NotificationEvent)}를 가져와서 필요에 따라 약간
+     * 수정한 것이다.
+     *
+     * @param event
+     */
+    public static void add(IssueEvent event) {
+        Date draftDate = DateTime.now().minusMillis(DRAFT_TIME_IN_MILLIS).toDate();
+
+        IssueEvent lastEvent = IssueEvent.find.where()
+                .eq("issue.id", event.issue.id)
+                .gt("created", draftDate)
+                .orderBy("id desc").setMaxRows(1).findUnique();
+
+        if (lastEvent != null) {
+            if (lastEvent.eventType == event.eventType &&
+                    StringUtils.equals(event.senderLoginId, lastEvent.senderLoginId)) {
+                // A -> B, B -> C ==> A -> C
+                event.oldValue = lastEvent.oldValue;
+                lastEvent.delete();
+
+                // A -> B, B -> A ==> remove all of them
+                if (StringUtils.equals(event.oldValue, event.newValue)) {
+                    // No need to add this event because the event just cancels the last event
+                    // which has just been deleted.
+                    return;
+                }
+            }
+        }
+
+        event.save();
+    }
+
+    /**
+     * 주어진 {@code notiEvent}, {@code updatedIssue}, {@code senderLoginId}를 바탕으로 새로운 이슈
+     * 이벤트를 만들어 추가한다.
+     *
+     * @param notiEvent
+     * @param updatedIssue
+     * @param senderLoginId
+     * @see {@link #add(IssueEvent)}
+     */
+    public static void addFromNotificationEvent(NotificationEvent notiEvent, Issue updatedIssue,
+                                                String senderLoginId) {
+        IssueEvent event = new IssueEvent();
+        event.created = notiEvent.created;
+        event.senderLoginId = senderLoginId;
+        event.issue = updatedIssue;
+        event.eventType = notiEvent.eventType;
+        event.oldValue = notiEvent.oldValue;
+        event.newValue = notiEvent.newValue;
+        add(event);
+    }
+
+    @Override
+    public Date getDate() {
+        return created;
+    }
+}

app/models/TimelineItem.java
@@ -1,0 +1,7 @@
+package models;
+
+import java.util.Date;
+
+public interface TimelineItem {
+    public Date getDate();
+}

app/views/issue/view.scala.html
@@ -8,6 +8,7 @@
 @import java.security.MessageDigest
 @import utils.TemplateHelper._
 @import utils.AccessControl._
+@import utils.JodaDateUtil
 @import java.net._
 @import utils.Config
 
@@ -20,6 +21,17 @@
 @isFirstState(state:State) = {@if(issue.state.state == Issue.availableStates.get(0).state()){dirty}}
 
 @urlToIssues = @{ urlToList(request.getHeader("Referer"), routes.IssueApp.issues(project.owner, project.name, "open").toString) }
+
+@avatarByLoginId(loginId: String) = {
+    <img src="@User.findByLoginId(loginId).avatarUrl" class="avatar-wrap smaller" alt="@loginId">
+}
+
+@linkToUser(loginId: String, showAvatar: Boolean = false) = {
+    @loginId match {
+    case (loginId: String) => { <a href="@routes.UserApp.userInfo(loginId)" class="usf-group">@if(showAvatar){@avatarByLoginId(loginId)}<strong>@loginId</strong></a> }
+    case _ => { Anonymous }
+    }
+}
 
 @main(getTitle(issue, project).toString(), project, utils.MenuType.ISSUE){
 <div class="page board-view">
@@ -236,9 +248,11 @@
     <div id="comments" class="board-comment-wrap">
         <div class="comment-header"><strong>@Messages("comment")</strong> <strong class="num">@issue.comments.size</strong></div>
 
-        @if(issue.comments.size > 0) {
+        @if(issue.comments.size + issue.events.size > 0) {
         <ul class="comments">
-        @for(comment <- issue.comments){
+        @for(item <- issue.getTimeline){
+            @item match {
+            case (comment: Comment) => {
             <li class="comment" id="comment-@comment.id">
                 <a href="@routes.UserApp.userInfo(comment.authorLoginId)" class="avatar-wrap pull-left mt20">
                     <img src="@User.findByLoginId(comment.authorLoginId).avatarUrl" width="32" height="32" alt="@comment.authorLoginId">
@@ -256,6 +270,29 @@
                 </div>
             </li>
             }
+            case (event: IssueEvent) => {
+            <li class="event" id="event-@event.id">
+                @event.eventType match {
+                case EventType.ISSUE_STATE_CHANGED => {
+                <span class="label @if(event.newValue.equals("open")){label-info}">@Messages("issue.state." + event.newValue)</span> @Html(Messages("issue.event." + event.newValue, linkToUser(event.senderLoginId)))
+                <!-- @Html(Messages("issue.event." + event.newValue, linkToUser(event.senderLoginId))) -->
+                }
+                case EventType.ISSUE_ASSIGNEE_CHANGED => {
+                @Html(Messages(
+                event.newValue match {
+                case assignee: String => "issue.event.assigned"
+                case _ => "issue.event.unassigned"
+                }, linkToUser(event.senderLoginId), linkToUser(event.newValue, true)))
+                }
+                case _ => {
+                    @event.newValue by @linkToUser(event.senderLoginId)
+                }
+                }
+                <span class="date"><a href="#event-@event.id">@utils.TemplateHelper.agoString(JodaDateUtil.ago(event.getDate()))</a></span>
+            </li>
+            }
+            }
+        }
         </ul>
         }
 

commit 135fa8781e07247a00d0a060fadeef3891a8cc1e

Fix errors caused by c81c0b3.
* Rename NotificationType to EventType
* Copy 33.sql from 3196021.

app/controllers/WatchProjectApp.java
@@ -4,7 +4,7 @@ import models.Project;
 import models.User;
 import models.UserProjectNotification;
 import models.Watch;
-import models.enumeration.NotificationType;
+import models.enumeration.EventType;
 import models.enumeration.ResourceType;
 import org.codehaus.jackson.node.ObjectNode;
 import play.i18n.Messages;
@@ -53,7 +53,7 @@ public class WatchProjectApp extends Controller {
     }
 
     public static Result toggle(Long projectId, String notificationType) {
-        NotificationType notiType = NotificationType.valueOf(notificationType);
+        EventType notiType = EventType.valueOf(notificationType);
         Project project = Project.find.byId(projectId);
         if(project == null) {
             return notFound(ErrorViews.NotFound.render("No project matches given id '" + projectId + "'"));
@@ -66,7 +66,7 @@ public class WatchProjectApp extends Controller {
         }
 
         UserProjectNotification upn = UserProjectNotification.findOne(user, project, notiType);
-        if(upn == null) { // make the NotificationType OFF, because default is ON.
+        if(upn == null) { // make the EventType OFF, because default is ON.
             UserProjectNotification.unwatchExplictly(user, project, notiType);
         } else {
             upn.toggle();

app/models/UserProjectNotification.java
@@ -1,6 +1,6 @@
 package models;
 
-import models.enumeration.NotificationType;
+import models.enumeration.EventType;
 import play.db.ebean.Model;
 
 import javax.persistence.*;
@@ -31,16 +31,16 @@ public class UserProjectNotification extends Model {
     public Project project;
 
     @Enumerated(EnumType.STRING)
-    public NotificationType notificationType;
+    public EventType notificationType;
 
     public boolean allowed;
 
-    public static Map<Project, Map<NotificationType, Boolean>> getProjectNotifications(User user) {
-        Map<Project, Map<NotificationType, Boolean>> result = new HashMap<>();
+    public static Map<Project, Map<EventType, Boolean>> getProjectNotifications(User user) {
+        Map<Project, Map<EventType, Boolean>> result = new HashMap<>();
         List<UserProjectNotification> list = find.where().eq("user", user).findList();
         for(UserProjectNotification noti : list) {
             Project notiProject = noti.project;
-            Map<NotificationType, Boolean> pn = result.get(notiProject);
+            Map<EventType, Boolean> pn = result.get(notiProject);
             if(pn == null) {
                 pn = new HashMap<>();
                 result.put(notiProject, pn);
@@ -62,12 +62,12 @@ public class UserProjectNotification extends Model {
      * @param notiType
      * @return
      */
-    public static boolean isEnabledNotiType(Map<Project, Map<NotificationType, Boolean>> notiMap, Project project, NotificationType notiType) {
+    public static boolean isEnabledNotiType(Map<Project, Map<EventType, Boolean>> notiMap, Project project, EventType notiType) {
         if(!notiMap.containsKey(project)) {
             return true;
         }
 
-        Map<NotificationType, Boolean> projectNoti = notiMap.get(project);
+        Map<EventType, Boolean> projectNoti = notiMap.get(project);
         if(!projectNoti.containsKey(notiType)) {
             return true;
         } else {
@@ -75,7 +75,7 @@ public class UserProjectNotification extends Model {
         }
     }
 
-    public static UserProjectNotification findOne(User user, Project project, NotificationType notificationType) {
+    public static UserProjectNotification findOne(User user, Project project, EventType notificationType) {
         return find.where()
                 .eq("user", user)
                 .eq("project", project)
@@ -92,7 +92,7 @@ public class UserProjectNotification extends Model {
         update();
     }
 
-    public static void unwatchExplictly(User user, Project project, NotificationType notiType) {
+    public static void unwatchExplictly(User user, Project project, EventType notiType) {
         UserProjectNotification newOne = new UserProjectNotification();
         newOne.user = user;
         newOne.project = project;

app/models/enumeration/EventType.java
@@ -19,7 +19,7 @@ public enum EventType {
 
     private int order;
 
-    NotificationType(String messageKey, int order) {
+    EventType(String messageKey, int order) {
         this.descr = Messages.get(messageKey);
         this.order = order;
     }
@@ -32,13 +32,13 @@ public enum EventType {
         return order;
     }
 
-    public static final List<NotificationType> notiTypes;
+    public static final List<EventType> notiTypes;
 
     static {
-        notiTypes = Arrays.asList(NotificationType.values());
-        Collections.sort(notiTypes, new Comparator<NotificationType>() {
+        notiTypes = Arrays.asList(EventType.values());
+        Collections.sort(notiTypes, new Comparator<EventType>() {
             @Override
-            public int compare(NotificationType o1, NotificationType o2) {
+            public int compare(EventType o1, EventType o2) {
                 return o1.getOrder() - o2.getOrder();
             }
         });

app/views/user/edit.scala.html
@@ -96,7 +96,7 @@
                             @defining(Project.find.byId(watches.get(i).resourceId.toLong)) { project =>
                             <div class="tab-pane @if(i == 0){active}" id="@project.id">
                                 <table class="table table-striped table-bordered">
-                                @for(notiType <- models.enumeration.NotificationType.notiTypes) {
+                                @for(notiType <- models.enumeration.EventType.notiTypes) {
                                     <tr>
                                         <th>@notiType.getDescr()</th>
                                         <td>

conf/evolutions/default/33.sql
@@ -1,23 +1,5 @@
 # --- !Ups
-create table issue_event (
-  id                        bigint not null,
-  created                   timestamp,
-  sender_login_id           varchar(255),
-  issue_id                  bigint,
-  event_type         varchar(26),
-  old_value                 varchar(255),
-  new_value                 varchar(255),
-  constraint ck_issue_event_event_type check (event_type in ('NEW_ISSUE','NEW_POSTING','ISSUE_ASSIGNEE_CHANGED','ISSUE_STATE_CHANGED','NEW_COMMENT','NEW_PULL_REQUEST','NEW_SIMPLE_COMMENT','PULL_REQUEST_STATE_CHANGED')),
-  constraint pk_issue_event primary key (id))
-;
-
-create sequence issue_event_seq;
-
-alter table issue_event add constraint fk_issue_event_issue_8 foreign key (issue_id) references issue (id) on delete restrict on update restrict;
-
-create index ix_issue_event_issue_8 on issue_event (issue_id);
+ALTER TABLE notification_event ALTER COLUMN notification_type RENAME TO event_type;
 
 # --- !Downs
-drop table if exists issue_event;
-
-drop sequence if exists issue_event_seq;
+ALTER TABLE notification_event ALTER COLUMN event_type RENAME TO notification_type;