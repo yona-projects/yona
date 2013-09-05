a660ba2e324316c3e24259923c728be34adcf5f6
Yi EungJun <semtlenori@gmail.com>
2013-09-05 17:09:44 +0900

    Add SearchCondition.java missed at 76b53ce.

---
+++ app/models/support/SearchCondition.java
@@ -0,0 +1,159 @@
+package models.support;
+
+import com.avaje.ebean.ExpressionList;
+import controllers.AbstractPostingApp;
+import controllers.IssueApp;
+import models.*;
+import models.enumeration.State;
+import org.apache.commons.collections.CollectionUtils;
+import utils.LabelSearchUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.avaje.ebean.Expr.icontains;
+
+public class SearchCondition extends AbstractPostingApp.SearchCondition {
+    public String state;
+    public Boolean commentedCheck;
+    public Long milestoneId;
+
+    public Set<Long> labelIds = new HashSet<>();
+    public Long authorId;
+
+    public Long assigneeId;
+
+    public SearchCondition clone() {
+        SearchCondition one = new SearchCondition();
+        one.orderBy = this.orderBy;
+        one.orderDir = this.orderDir;
+        one.filter = this.filter;
+        one.pageNum = one.pageNum;
+        one.state = this.state;
+        one.commentedCheck = this.commentedCheck;
+        one.milestoneId = this.milestoneId;
+        one.labelIds = new HashSet<Long>(this.labelIds);
+        one.authorId = this.authorId;
+        one.assigneeId = this.assigneeId;
+        return one;
+    }
+
+    public SearchCondition setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+        return this;
+    }
+
+    public SearchCondition setOrderDir(String orderDir) {
+        this.orderDir = orderDir;
+        return this;
+    }
+
+    public SearchCondition setFilter(String filter) {
+        this.filter = filter;
+        return this;
+    }
+
+    public SearchCondition setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+        return this;
+    }
+
+    public SearchCondition setState(String state) {
+        this.state = state;
+        return this;
+    }
+
+    public SearchCondition setState(State state) {
+        this.state = state.state();
+        return this;
+    }
+
+    public SearchCondition setCommentedCheck(Boolean commentedCheck) {
+        this.commentedCheck = commentedCheck;
+        return this;
+    }
+
+    public SearchCondition setMilestoneId(Long milestoneId) {
+        this.milestoneId = milestoneId;
+        return this;
+    }
+
+    public SearchCondition setLabelIds(Set<Long> labelIds) {
+        this.labelIds = labelIds;
+        return this;
+    }
+
+    public SearchCondition addLabelId(Long labelId) {
+        labelIds.add(labelId);
+        return this;
+    }
+
+    public SearchCondition setAuthorId(Long authorId) {
+        this.authorId = authorId;
+        return this;
+    }
+
+    public SearchCondition setAssigneeId(Long assigneeId) {
+        this.assigneeId = assigneeId;
+        return this;
+    }
+
+    public SearchCondition() {
+        super();
+        milestoneId = null;
+        state = State.OPEN.name();
+        commentedCheck = false;
+    }
+
+    public ExpressionList<Issue> asExpressionList(Project project) {
+        ExpressionList<Issue> el = Issue.finder.where().eq("project.id", project.id);
+
+        if (filter != null) {
+            el.or(icontains("title", filter), icontains("body", filter));
+        }
+
+        if (authorId != null) {
+            if (authorId == User.anonymous.id) {
+                el.isNull("authorId");
+            } else {
+                el.eq("authorId", authorId);
+            }
+        }
+
+        if (assigneeId != null) {
+            if (assigneeId == User.anonymous.id) {
+                el.isNull("assignee");
+            } else {
+                el.eq("assignee.user.id", assigneeId);
+                el.eq("assignee.project.id", project.id);
+            }
+        }
+
+        if (milestoneId != null) {
+            if (milestoneId == Milestone.NULL_MILESTONE_ID) {
+                el.isNull("milestone");
+            } else {
+                el.eq("milestone.id", milestoneId);
+            }
+        }
+
+        if (commentedCheck) {
+            el.ge("numOfComments", AbstractPosting.NUMBER_OF_ONE_MORE_COMMENTS);
+        }
+
+        State st = State.getValue(state);
+        if (st.equals(State.OPEN) || st.equals(State.CLOSED)) {
+            el.eq("state", st);
+        }
+
+        if (CollectionUtils.isNotEmpty(labelIds)) {
+            el.add(LabelSearchUtil.createLabelSearchExpression(el.query(), labelIds));
+        }
+
+        if (orderBy != null) {
+            el.orderBy(orderBy + " " + orderDir);
+        }
+
+        return el;
+    }
+}
