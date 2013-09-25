yona-projects/yona

commit 412ff1984128f70a561d70ab69831d30dbffa7ec

notification: Introduce WatchService.
You can implement Watch feature for any Resource with the WatchService.

WatchService absorbs many facilities which originally Issue, Posting,
Commit and Project had for providing watching feature.

To do that, this commit make some changes on Resource as below:
* The type of Resource's id has changed from Long to String.
* ResourceParam, which implements QueryStringBindable, is added to
  easily pass a Resource to controller.

app/models/resource/Resource.java
@@ -1,10 +1,132 @@
 package models.resource;
 
-import models.Project;
+import models.*;
 import models.enumeration.ResourceType;
+import play.db.ebean.Model;
+import play.libs.F;
+import play.mvc.QueryStringBindable;
+import playRepository.Commit;
+import playRepository.RepositoryService;
+
+import java.util.EnumSet;
+import java.util.Map;
 
 public abstract class Resource {
-    abstract public Long getId();
+    public static boolean exists(ResourceType type, String id) {
+        Model.Finder<Long, ? extends Model> finder = null;
+
+        switch(type) {
+            case ISSUE_POST:
+                finder = Issue.finder;
+                break;
+            case ISSUE_ASSIGNEE:
+                finder = Assignee.finder;
+                break;
+            case ISSUE_COMMENT:
+                finder = IssueComment.find;
+                break;
+            case NONISSUE_COMMENT:
+                finder = PostingComment.find;
+                break;
+            case LABEL:
+                finder = Label.find;
+                break;
+            case BOARD_POST:
+                finder = Posting.finder;
+                break;
+            case USER:
+                finder = User.find;
+                break;
+            case PROJECT:
+                finder = Project.find;
+                break;
+            case ATTACHMENT:
+                finder = Attachment.find;
+                break;
+            case MILESTONE:
+                finder = Milestone.find;
+                break;
+            case CODE_COMMENT:
+                finder = CodeComment.find;
+                break;
+            case COMMIT:
+                try {
+                    String[] pair = id.split(":");
+                    Project project = Project.find.byId(Long.valueOf(pair[0]));
+                    if (RepositoryService.getRepository(project).getCommit(pair[1]) != null) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } catch (Exception e) {
+                    play.Logger.error("Failed to determine whether the commit exists", e);
+                    return false;
+                }
+            default:
+                throw new IllegalArgumentException(getInvalidResourceTypeMessage(type));
+        }
+
+        return finder.byId(Long.valueOf(id)) != null;
+    }
+
+    public static String getInvalidResourceTypeMessage(ResourceType resourceType) {
+        if (EnumSet.allOf(ResourceType.class).contains(resourceType)) {
+            return "Unsupported resource type " + resourceType;
+        } else {
+            return "Unknown resource type " + resourceType;
+        }
+    }
+
+    public static Resource get(ResourceType resourceType, String resourceId) {
+        Resource resource = null;
+
+        Long longId = Long.valueOf(resourceId);
+
+        switch(resourceType) {
+            case ISSUE_POST:
+                resource = Issue.finder.byId(longId).asResource();
+                break;
+            case ISSUE_COMMENT:
+                resource = IssueComment.find.byId(longId).asResource();
+                break;
+            case NONISSUE_COMMENT:
+                resource = PostingComment.find.byId(longId).asResource();
+                break;
+            case LABEL:
+                resource = Label.find.byId(longId).asResource();
+                break;
+            case BOARD_POST:
+                resource = Posting.finder.byId(longId).asResource();
+                break;
+            case USER:
+                resource = null;
+                break;
+            case PROJECT:
+                resource = Project.find.byId(longId).asResource();
+                break;
+            case ATTACHMENT:
+                resource = Attachment.find.byId(longId).asResource();
+                break;
+            case MILESTONE:
+                resource = Milestone.find.byId(longId).asResource();
+                break;
+            case CODE_COMMENT:
+                resource = CodeComment.find.byId(longId).asResource();
+                break;
+            case COMMIT:
+                return Commit.getAsResource(resourceId);
+            default:
+                throw new IllegalArgumentException(getInvalidResourceTypeMessage(resourceType));
+        }
+
+        return resource;
+    }
+
+    public ResourceParam asParameter() {
+        return ResourceParam.get(this);
+    }
+
+    abstract public String getId();
     abstract public Project getProject();
     abstract public ResourceType getType();
     public Resource getContainer() { return null; }

app/controllers/AbstractPostingApp.java
@@ -151,7 +151,7 @@ public class AbstractPostingApp extends Controller {
         notiEvent.senderId = UserApp.currentUser().id;
         notiEvent.receivers = watchers;
         notiEvent.urlToView = toView.absoluteURL(request());
-        notiEvent.resourceId = comment.id;
+        notiEvent.resourceId = comment.id.toString();
         notiEvent.resourceType = comment.asResource().getType();
         notiEvent.type = NotificationType.NEW_COMMENT;
         notiEvent.oldValue = null;
@@ -274,45 +274,5 @@ public class AbstractPostingApp extends Controller {
         }
 
         return ok(content);
-    }
-
-    /**
-     * 현재 사용자가 게시물을 명시적으로 지켜보는 것으로 설정한다.
-     *
-     * @param target
-     * @return
-     */
-    public static Result watch(AbstractPosting target) {
-        User user = UserApp.currentUser();
-
-        if (!AccessControl.isAllowed(user, target.asResource(), Operation.READ)) {
-            return forbidden(ErrorViews.Forbidden.render("You have no permission to do that.", target.asResource().getProject()));
-        }
-
-        if (user.isAnonymous()) {
-            return forbidden(ErrorViews.Forbidden.render("Anonymous cannot watch it.", target.asResource().getProject()));
-        }
-
-        target.watch(user);
-
-        return ok();
-    }
-
-    /**
-     * 현재 사용자가 게시물을 명시적으로 무시하는(지켜보지 않는) 것으로 설정한다.
-     *
-     * @param target
-     * @return
-     */
-    public static Result unwatch(AbstractPosting target) {
-        User user = UserApp.currentUser();
-
-        if (user.isAnonymous()) {
-            return forbidden(ErrorViews.Forbidden.render("Anonymous cannot unwatch it.", target.asResource().getProject()));
-        }
-
-        target.unwatch(user);
-
-        return ok();
     }
 }

app/controllers/AttachmentApp.java
@@ -259,8 +259,8 @@ public class AttachmentApp extends Controller {
 
         if (containerType != null && containerId != null) {
             List<Map<String, String>> attachments = new ArrayList<>();
-            for (Attachment attach : Attachment.findByContainer(ResourceType.valueOf(containerType),
-                    Long.parseLong(containerId))) {
+            for (Attachment attach : Attachment.findByContainer(ResourceType.valueOf
+                    (containerType), containerId)) {
                 if (!AccessControl.isAllowed(UserApp.currentUser(),
                         attach.asResource(), Operation.READ)) {
                     return forbidden();

app/controllers/BoardApp.java
@@ -160,7 +160,7 @@ public class BoardApp extends AbstractPostingApp {
         notiEvent.senderId = UserApp.currentUser().id;
         notiEvent.receivers = watchers;
         notiEvent.urlToView = toPost.absoluteURL(request());
-        notiEvent.resourceId = post.id;
+        notiEvent.resourceId = post.id.toString();
         notiEvent.resourceType = post.asResource().getType();
         notiEvent.type = NotificationType.NEW_POSTING;
         notiEvent.oldValue = null;
@@ -367,20 +367,4 @@ public class BoardApp extends AbstractPostingApp {
 
         return delete(comment, comment.asResource(), redirectTo);
     }
-
-    public static Result watch(String ownerName, String projectName, Long postingNumber) {
-        Project project = Project.findByOwnerAndProjectName(ownerName, projectName);
-        Posting posting = Posting.findByNumber(project, postingNumber);
-
-        return AbstractPostingApp.watch(posting);
-    }
-
-    public static Result unwatch(String ownerName, String projectName, Long postingNumber) {
-        Project project = Project.findByOwnerAndProjectName(ownerName, projectName);
-        Posting posting = Posting.findByNumber(project, postingNumber);
-
-        return AbstractPostingApp.unwatch(posting);
-    }
-
-
 }

app/controllers/CodeHistoryApp.java
@@ -212,7 +212,7 @@ public class CodeHistoryApp extends Controller {
         notiEvent.senderId = UserApp.currentUser().id;
         notiEvent.receivers = watchers;
         notiEvent.urlToView = toView.absoluteURL(request());
-        notiEvent.resourceId = codeComment.id;
+        notiEvent.resourceId = codeComment.id.toString();
         notiEvent.resourceType = codeComment.asResource().getType();
         notiEvent.type = NotificationType.NEW_COMMENT;
         notiEvent.oldValue = null;
@@ -237,42 +237,5 @@ public class CodeHistoryApp extends Controller {
         codeComment.delete();
 
         return redirect(routes.CodeHistoryApp.show(ownerName, projectName, commitId));
-    }
-
-    public static Result watch(String ownerName, String projectName,
-                               String commitId) throws IOException, ServletException, SVNException {
-        Project project = Project.findByOwnerAndProjectName(ownerName, projectName);
-        Commit commit = RepositoryService.getRepository(project).getCommit(commitId);
-
-        User user = UserApp.currentUser();
-
-        if (!AccessControl.isAllowed(user, project.asResource(), Operation.READ)) {
-            return forbidden(ErrorViews.Forbidden.render("You have no permission to do that.",
-                    project.asResource().getProject()));
-        }
-
-        if (user.isAnonymous()) {
-            return forbidden(ErrorViews.Forbidden.render("Anonymous cannot watch it.", project));
-        }
-
-        commit.watch(project, user);
-
-        return ok();
-    }
-
-    public static Result unwatch(String ownerName, String projectName,
-                                 String commitId) throws IOException, ServletException, SVNException {
-        Project project = Project.findByOwnerAndProjectName(ownerName, projectName);
-        Commit commit = RepositoryService.getRepository(project).getCommit(commitId);
-
-        User user = UserApp.currentUser();
-
-        if (user.isAnonymous()) {
-            return forbidden(ErrorViews.Forbidden.render("Anonymous cannot unwatch it.", project));
-        }
-
-        commit.unwatch(project, user);
-
-        return ok();
     }
 }

app/controllers/IssueApp.java
@@ -400,7 +400,7 @@ public class IssueApp extends AbstractPostingApp {
         notiEvent.senderId = UserApp.currentUser().id;
         notiEvent.receivers = watchers;
         notiEvent.urlToView = issueCall.absoluteURL(request());
-        notiEvent.resourceId = newIssue.id;
+        notiEvent.resourceId = newIssue.id.toString();
         notiEvent.resourceType = newIssue.asResource().getType();
         notiEvent.type = NotificationType.NEW_ISSUE;
         notiEvent.oldValue = null;
@@ -554,7 +554,7 @@ public class IssueApp extends AbstractPostingApp {
 
         notiEvent.created = new Date();
         notiEvent.urlToView = urlToView;
-        notiEvent.resourceId = updatedIssue.id;
+        notiEvent.resourceId = updatedIssue.id.toString();
         notiEvent.resourceType = updatedIssue.asResource().getType();
         notiEvent.type = NotificationType.ISSUE_STATE_CHANGED;
 
@@ -591,7 +591,7 @@ public class IssueApp extends AbstractPostingApp {
         notiEvent.senderId = UserApp.currentUser().id;
         notiEvent.receivers = receivers;
         notiEvent.urlToView = urlToView;
-        notiEvent.resourceId = updatedIssue.id;
+        notiEvent.resourceId = updatedIssue.id.toString();
         notiEvent.resourceType = updatedIssue.asResource().getType();
         notiEvent.type = NotificationType.ISSUE_ASSIGNEE_CHANGED;
 
@@ -728,19 +728,5 @@ public class IssueApp extends AbstractPostingApp {
                 labels.add(IssueLabel.finder.byId(Long.parseLong(labelId)));
             }
         }
-    }
-
-    public static Result watch(String ownerName, String projectName, Long issueNumber) {
-        Project project = Project.findByOwnerAndProjectName(ownerName, projectName);
-        Issue issue = Issue.findByNumber(project, issueNumber);
-
-        return AbstractPostingApp.watch(issue);
-    }
-
-    public static Result unwatch(String ownerName, String projectName, Long issueNumber) {
-        Project project = Project.findByOwnerAndProjectName(ownerName, projectName);
-        Issue issue = Issue.findByNumber(project, issueNumber);
-
-        return AbstractPostingApp.unwatch(issue);
     }
 }

app/controllers/UserApp.java
@@ -418,7 +418,7 @@ public class UserApp extends Controller {
                     addProjectNotDupped(projectCollection, Project.findProjectsJustMemberAndNotOwner(user));
                     break;
                 case "watching":
-                    addProjectNotDupped(projectCollection, user.watchingProjects);
+                    addProjectNotDupped(projectCollection, user.getWatchingProjects());
                     break;
             }
         }

app/controllers/WatchApp.java
@@ -1,0 +1,46 @@
+package controllers;
+
+import models.User;
+import models.enumeration.Operation;
+import models.resource.Resource;
+import models.resource.ResourceParam;
+import play.mvc.Controller;
+import play.mvc.Result;
+import utils.AccessControl;
+import utils.WatchService;
+
+public class WatchApp extends Controller {
+    public static Result watch(ResourceParam resourceParam) {
+        User user = UserApp.currentUser();
+        Resource resource = resourceParam.resource;
+
+        if (user.isAnonymous()) {
+            return forbidden("Anonymous cannot watch it.");
+        }
+
+        if (resource == null) {
+            return notFound();
+        }
+
+        if (!AccessControl.isAllowed(user, resource, Operation.READ)) {
+            return forbidden("You have no permission to watch it.");
+        }
+
+        WatchService.watch(user, resource);
+
+        return ok();
+    }
+
+    public static Result unwatch(ResourceParam resourceParam) {
+        User user = UserApp.currentUser();
+        Resource resource = resourceParam.resource;
+
+        if (user.isAnonymous()) {
+            return forbidden("Anonymous cannot unwatch it.");
+        }
+
+        WatchService.unwatch(user, resource);
+
+        return ok();
+    }
+}

commit c9b4b013bb7a7bed060590fed567b4a23d1916a2

watch: Fix 400 while trying to watch a commit.
This bug is caused by 412ff1984128f70a561d70ab69831d30dbffa7ec

app/models/resource/Resource.java
@@ -86,6 +86,10 @@ public abstract class Resource {
     public static Resource get(ResourceType resourceType, String resourceId) {
         Resource resource = null;
 
+        if (resourceType.equals(ResourceType.COMMIT)) {
+            return Commit.getAsResource(resourceId);
+        }
+
         Long longId = Long.valueOf(resourceId);
 
         switch(resourceType) {
@@ -123,8 +127,6 @@ public abstract class Resource {
                 return PullRequest.finder.byId(longId).asResource();
             case SIMPLE_COMMENT:
                 return SimpleComment.find.byId(longId).asResource();
-            case COMMIT:
-                return Commit.getAsResource(resourceId);
             default:
                 throw new IllegalArgumentException(getInvalidResourceTypeMessage(resourceType));
         }