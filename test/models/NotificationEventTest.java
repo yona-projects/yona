8829e4aa910ca8bc74accac82afeb2d4601092d8
whiteship <whiteship@epril.com>
2013-09-26 14:49:38 +0900

    fixed broken test

--- test/models/NotificationEventTest.java
+++ test/models/NotificationEventTest.java
@@ -1,6 +1,10 @@
 package models;
 
+import models.enumeration.ResourceType;
+import models.enumeration.State;
 import org.junit.Test;
+
+import java.util.HashSet;
 
 import static org.fest.assertions.Assertions.assertThat;
 
@@ -8,25 +12,50 @@ public class NotificationEventTest extends ModelTest<NotificationEvent> {
 
     @Test
     public void add() {
-        NotificationEvent event = new NotificationEvent();
+        // Given
+        NotificationEvent event = getNotificationEvent();
+
+        // When
         NotificationEvent.add(event);
+
+        // Then
         assertThat(NotificationMail.find.byId(event.notificationMail.id)).isNotNull();
+    }
+
+    private NotificationEvent getNotificationEvent() {
+        NotificationEvent event = new NotificationEvent();
+        event.resourceType = ResourceType.ISSUE_POST;
+        event.resourceId = "1";
+        HashSet<User> users = new HashSet<>();
+        users.add(User.findByLoginId("yobi"));
+        event.receivers = users;
+        return event;
     }
 
     @Test
     public void addTwoTimes() {
-        NotificationEvent event = new NotificationEvent();
+        // Given
+        NotificationEvent event = getNotificationEvent();
         NotificationEvent.add(event);
         int numOfMails = NotificationMail.find.all().size();
+
+        // When
         NotificationEvent.add(event);
+
+        // Then
         assertThat(NotificationEvent.find.all().size()).isEqualTo(numOfMails);
     }
 
     @Test
     public void delete() {
-        NotificationEvent event = new NotificationEvent();
+        // Given
+        NotificationEvent event = getNotificationEvent();
         NotificationEvent.add(event);
+
+        // When
         event.delete();
+
+        // Then
         assertThat(NotificationMail.find.byId(event.notificationMail.id)).isNull();
     }
 }
