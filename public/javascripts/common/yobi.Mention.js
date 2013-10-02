71fabeac79f185aba5305296ceaef95d4b2c7c2f
JiHan Kim <laziel@gmail.com>
2013-10-02 14:40:27 +0900

    trimming yobi.Mention to conventional JS module

--- public/javascripts/common/yobi.Mention.js
+++ public/javascripts/common/yobi.Mention.js
@@ -1,45 +1,86 @@
+/**
+ * @(#)yobi.Mention.js 2013.08.12
+ *
+ * Copyright NHN Corporation.
+ * Released under the MIT license
+ * 
+ * http://yobi.dev.naver.com/license
+ */
 yobi.Mention = function(htOptions) {
 
     var htVar = {};
     var htElement = {};
 
+    /**
+     * Initialize
+     * 
+     * @param {Hash Table} htOptions
+     */
     function _init(htOptions){
         _initVar(htOptions);
+        _initElement();
         _attachEvent();
     }
 
+    /**
+     * Initialize Variables 
+     * 
+     * @param {Hash Table} htOptions
+     */
     function _initVar(htOptions) {
-        htVar = htOptions || {};
+        htVar = htOptions || {}; // set htVar as htOptions
 
-        htVar.memberList = {
-            emptyQuery: true,
-            typeaheadOpts: {
-                items: 15
+        htVar.htMemberList = {
+            "emptyQuery": true,
+            "typeaheadOpts": {
+                "items": 15
             },
-            users: []
+            "users": []
         };
     }
 
+    /**
+     * Initialize Element variables
+     */
+    function _initElement(){
+        htElement.welTarget = $('#' + htVar.target);
+    }
+    
+    /**
+     * attachEvent
+     */
     function _attachEvent() {
-        $('#'+htVar.target).keypress(function(event){
-            var event = event || window.event;
-
-            if( event.which == 64 ){ // 64 = at
-                _findUserList(htVar.url, htVar.target);
-            }
-        });
+        htElement.welTarget.on("keypress", _onKeyInput);
     }
 
-    function _findUserList(url, target){
+    /**
+     * Event handler on KeyInput event
+     * 
+     * @param {Event} eEvt
+     */
+    function _onKeyInput(eEvt){
+        eEvt = eEvt || window.event;
+
+        if(eEvt.which === 64){ // 64 = at
+            _findUserList();
+        }
+    }
+    
+    /**
+     * Find Userlist
+     */
+    function _findUserList(){
         $.ajax({
-            type: "GET",
-            contentType: 'application/json',
-            url: url,
-            dataType: "json"
-        }).done(function( data ) {
-            htVar.memberList.users = data;
-            $("#"+target).mention(htVar.memberList);
-        });
+            "url"        : htVar.url,
+            "type"       : "get",
+            "contentType": "application/json",
+            "dataType"   : "json"
+        }).done(_onLoadUserList);
+    }
+    
+    function _onLoadUserList(aData){
+        htVar.htMemberList.users = aData;
+        htElement.welTarget.mention(htVar.htMemberList);
     }
 
     _init(htOptions || {});
