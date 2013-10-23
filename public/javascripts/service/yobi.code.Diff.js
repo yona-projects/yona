ec7df44793a92334beb9e258823910ce0112c4f7
JiHan Kim <laziel@navercorp.com>
2013-10-23 16:20:33 +0900

    hotfix: fix syntax error

--- public/javascripts/service/yobi.code.Diff.js
+++ public/javascripts/service/yobi.code.Diff.js
@@ -52,7 +52,7 @@
             htVar.sTplMiniMapLink = '<a href="#${id}" style="top:${top}px; height:${height}px;"></a>';
             
             // yobi.Attachments
-            htVar.sTplFileItem = ('#tplAttachedFile').text();
+            htVar.sTplFileItem = $('#tplAttachedFile').text();
         }
 
         /**
