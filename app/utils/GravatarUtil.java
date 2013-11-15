45e6d1c1eae4b5d80578f5008253a68e6ff7ec27
whiteship <whiteship@epril.com>
2013-11-15 11:25:01 +0900

    gravatarutils: added a size constant

--- app/utils/GravatarUtil.java
+++ app/utils/GravatarUtil.java
@@ -1,14 +1,17 @@
 package utils;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 public class GravatarUtil {
 
     public static final String DEFAULT_GRAVATAR_URL = "http://ko.gravatar.com/userimage/53495145/0eaeeb47c620542ad089f17377298af6.png";
+    public static final int DEFAULT_SIZE = 80;
 
     public static String getAvatar(String email) {
-        return getAvatar(email, 80);
+        return getAvatar(email, DEFAULT_SIZE);
     }
 
     public static String getAvatar(String email, int size) {
@@ -17,7 +20,11 @@ public class GravatarUtil {
 
     public static String getAvatar(String email, int size, String defaultImageUrl) {
         try {
-            return "http://www.gravatar.com/avatar/" + MD5Util.md5Hex(email) + "?s=" + size + "&d=" + URLEncoder.encode(defaultImageUrl, "UTF-8");
+            String url = "http://www.gravatar.com/avatar/" + MD5Util.md5Hex(email) + "?s=" + size;
+            if(StringUtils.isNotEmpty(defaultImageUrl)) {
+                url += "&d=" + URLEncoder.encode(defaultImageUrl, "UTF-8");
+            }
+            return url;
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
