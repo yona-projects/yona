0f22b9ea8a86a5c1a93b008d70e6cff04e187ed5
kjkmadness <kjkmadness@nhn.com>
2013-11-21 17:48:07 +0900

    test: fix models.UserTest

--- test/models/UserTest.java
+++ test/models/UserTest.java
@@ -68,7 +68,7 @@ public class UserTest extends ModelTest<User> {
         // When
         Map<String, String> userOptions = User.options();
         // Then
-        assertThat(userOptions).hasSize(5);
+        assertThat(userOptions).hasSize(6);
     }
 
 	@Test
