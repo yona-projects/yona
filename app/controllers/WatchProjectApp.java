a13da0575638f4a5c6c81a9a79513c5fcb1118b7
whiteship <whiteship@epril.com>
2013-11-27 14:12:31 +0900

    watching project: fixed to check auth

--- app/controllers/WatchProjectApp.java
+++ app/controllers/WatchProjectApp.java
@@ -1,10 +1,13 @@
 package controllers;
 
+import actions.AnonymousCheckAction;
+import controllers.annotation.ProjectAccess;
 import models.Project;
 import models.User;
 import models.UserProjectNotification;
 import models.Watch;
 import models.enumeration.EventType;
+import models.enumeration.Operation;
 import models.enumeration.ResourceType;
 import org.codehaus.jackson.node.ObjectNode;
 import play.i18n.Messages;
@@ -12,55 +15,36 @@ import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
+import play.mvc.With;
 import utils.Constants;
 import utils.ErrorViews;
 import utils.WatchService;
 
 public class WatchProjectApp extends Controller {
 
+    @ProjectAccess(Operation.READ)
+    @With(AnonymousCheckAction.class)
     public static Result watch(String userName, String projectName) {
         Project project = Project.findByOwnerAndProjectName(userName, projectName);
-        if(project == null) {
-            return badProject(userName, projectName);
-        }
-
-        User user = UserApp.currentUser();
-        if(user.isAnonymous()) {
-            flash(Constants.WARNING, "user.login.alert");
-            return redirect(routes.UserApp.loginForm());
-        }
-
         WatchService.watch(project.asResource());
-
         return redirect(request().getHeader(Http.HeaderNames.REFERER));
     }
 
+    @ProjectAccess(Operation.READ)
+    @With(AnonymousCheckAction.class)
     public static Result unwatch(String userName, String projectName) {
         Project project = Project.findByOwnerAndProjectName(userName, projectName);
-        if(project == null) {
-            return badProject(userName, projectName);
-        }
-
-        User user = UserApp.currentUser();
-        if(user.isAnonymous()) {
-            flash(Constants.WARNING, "user.login.alert");
-            return redirect(routes.UserApp.loginForm());
-        }
-
         WatchService.unwatch(project.asResource());
-
         return redirect(request().getHeader(Http.HeaderNames.REFERER));
     }
 
+    @ProjectAccess(Operation.READ)
+    @With(AnonymousCheckAction.class)
     public static Result toggle(Long projectId, String notificationType) {
         EventType notiType = EventType.valueOf(notificationType);
         Project project = Project.find.byId(projectId);
-        if(project == null) {
-            return notFound(ErrorViews.NotFound.render("No project matches given id '" + projectId + "'"));
-        }
 
         User user = UserApp.currentUser();
-
         if(!WatchService.isWatching(user, project.asResource())) {
             return badRequest(Messages.get("error.notfound.watch"));
         }
@@ -73,9 +57,5 @@ public class WatchProjectApp extends Controller {
         }
 
         return ok();
-    }
-
-    private static Result badProject(String userName, String projectName) {
-        return badRequest(ErrorViews.BadRequest.render("No project matches given user name '" + userName + "' and project name '" + projectName + "'"));
     }
 }
