9c9143c9487af548ee10b4fabcd397398d24ef85
whiteship <whiteship@epril.com>
2013-12-19 16:45:26 +0900

    fixed tests to not to use id directly

--- test/models/CommentThreadTest.java
+++ test/models/CommentThreadTest.java
@@ -4,6 +4,7 @@ import org.apache.shiro.util.ThreadState;
 import org.junit.Test;
 import utils.JodaDateUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.fest.assertions.Assertions.assertThat;
@@ -39,15 +40,15 @@ public class CommentThreadTest extends ModelTest<CommentThread>  {
     public void findByCommitIdAndState() {
         // given
         String commitId = "123123";
-        addTestData(commitId);
+        List<Long> threadIds = addTestData(commitId);
 
         // when and then
         List<CommentThread> threadList = CommentThread.findByCommitIdAndState(commitId, CommentThread.ThreadState.OPEN);
-        assertThat(threadList.get(0).id).isEqualTo(1);
+        assertThat(threadList.get(0).id).isEqualTo(threadIds.get(0));
 
         // when and then
         threadList = CommentThread.findByCommitIdAndState(commitId, CommentThread.ThreadState.CLOSED);
-        assertThat(threadList.get(0).id).isEqualTo(2);
+        assertThat(threadList.get(0).id).isEqualTo(threadIds.get(1));
     }
 
 
@@ -55,25 +56,25 @@ public class CommentThreadTest extends ModelTest<CommentThread>  {
      * {@code commitId}를 가지는 {@link models.NonRangedCodeCommentThread} 한 개 저장.
      *  - state: OPEN
      *  - createDate: 3일전
-     *  - id: 1
      * {@code commitId}를 가지는 {@link models.CodeCommentThread} 한 개 저장.
      *  - state: CLOSED
      *  - path: "readme.md"
      *  - createdDate: 2일전
-     *  - id: 2
      * 123321을 커밋 ID로 가지는 {@link models.NonRangedCodeCommentThread} 한 개 저장.
      *  - state: OPEN
      *  - createdDate: 1일전
-     *  - id: 3
      *
      * @param commitId
      */
-    private void addTestData(String commitId) {
+    private List<Long> addTestData(String commitId) {
+        List<Long> threadIds = new ArrayList<>();
+
         NonRangedCodeCommentThread thread1 = new NonRangedCodeCommentThread();
         thread1.commitId = commitId;
         thread1.state = CommentThread.ThreadState.OPEN;
         thread1.createdDate = JodaDateUtil.before(3);
         thread1.save();
+        threadIds.add(thread1.id);
 
         CodeCommentThread thread2 = new CodeCommentThread();
         thread2.commitId = commitId;
@@ -89,12 +90,16 @@ public class CommentThreadTest extends ModelTest<CommentThread>  {
         thread2.codeRange = codeRange;
         thread2.createdDate = JodaDateUtil.before(2);
         thread2.save();
+        threadIds.add(thread2.id);
 
         NonRangedCodeCommentThread thread3 = new NonRangedCodeCommentThread();
         thread3.commitId = "123321";
         thread3.state = CommentThread.ThreadState.OPEN;
         thread3.createdDate = JodaDateUtil.before(1);
         thread3.save();
+        threadIds.add(thread3.id);
+
+        return threadIds;
     }
 
 
--- test/models/ReviewCommentTest.java
+++ test/models/ReviewCommentTest.java
@@ -4,6 +4,7 @@ package models;
 import org.junit.Test;
 import utils.JodaDateUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.fest.assertions.Assertions.assertThat;
@@ -12,76 +13,77 @@ public class ReviewCommentTest extends ModelTest<ReviewComment> {
 
     @Test
     public void findByThread() {
-        /* Test comments in NonRangedCodeCommentThread */
-
         // given
-        addTestData();
-        CommentThread thread = CommentThread.find.byId(1l);
+        List<Long> ids = addTestData();
+        Long idOfThread1 = ids.get(0);
+        CommentThread thread = CommentThread.find.byId(idOfThread1);
         assertThat(thread).isNotNull();
 
         // when
-        List<ReviewComment> commentList = ReviewComment.findByThread(1l);
+        List<ReviewComment> commentList = ReviewComment.findByThread(idOfThread1);
 
         // then
         assertThat(commentList.size()).isEqualTo(2);
-        assertThat(commentList.get(0).id).isEqualTo(2);
-        assertThat(commentList.get(1).id).isEqualTo(1);
+        assertThat(commentList.get(0).id).isEqualTo(ids.get(2));
+        assertThat(commentList.get(1).id).isEqualTo(ids.get(1));
 
 
         /* Test comments in CodeCommentThread */
 
         // given
-        thread = CommentThread.find.byId(2l);
+        Long idOfThread2 = ids.get(3);
+        thread = CommentThread.find.byId(idOfThread2);
         assertThat(thread).isNotNull();
 
         // when
-        commentList = ReviewComment.findByThread(2l);
+        commentList = ReviewComment.findByThread(idOfThread2);
 
         // then
         assertThat(commentList.size()).isEqualTo(1);
-        assertThat(commentList.get(0).id).isEqualTo(3l);
+        assertThat(commentList.get(0).id).isEqualTo(ids.get(4));
     }
 
     /**
      * {@code commitId}를 가지는 {@link models.NonRangedCodeCommentThread} 한 개 저장.
      *  - state: OPEN
      *  - createDate: 3일전
-     *  - id: 1
      * {@link models.ReviewComment} 생성하여 {@link models.NonRangedCodeCommentThread}에 추가.
      *  - createdDate: 3일전
-     *  - id: 1
      * {@link models.ReviewComment} 생성하여 {@link models.NonRangedCodeCommentThread}에 추가.
      *  - createdDate: 2일전
-     *  - id: 2
      * {@code commitId}를 가지는 {@link models.CodeCommentThread} 한 개 저장.
      *  - state: CLOSED
      *  - path: "readme.md"
      *  - createdDate: 2일전
-     *  - id: 2
      * {@code }
      * {@link models.ReviewComment} 생성하여 {@link models.CodeCommentThread}에 추가.
      *  - createdDate: 3일전
-     *  - id: 1
+     *
+     *  @return {thread1.id, comment1.id, comment2.id, thread2.id, comment3.id}
      */
-    private void addTestData() {
+    private List<Long> addTestData() {
+        List<Long> ids = new ArrayList<>();
+
         NonRangedCodeCommentThread thread1 = new NonRangedCodeCommentThread();
         thread1.commitId = "123456";
         thread1.state = CommentThread.ThreadState.OPEN;
         thread1.createdDate = JodaDateUtil.before(3);
         thread1.save();
+        ids.add(thread1.id);
 
         ReviewComment reviewComment1 = new ReviewComment();
         thread1.addComment(reviewComment1);
         reviewComment1.createdDate = JodaDateUtil.before(3);
         reviewComment1.setContents("reviewComment1");
         reviewComment1.save();
+        ids.add(reviewComment1.id);
 
-        ReviewComment reviewComment3 = new ReviewComment();
-        thread1.addComment(reviewComment3);
-        reviewComment3.createdDate = JodaDateUtil.before(2);
-        reviewComment3.setContents("reviewComment3");
-        reviewComment3.save();
-
+        ReviewComment reviewComment2 = new ReviewComment();
+        thread1.addComment(reviewComment2);
+        reviewComment2.createdDate = JodaDateUtil.before(2);
+        reviewComment2.setContents("reviewComment3");
+        reviewComment2.save();
+        ids.add(reviewComment2.id);
 
         CodeCommentThread thread2 = new CodeCommentThread();
         thread2.commitId = "123456";
@@ -97,12 +99,16 @@ public class ReviewCommentTest extends ModelTest<ReviewComment> {
         thread2.codeRange = codeRange;
         thread2.createdDate = JodaDateUtil.before(2);
         thread2.save();
+        ids.add(thread2.id);
 
-        ReviewComment reviewComment2 = new ReviewComment();
-        reviewComment2.thread = thread2;
-        reviewComment2.createdDate = JodaDateUtil.before(2);
-        reviewComment2.setContents("reviewComment2");
-        reviewComment2.save();
+        ReviewComment reviewComment3 = new ReviewComment();
+        reviewComment3.thread = thread2;
+        reviewComment3.createdDate = JodaDateUtil.before(2);
+        reviewComment3.setContents("reviewComment2");
+        reviewComment3.save();
+        ids.add(reviewComment3.id);
+
+        return ids;
     }
 
 }
