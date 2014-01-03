yona-projects/yona

commit 26de570e190a323559f7e6abd69412360cf1a52f

user: Consider application.context
Consider application.context while getting reserved word from routes.

app/utils/ReservedWordsValidator.java
@@ -28,7 +28,13 @@ public class ReservedWordsValidator extends Validator<String> {
     static {
         RESERVED_WORDS = new HashSet<>();
         List<Tuple3<String, String, String>> list = Scala.asJava(Play.current().routes().get().documentation());
-        Pattern pattern = Pattern.compile("^/(" + User.LOGIN_ID_PATTERN + ")/?");
+        play.Configuration config = play.Configuration.root();
+        String context = config.getString("application.context");
+        String regex = String.format("^%s%s(%s)/?",
+                context,
+                (context.endsWith("/") ? "" : "/"),
+                User.LOGIN_ID_PATTERN);
+        Pattern pattern = Pattern.compile(regex);
         for (Tuple3<String, String, String> tuple : list) {
             Matcher matcher = pattern.matcher(tuple._2());
             if (matcher.find()) {

commit e582e225368a97a4bf101c8a50e598471b50e285

user: Fix an error while trying to login
This error is caused by 26de570e190a323559f7e6abd69412360cf1a52f.

app/utils/ReservedWordsValidator.java
@@ -29,7 +29,7 @@ public class ReservedWordsValidator extends Validator<String> {
         RESERVED_WORDS = new HashSet<>();
         List<Tuple3<String, String, String>> list = Scala.asJava(Play.current().routes().get().documentation());
         play.Configuration config = play.Configuration.root();
-        String context = config.getString("application.context");
+        String context = config.getString("application.context", "/");
         String regex = String.format("^%s%s(%s)/?",
                 context,
                 (context.endsWith("/") ? "" : "/"),