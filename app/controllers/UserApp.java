bafd23e8b9fd143a0f5c7d98bb0d75f9bafeb80a
whiteship <whiteship@epril.com>
2014-01-09 16:47:23 +0900

    Login: Fixed to check null

--- app/controllers/UserApp.java
+++ app/controllers/UserApp.java
@@ -185,6 +185,9 @@ public class UserApp extends Controller {
      */
     public static User authenticateWithPlainPassword(String loginId, String password) {
         User user = User.findByLoginId(loginId);
+        if(user == User.anonymous) {
+            return null;
+        }
         return authenticate(user, hashedPassword(password, user.passwordSalt));
     }
 
