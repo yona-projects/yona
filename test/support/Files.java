2d950579d264499523e70af32365f68af50eff3f
kjkmadness <kjkmadness@nhn.com>
2014-01-09 14:51:54 +0900

    fix broken tests for PullRequestApp

---
+++ test/support/Files.java
@@ -0,0 +1,38 @@
+/**
+ * Yobi, Project Hosting SW
+ *
+ * Copyright 2013 NAVER Corp.
+ * http://yobi.io
+ *
+ * @Author kjkmadness
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package support;
+
+import java.io.File;
+
+public class Files {
+    public static void rm_rf(File file) {
+        assert file != null;
+        if (file.isDirectory()) {
+            File[] list = file.listFiles();
+            assert list != null;
+            for(int i = 0; i < list.length; i++){
+                rm_rf(list[i]);
+            }
+        }
+        System.gc();
+        file.delete();
+    }
+}
