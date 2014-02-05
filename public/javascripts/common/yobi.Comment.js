442da50d3f3341bf364ab9b799b64fb54dc5eb59
kjkmadness <kjkmadness@nhn.com>
2014-02-05 17:13:43 +0900

    comment: display delete confirm layer

---
+++ public/javascripts/common/yobi.Comment.js
@@ -0,0 +1,63 @@
+/**
+ * Yobi, Project Hosting SW
+ *
+ * Copyright 2013 NAVER Corp.
+ * http://yobi.io
+ *
+ * @Author kjkmadness
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+yobi.Comment = (function(){
+    var htElement = {};
+
+    /**
+     * initialize
+     * @param {Hash Table} htOptions
+     */
+    function _init(htOptions){
+        _initElement(htOptions || {});
+        _attachEvent();
+    }
+
+    /**
+     * initialize element variables
+     */
+    function _initElement(htOptions) {
+        htElement.welContainer = $(htOptions.sContainer || '#comments');
+        htElement.welDeleteModal = $(htOptions.sDeleteModal || '#comment-delete-modal');
+        htElement.welDeleteConfirmBtn = $(htOptions.sDeleteConfirm || '#comment-delete-confirm');
+    }
+
+    /**
+     * attach event handler
+     */
+    function _attachEvent() {
+        htElement.welContainer.on('click', '[data-toggle="comment-delete"]', _openDeleteModal);
+    }
+
+    /**
+     * open delete modal
+     */
+    function _openDeleteModal() {
+        htElement.welDeleteConfirmBtn
+            .data('requestUri', $(this).data('requestUri'))
+            .data('requestMethod', 'delete')
+            .requestAs();
+        htElement.welDeleteModal.modal();
+    }
+
+    return {
+        "init"  : _init,
+    };
+})();
