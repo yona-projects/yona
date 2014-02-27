yona-projects/yona

commit c365527de2ab80ac4ad8fb4bc5046f1200163494

Notification: Fix incorrect links to events
Do not store the url for the link of notification events item and
notification emails in database. Instead Compute them just in time
because they could be changed by domain name, application.context
configuration, and routes file.

app/models/NotificationEvent.java
@@ -19,6 +19,7 @@ import play.i18n.Messages;
 import play.libs.Akka;
 import playRepository.*;
 import scala.concurrent.duration.Duration;
+import utils.RouteUtil;
 
 import javax.persistence.*;
 import javax.servlet.ServletException;
@@ -30,6 +31,8 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static models.enumeration.EventType.*;
 
 @Entity
 public class NotificationEvent extends Model {
@@ -59,8 +62,6 @@ public class NotificationEvent extends Model {
 
     @Temporal(TemporalType.TIMESTAMP)
     public Date created;
-
-    public String urlToView;
 
     @Enumerated(EnumType.STRING)
     public ResourceType resourceType;
@@ -244,17 +245,30 @@ public class NotificationEvent extends Model {
      * @return
      * @see {@link controllers.PullRequestApp#newPullRequest(String, String)}
      */
-    public static NotificationEvent afterNewPullRequest(User sender, String urlToView, PullRequest pullRequest) {
+    public static NotificationEvent afterNewPullRequest(User sender, PullRequest pullRequest) {
         NotificationEvent notiEvent = createFrom(sender, pullRequest);
-        notiEvent.title = formatNewTitle(pullRequest);;
-        notiEvent.urlToView = urlToView;
+        notiEvent.title = formatNewTitle(pullRequest);
         notiEvent.receivers = getReceiversWithRelatedAuthors(sender, pullRequest);
-        notiEvent.eventType = EventType.NEW_PULL_REQUEST;
+        notiEvent.eventType = NEW_PULL_REQUEST;
         notiEvent.oldValue = null;
         notiEvent.newValue = pullRequest.body;
         NotificationEvent.add(notiEvent);
         return notiEvent;
     }
+
+    public String getUrlToView() {
+        switch(eventType) {
+            case MEMBER_ENROLL_REQUEST:
+                return routes.ProjectApp.members(
+                        getProject().owner, getProject().name).url();
+            case NEW_COMMIT:
+                return routes.CodeHistoryApp.historyUntilHead(
+                        getProject().owner, getProject().name).url();
+            default:
+                return RouteUtil.getUrl(resourceType, resourceId);
+        }
+    }
+
 
     /**
      * 보낸코드의 상태가 변경되었을때의 알림을 추가한다.
@@ -268,12 +282,11 @@ public class NotificationEvent extends Model {
      * @see {@link models.PullRequest#merge(models.PullRequestEventMessage)}
      * @see {@link controllers.PullRequestApp#addNotification(models.PullRequest, play.api.mvc.Call, models.enumeration.State, models.enumeration.State)}
      */
-    public static NotificationEvent afterPullRequestUpdated(User sender, String urlToView, PullRequest pullRequest, State oldState, State newState) {
+    public static NotificationEvent afterPullRequestUpdated(User sender, PullRequest pullRequest, State oldState, State newState) {
         NotificationEvent notiEvent = createFrom(sender, pullRequest);
         notiEvent.title = formatReplyTitle(pullRequest);
-        notiEvent.urlToView = urlToView;
         notiEvent.receivers = getReceivers(sender, pullRequest);
-        notiEvent.eventType = EventType.PULL_REQUEST_STATE_CHANGED;
+        notiEvent.eventType = PULL_REQUEST_STATE_CHANGED;
         notiEvent.oldValue = oldState.state();
         notiEvent.newValue = newState.state();
         NotificationEvent.add(notiEvent);
@@ -293,9 +306,8 @@ public class NotificationEvent extends Model {
     public static NotificationEvent afterMerge(User sender, PullRequest pullRequest, GitConflicts conflicts, State state) {
         NotificationEvent notiEvent = createFrom(sender, pullRequest);
         notiEvent.title = formatReplyTitle(pullRequest);
-        notiEvent.urlToView = urlToView(pullRequest);
         notiEvent.receivers = state == State.MERGED ? getReceiversWithRelatedAuthors(sender, pullRequest) : getReceivers(sender, pullRequest);
-        notiEvent.eventType = EventType.PULL_REQUEST_MERGED;
+        notiEvent.eventType = PULL_REQUEST_MERGED;
         notiEvent.newValue = state.state();
         if (conflicts != null) {
             notiEvent.oldValue = StringUtils.join(conflicts.conflictFiles, "\n");
@@ -310,18 +322,16 @@ public class NotificationEvent extends Model {
      * @param sender
      * @param pullRequest
      * @param newComment
-     * @param urlToView
      * @see {@link controllers.PullRequestCommentApp#newComment(String, String, Long)}
      */
-    public static void afterNewComment(User sender, PullRequest pullRequest, PullRequestComment newComment, String urlToView) {
+    public static void afterNewComment(User sender, PullRequest pullRequest, PullRequestComment newComment) {
         NotificationEvent notiEvent = createFrom(sender, newComment);
         notiEvent.title = formatReplyTitle(pullRequest);
-        notiEvent.urlToView = urlToView;
         Set<User> receivers = getMentionedUsers(newComment.contents);
         receivers.addAll(getReceivers(sender, pullRequest));
         receivers.remove(User.findByLoginId(newComment.authorLoginId));
         notiEvent.receivers = receivers;
-        notiEvent.eventType = EventType.NEW_PULL_REQUEST_COMMENT;
+        notiEvent.eventType = NEW_PULL_REQUEST_COMMENT;
         notiEvent.oldValue = null;
         notiEvent.newValue = newComment.contents;
 
@@ -329,38 +339,28 @@ public class NotificationEvent extends Model {
     }
 
     public static NotificationEvent afterNewPullRequest(PullRequest pullRequest) {
-        return afterNewPullRequest(UserApp.currentUser(), urlToView(pullRequest), pullRequest);
-    }
-
-    public static NotificationEvent afterPullRequestUpdated(User sender, PullRequest pullRequest, State oldState, State newState) {
-        return afterPullRequestUpdated(sender, urlToView(pullRequest), pullRequest, oldState, newState);
-    }
-
-    public static NotificationEvent afterPullRequestUpdated(String urlToView, PullRequest pullRequest, State oldState, State newState) {
-        return afterPullRequestUpdated(UserApp.currentUser(), urlToView, pullRequest, oldState, newState);
+        return afterNewPullRequest(UserApp.currentUser(), pullRequest);
     }
 
     public static NotificationEvent afterPullRequestUpdated(PullRequest pullRequest, State oldState, State newState) {
-        return afterPullRequestUpdated(urlToView(pullRequest), pullRequest, oldState, newState);
+        return afterPullRequestUpdated(UserApp.currentUser(), pullRequest, oldState, newState);
     }
 
     /**
      * 이슈와 게시물에 새 댓글을 달렸을 때 알림을 추가한다.
      *
      * @param comment
-     * @param urlToView
      */
-    public static void afterNewComment(Comment comment, String urlToView) {
+    public static void afterNewComment(Comment comment) {
         AbstractPosting post = comment.getParent();
 
         NotificationEvent notiEvent = createFromCurrentUser(comment);
         notiEvent.title = formatReplyTitle(post);
-        notiEvent.urlToView = urlToView;
         Set<User> receivers = getReceivers(post);
         receivers.addAll(getMentionedUsers(comment.contents));
         receivers.remove(UserApp.currentUser());
         notiEvent.receivers = receivers;
-        notiEvent.eventType = EventType.NEW_COMMENT;
+        notiEvent.eventType = NEW_COMMENT;
         notiEvent.oldValue = null;
         notiEvent.newValue = comment.contents;
 
@@ -371,21 +371,21 @@ public class NotificationEvent extends Model {
      * 이슈와 게시물에 새 댓글을 달렸을 때 알림을 추가한다.
      *
      * @param comment
-     * @param urlToView
      */
-    public static void afterNewCommentWithState(Comment comment, String urlToView, State state) {
+    public static void afterNewCommentWithState(Comment comment, State state) {
         AbstractPosting post = comment.getParent();
 
         NotificationEvent notiEvent = createFromCurrentUser(comment);
         notiEvent.title = formatReplyTitle(post);
-        notiEvent.urlToView = urlToView;
         Set<User> receivers = getReceivers(post);
         receivers.addAll(getMentionedUsers(comment.contents));
         receivers.remove(UserApp.currentUser());
         notiEvent.receivers = receivers;
-        notiEvent.eventType = EventType.NEW_COMMENT;
+        notiEvent.eventType = NEW_COMMENT;
         notiEvent.oldValue = null;
         notiEvent.newValue = comment.contents + "\n" + state.state();
+        notiEvent.resourceType = comment.asResource().getType();
+        notiEvent.resourceId = comment.asResource().getId();
 
         NotificationEvent.add(notiEvent);
     }
@@ -398,14 +398,12 @@ public class NotificationEvent extends Model {
      *
      * @param oldState
      * @param issue
-     * @param urlToView
      */
-    public static NotificationEvent afterStateChanged(State oldState, Issue issue, String urlToView) {
+    public static NotificationEvent afterStateChanged(State oldState, Issue issue) {
         NotificationEvent notiEvent = createFromCurrentUser(issue);
         notiEvent.title = formatReplyTitle(issue);
-        notiEvent.urlToView = urlToView;
         notiEvent.receivers = getReceivers(issue);
-        notiEvent.eventType = EventType.ISSUE_STATE_CHANGED;
+        notiEvent.eventType = ISSUE_STATE_CHANGED;
         notiEvent.oldValue = oldState != null ? oldState.state() : null;
         notiEvent.newValue = issue.state.state();
 
@@ -422,9 +420,8 @@ public class NotificationEvent extends Model {
      *
      * @param oldAssignee
      * @param issue
-     * @param urlToView
      */
-    public static NotificationEvent afterAssigneeChanged(User oldAssignee, Issue issue, String urlToView) {
+    public static NotificationEvent afterAssigneeChanged(User oldAssignee, Issue issue) {
         NotificationEvent notiEvent = createFromCurrentUser(issue);
 
         Set<User> receivers = getReceivers(issue);
@@ -440,8 +437,7 @@ public class NotificationEvent extends Model {
         }
         notiEvent.title = formatReplyTitle(issue);
         notiEvent.receivers = receivers;
-        notiEvent.urlToView = urlToView;
-        notiEvent.eventType = EventType.ISSUE_ASSIGNEE_CHANGED;
+        notiEvent.eventType = ISSUE_ASSIGNEE_CHANGED;
 
         NotificationEvent.add(notiEvent);
 
@@ -452,18 +448,16 @@ public class NotificationEvent extends Model {
         NotificationEvent notiEvent = createFromCurrentUser(issue);
         notiEvent.title = formatNewTitle(issue);
         notiEvent.receivers = getReceivers(issue);
-        notiEvent.urlToView = getUrlToView(issue);
-        notiEvent.eventType = EventType.NEW_ISSUE;
+        notiEvent.eventType = NEW_ISSUE;
         notiEvent.oldValue = null;
         notiEvent.newValue = issue.body;
 
         NotificationEvent.add(notiEvent);
     }
 
-    public static NotificationEvent afterIssueBodyChanged(String oldBody, Issue issue, String urlToView) {
+    public static NotificationEvent afterIssueBodyChanged(String oldBody, Issue issue) {
         NotificationEvent notiEvent = createFromCurrentUser(issue);
         notiEvent.title = formatReplyTitle(issue);
-        notiEvent.urlToView = urlToView;
         notiEvent.receivers = getReceivers(issue);
         notiEvent.eventType = EventType.ISSUE_BODY_CHANGED;
         notiEvent.oldValue = oldBody;
@@ -478,15 +472,14 @@ public class NotificationEvent extends Model {
         NotificationEvent notiEvent = createFromCurrentUser(post);
         notiEvent.title = formatNewTitle(post);
         notiEvent.receivers = getReceivers(post);
-        notiEvent.urlToView = getUrlToView(post);
-        notiEvent.eventType = EventType.NEW_POSTING;
+        notiEvent.eventType = NEW_POSTING;
         notiEvent.oldValue = null;
         notiEvent.newValue = post.body;
 
         NotificationEvent.add(notiEvent);
     }
 
-    public static void afterNewCommitComment(Project project, CommitComment codeComment, String urlToView) throws IOException, SVNException, ServletException {
+    public static void afterNewCommitComment(Project project, CommitComment codeComment) throws IOException, SVNException, ServletException {
         Commit commit = RepositoryService.getRepository(project).getCommit(codeComment.commitId);
         Set<User> watchers = commit.getWatchers(project);
         watchers.addAll(getMentionedUsers(codeComment.contents));
@@ -495,8 +488,7 @@ public class NotificationEvent extends Model {
         NotificationEvent notiEvent = createFromCurrentUser(codeComment);
         notiEvent.title = formatReplyTitle(project, commit);
         notiEvent.receivers = watchers;
-        notiEvent.urlToView = urlToView;
-        notiEvent.eventType = EventType.NEW_COMMENT;
+        notiEvent.eventType = NEW_COMMENT;
         notiEvent.oldValue = null;
         notiEvent.newValue = codeComment.contents;
 
@@ -509,14 +501,12 @@ public class NotificationEvent extends Model {
      * @param project
      * @param user
      * @param state
-     * @param urlToView
      */
-    public static void afterMemberRequest(Project project, User user, RequestState state, String urlToView) {
+    public static void afterMemberRequest(Project project, User user, RequestState state) {
         NotificationEvent notiEvent = createFromCurrentUser(project);
-        notiEvent.eventType = EventType.MEMBER_ENROLL_REQUEST;
+        notiEvent.eventType = MEMBER_ENROLL_REQUEST;
         notiEvent.receivers = getReceivers(project);
         notiEvent.newValue = state.name();
-        notiEvent.urlToView = urlToView;
         if (state == RequestState.ACCEPT || state == RequestState.REJECT) {
             notiEvent.receivers.remove(UserApp.currentUser());
             notiEvent.receivers.add(user);
@@ -528,6 +518,8 @@ public class NotificationEvent extends Model {
             notiEvent.title = formatReplyTitle(project, user);
             notiEvent.oldValue = RequestState.REQUEST.name();
         }
+        notiEvent.resourceType = project.asResource().getType();
+        notiEvent.resourceId = project.asResource().getId();
         NotificationEvent.add(notiEvent);
     }
 
@@ -545,10 +537,11 @@ public class NotificationEvent extends Model {
         NotificationEvent notiEvent = createFrom(sender, project);
         notiEvent.title = title;
         notiEvent.receivers = watchers;
-        notiEvent.urlToView = getUrlToHistoryView(project);
-        notiEvent.eventType = EventType.NEW_COMMIT;
+        notiEvent.eventType = NEW_COMMIT;
         notiEvent.oldValue = null;
         notiEvent.newValue = newCommitsMessage(commits, refNames, project);
+        notiEvent.resourceType = project.asResource().getType();
+        notiEvent.resourceId = project.asResource().getId();
         NotificationEvent.add(notiEvent);
     }
 
@@ -560,7 +553,7 @@ public class NotificationEvent extends Model {
      * @param eventType
      * @return
      */
-    public static NotificationEvent afterReviewed(Call call, PullRequest pullRequest, EventType eventType) {
+    public static NotificationEvent afterReviewed(PullRequest pullRequest, EventType eventType) {
         String title = formatReplyTitle(pullRequest);
         Resource resource = pullRequest.asResource();
         Set<User> receivers = pullRequest.getWatchers();
@@ -573,7 +566,6 @@ public class NotificationEvent extends Model {
         notiEvent.title = title;
         notiEvent.senderId = reviewer.id;
         notiEvent.receivers = receivers;
-        notiEvent.urlToView = call.url();
         notiEvent.resourceId = resource.getId();
         notiEvent.resourceType = resource.getType();
         notiEvent.eventType = eventType;
@@ -582,10 +574,6 @@ public class NotificationEvent extends Model {
         add(notiEvent);
 
         return notiEvent;
-    }
-
-    private static String getUrlToHistoryView(Project project) {
-        return routes.CodeHistoryApp.historyUntilHead(project.owner, project.name).url();
     }
 
     private static String newCommitsMessage(List<RevCommit> commits, List<String> refNames, Project project) {
@@ -656,14 +644,6 @@ public class NotificationEvent extends Model {
         return receivers;
     }
 
-    private static String getUrlToView(Issue issue) {
-        return routes.IssueApp.issue(issue.project.owner, issue.project.name, issue.getNumber()).url();
-    }
-
-    private static String getUrlToView(Posting post) {
-        return routes.BoardApp.post(post.project.owner, post.project.name, post.getNumber()).url();
-    }
-
     /**
      * {@code posting}의 번호를 반환하되, 이슈라면 앞에 "#"을 붙인다.
      *
@@ -691,11 +671,6 @@ public class NotificationEvent extends Model {
     private static String formatReplyTitle(Project project, Commit commit) {
         return String.format("Re: [%s] %s (%s)",
                 project.name, commit.getShortMessage(), commit.getShortId());
-    }
-
-    private static String urlToView(PullRequest pullRequest) {
-        Project toProject = pullRequest.toProject;
-        return routes.PullRequestApp.pullRequest(toProject.owner, toProject.name, pullRequest.number).url();
     }
 
     private static Set<User> getReceivers(User sender, PullRequest pullRequest) {

app/utils/RouteUtil.java
@@ -1,0 +1,98 @@
+/**
+ * Yobi, Project Hosting SW
+ *
+ * Copyright 2014 NAVER Corp.
+ * http://yobi.io
+ *
+ * @Author Yi EungJun
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils;
+
+import controllers.CodeHistoryApp;
+import models.*;
+import models.enumeration.ResourceType;
+import models.resource.Resource;
+
+public class RouteUtil {
+    public static String getUrl(ResourceType resourceType, String resourceId) {
+        Long longId = Long.valueOf(resourceId);
+
+        switch(resourceType) {
+            case ISSUE_POST:
+                return getUrl(Issue.finder.byId(longId));
+            case ISSUE_COMMENT:
+                return getUrl(IssueComment.find.byId(longId));
+            case NONISSUE_COMMENT:
+                return getUrl(PostingComment.find.byId(longId));
+            case BOARD_POST:
+                return getUrl(Posting.finder.byId(longId));
+            case COMMIT_COMMENT:
+                return getUrl(CommitComment.find.byId(longId));
+            case PULL_REQUEST:
+                return getUrl(PullRequest.finder.byId(longId));
+            case PULL_REQUEST_COMMENT:
+                return getUrl(PullRequestComment.find.byId(longId));
+            default:
+                throw new IllegalArgumentException(
+                        Resource.getInvalidResourceTypeMessage(resourceType));
+        }
+    }
+
+    public static String getUrl(Issue issue) {
+        return controllers.routes.IssueApp.issue(
+                issue.project.owner, issue.project.name, issue.getNumber()).url();
+    }
+
+    public static String getUrl(Posting post) {
+        return controllers.routes.BoardApp.post(
+                post.project.owner, post.project.name, post.getNumber()).url();
+    }
+
+    public static String getUrl(IssueComment comment) {
+        return getUrl(comment.issue) + "#comment-" + comment.id;
+    }
+
+    public static String getUrl(PostingComment comment) {
+        return getUrl(comment.posting) + "#comment-" + comment.id;
+    }
+
+    public static String getUrl(PullRequest pullRequest) {
+        Project toProject = pullRequest.toProject;
+        return controllers.routes.PullRequestApp.pullRequest(
+                toProject.owner, toProject.name, pullRequest.number).url();
+    }
+
+    public static String getUrl(CommitComment comment) {
+        play.mvc.Call toView = controllers.routes.CodeHistoryApp.show(
+                comment.project.owner, comment.project.name, comment.commitId);
+        toView = CodeHistoryApp.backToThePullRequestCommitView(toView);
+        return toView + "#comment-" + comment.id;
+    }
+
+    public static String getUrl(Comment comment) {
+        if (comment instanceof IssueComment) {
+            return getUrl((IssueComment) comment);
+        } else if (comment instanceof PostingComment) {
+            return getUrl((PostingComment) comment);
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+    public static String getUrl(PullRequestComment newComment) {
+        return getUrl(newComment.pullRequest) + "#comment-" + newComment.id;
+    }
+}

app/views/index/partial_notifications.scala.html
@@ -64,7 +64,7 @@
                 <img src="@routes.Assets.at("images/default-avatar-64.png")" width="42" height="42">
             </div>
             }
-            <div class="title"><a href="@noti.urlToView">@noti.title</a></div>
+            <div class="title"><a href="@noti.getUrlToView">@noti.title</a></div>
             <div class="message-wrap nowrap" id="message-@noti.id">
                 <div class="message">@noti.getMessage</div>
             </div>

app/controllers/AbstractPostingApp.java
@@ -71,8 +71,8 @@ public class AbstractPostingApp extends Controller {
         // Attach all of the files in the current user's temporary storage.
         attachUploadFilesToPost(comment.asResource());
 
-        String urlToView = toView + "#comment-" + comment.id;
-        NotificationEvent.afterNewComment(comment, urlToView);
+        String urlToView = RouteUtil.getUrl(comment);
+        NotificationEvent.afterNewComment(comment);
         return redirect(urlToView);
     }
 

app/controllers/CodeHistoryApp.java
@@ -25,6 +25,7 @@ import playRepository.RepositoryService;
 import utils.ErrorViews;
 import utils.HttpUtil;
 import utils.PullRequestCommit;
+import utils.RouteUtil;
 import views.html.code.diff;
 import views.html.code.history;
 import views.html.code.nohead;
@@ -196,12 +197,12 @@ public class CodeHistoryApp extends Controller {
         Call toView = routes.CodeHistoryApp.show(project.owner, project.name, commitId);
         toView = backToThePullRequestCommitView(toView);
 
-        String urlToView = toView + "#comment-" + codeComment.id;
-        NotificationEvent.afterNewCommitComment(project, codeComment, urlToView);
-        return redirect(urlToView);
+        NotificationEvent.afterNewCommitComment(project, codeComment);
+
+        return redirect(RouteUtil.getUrl(codeComment));
     }
 
-    private static Call backToThePullRequestCommitView(Call toView) {
+    public static Call backToThePullRequestCommitView(Call toView) {
         String referer = request().getHeader("Referer");
         if(PullRequestCommit.isValid(referer)) {
             PullRequestCommit prc = new PullRequestCommit(referer);

commit cb28895ef37214624873c3a6e197360d1d3d8604

Notification: Fix NPE caused by c365527
c365527 introduces the RouteUtil to get the url from a resource, but it
can cause NullPointerException because of these two reasons:

* The resource does not exist
* Direct field access returns null (I don't know why, but the solution
  is calling refresh() before access the field)

app/models/NotificationEvent.java
@@ -259,11 +259,19 @@ public class NotificationEvent extends Model {
     public String getUrlToView() {
         switch(eventType) {
             case MEMBER_ENROLL_REQUEST:
-                return routes.ProjectApp.members(
-                        getProject().owner, getProject().name).url();
+                if (getProject() == null) {
+                    return null;
+                } else {
+                    return routes.ProjectApp.members(
+                            getProject().owner, getProject().name).url();
+                }
             case NEW_COMMIT:
-                return routes.CodeHistoryApp.historyUntilHead(
-                        getProject().owner, getProject().name).url();
+                if (getProject() == null) {
+                    return null;
+                } else {
+                    return routes.CodeHistoryApp.historyUntilHead(
+                            getProject().owner, getProject().name).url();
+                }
             default:
                 return RouteUtil.getUrl(resourceType, resourceId);
         }

app/utils/RouteUtil.java
@@ -30,52 +30,72 @@ public class RouteUtil {
     public static String getUrl(ResourceType resourceType, String resourceId) {
         Long longId = Long.valueOf(resourceId);
 
-        switch(resourceType) {
-            case ISSUE_POST:
-                return getUrl(Issue.finder.byId(longId));
-            case ISSUE_COMMENT:
-                return getUrl(IssueComment.find.byId(longId));
-            case NONISSUE_COMMENT:
-                return getUrl(PostingComment.find.byId(longId));
-            case BOARD_POST:
-                return getUrl(Posting.finder.byId(longId));
-            case COMMIT_COMMENT:
-                return getUrl(CommitComment.find.byId(longId));
-            case PULL_REQUEST:
-                return getUrl(PullRequest.finder.byId(longId));
-            case PULL_REQUEST_COMMENT:
-                return getUrl(PullRequestComment.find.byId(longId));
-            default:
-                throw new IllegalArgumentException(
-                        Resource.getInvalidResourceTypeMessage(resourceType));
+        try {
+            switch(resourceType) {
+                case ISSUE_POST:
+                    return getUrl(Issue.finder.byId(longId));
+                case ISSUE_COMMENT:
+                    return getUrl(IssueComment.find.byId(longId));
+                case NONISSUE_COMMENT:
+                    return getUrl(PostingComment.find.byId(longId));
+                case BOARD_POST:
+                    return getUrl(Posting.finder.byId(longId));
+                case COMMIT_COMMENT:
+                    return getUrl(CommitComment.find.byId(longId));
+                case PULL_REQUEST:
+                    return getUrl(PullRequest.finder.byId(longId));
+                case PULL_REQUEST_COMMENT:
+                    return getUrl(PullRequestComment.find.byId(longId));
+                default:
+                    throw new IllegalArgumentException(
+                            Resource.getInvalidResourceTypeMessage(resourceType));
+            }
+        } catch (Exception e) {
+            play.Logger.error("Failed to get a url to the resource", e);
         }
+
+        return null;
     }
 
     public static String getUrl(Issue issue) {
+        if (issue == null) return null;
+        issue.refresh();
+
         return controllers.routes.IssueApp.issue(
                 issue.project.owner, issue.project.name, issue.getNumber()).url();
     }
 
     public static String getUrl(Posting post) {
+        if (post == null) return null;
+        post.refresh();
+
         return controllers.routes.BoardApp.post(
                 post.project.owner, post.project.name, post.getNumber()).url();
     }
 
     public static String getUrl(IssueComment comment) {
+        if (comment == null) return null;
+
         return getUrl(comment.issue) + "#comment-" + comment.id;
     }
 
     public static String getUrl(PostingComment comment) {
+        if (comment == null) return null;
+
         return getUrl(comment.posting) + "#comment-" + comment.id;
     }
 
     public static String getUrl(PullRequest pullRequest) {
+        if (pullRequest == null) return null;
+
         Project toProject = pullRequest.toProject;
         return controllers.routes.PullRequestApp.pullRequest(
                 toProject.owner, toProject.name, pullRequest.number).url();
     }
 
     public static String getUrl(CommitComment comment) {
+        if (comment == null) return null;
+
         play.mvc.Call toView = controllers.routes.CodeHistoryApp.show(
                 comment.project.owner, comment.project.name, comment.commitId);
         toView = CodeHistoryApp.backToThePullRequestCommitView(toView);
@@ -83,6 +103,8 @@ public class RouteUtil {
     }
 
     public static String getUrl(Comment comment) {
+        if (comment == null) return null;
+
         if (comment instanceof IssueComment) {
             return getUrl((IssueComment) comment);
         } else if (comment instanceof PostingComment) {
@@ -93,6 +115,8 @@ public class RouteUtil {
     }
 
     public static String getUrl(PullRequestComment newComment) {
+        if (newComment == null) return null;
+
         return getUrl(newComment.pullRequest) + "#comment-" + newComment.id;
     }
 }

app/views/index/partial_notifications.scala.html
@@ -64,7 +64,14 @@
                 <img src="@routes.Assets.at("images/default-avatar-64.png")" width="42" height="42">
             </div>
             }
-            <div class="title"><a href="@noti.getUrlToView">@noti.title</a></div>
+            <div class="title">
+                @defining(noti.getUrlToView){ url =>
+                    @url match {
+                        case null => { @noti.title }
+                        case _ => { <a href="@noti.getUrlToView">@noti.title</a> }
+                    }
+                }
+            </div>
             <div class="message-wrap nowrap" id="message-@noti.id">
                 <div class="message">@noti.getMessage</div>
             </div>