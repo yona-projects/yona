ad83f8d680d4f63ce2510d2841703a6d29c36c54
kjkmadness <kjkmadness@nhn.com>
2014-04-03 13:57:38 +0900

    project: fix duplicated site-admin role assignment

--- app/controllers/ProjectApp.java
+++ app/controllers/ProjectApp.java
@@ -219,10 +219,10 @@ public class ProjectApp extends Controller {
         Project project = filledNewProjectForm.get();
         if (Organization.isNameExist(owner)) {
             project.organization = organization;
+            Project.create(project);
         } else {
             ProjectUser.assignRole(UserApp.currentUser().id, Project.create(project), RoleType.MANAGER);
         }
-        Project.create(project);
         RepositoryService.createRepository(project);
         return redirect(routes.ProjectApp.project(project.owner, project.name));
     }
