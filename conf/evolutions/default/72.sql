yona-projects/yona

commit 4ea69ee844e68bb442eaa67b2ef8f0287e31e90a

Review: Tables aren't deleted by downs of 72.sql
This fixes the bug that the tables for Review feature are not deleted
after running the downs part of 72.sql

conf/evolutions/default/72.sql
@@ -457,3 +457,8 @@ WHERE
     notification_event.event_type = 'NEW_COMMENT';
 
 ALTER TABLE notification_event ADD constraint ck_notification_event_resource_type check (resource_type in ('ISSUE_POST','ISSUE_ASSIGNEE','ISSUE_STATE','ISSUE_CATEGORY','ISSUE_MILESTONE','ISSUE_LABEL','BOARD_POST','BOARD_CATEGORY','BOARD_NOTICE','CODE','MILESTONE','WIKI_PAGE','PROJECT_SETTING','SITE_SETTING','USER','USER_AVATAR','PROJECT','ATTACHMENT','ISSUE_COMMENT','NONISSUE_COMMENT','LABEL','PROJECT_LABELS','FORK','COMMIT_COMMENT','PULL_REQUEST','PULL_REQUEST_COMMENT'));
+
+-- Drop the tables for Review feature
+
+DROP TABLE IF EXISTS comment_thread;
+DROP TABLE IF EXISTS review_comment;

commit d4c89df096459627079400109d6c4d0f020e0f8d

Review: Fix sql error of 72.sql caused by 4ea69ee
Reason: The tables for Review feature, review_comment and
comment_thread, MUST NOT be deleted by 72.sql because they are NOT
added by the script. This causes sql error while running the ups part of
72.sql because it assumes the tables exist.

Solution: Delete every rows of the tables instead drop them if need to
avoid the duplication of review comments.

conf/evolutions/default/72.sql
@@ -360,6 +360,9 @@ SELECT
 FROM comment_thread t, review_comment c
 WHERE c.thread_id = t.id and t.pull_request_id IS null;
 
+DELETE FROM review_comment;
+DELETE FROM comment_thread;
+
 -- watch
 
 ALTER TABLE watch DROP CONSTRAINT IF EXISTS ck_watch_resource_type;
@@ -463,8 +466,3 @@ WHERE
     notification_event.event_type = 'NEW_COMMENT';
 
 ALTER TABLE notification_event ADD constraint ck_notification_event_resource_type check (resource_type in ('ISSUE_POST','ISSUE_ASSIGNEE','ISSUE_STATE','ISSUE_CATEGORY','ISSUE_MILESTONE','ISSUE_LABEL','BOARD_POST','BOARD_CATEGORY','BOARD_NOTICE','CODE','MILESTONE','WIKI_PAGE','PROJECT_SETTING','SITE_SETTING','USER','USER_AVATAR','PROJECT','ATTACHMENT','ISSUE_COMMENT','NONISSUE_COMMENT','LABEL','PROJECT_LABELS','FORK','COMMIT_COMMENT','PULL_REQUEST','PULL_REQUEST_COMMENT'));
-
--- Drop the tables for Review feature
-
-DROP TABLE IF EXISTS comment_thread;
-DROP TABLE IF EXISTS review_comment;