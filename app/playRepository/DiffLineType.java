8866340f8d563eceb31a2ce3e3ab9d17401d6bc4
Changsung Kim <changsung.kim@nhn.com>
2014-04-21 23:28:57 +0900

    fix the issue 1133.
    Cause:
    So many files does not have copyright.
    
    Solution:
    The copyright is added to files that do not include it.

--- app/playRepository/DiffLineType.java
+++ app/playRepository/DiffLineType.java
@@ -1,12 +1,25 @@
+/**
+ * Yobi, Project Hosting SW
+ *
+ * Copyright 2013 NAVER Corp.
+ * http://yobi.io
+ *
+ * @Author Yi EungJun
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package playRepository;
 
-/**
- * Created with IntelliJ IDEA.
- * User: nori
- * Date: 13. 9. 12
- * Time: 오전 10:29
- * To change this template use File | Settings | File Templates.
- */
 public enum DiffLineType {
     CONTEXT, ADD, REMOVE
 }
--- test/support/Helpers.java
+++ test/support/Helpers.java
@@ -1,3 +1,23 @@
+/**
+ * Yobi, Project Hosting SW
+ *
+ * Copyright 2013 NAVER Corp.
+ * http://yobi.io
+ *
+ * @Author Jungkook Kim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package support;
 
 import java.util.HashMap;
@@ -14,11 +34,6 @@ import play.GlobalSettings;
 import play.test.FakeApplication;
 import utils.YamlUtil;
 
-/**
- * User: nori
- * Date: 13. 7. 16
- * Time: 오후 6:18
- */
 public class Helpers {
     public static Map<String, String> makeTestConfig() {
         HashMap<String, String> config = new HashMap<>(play.test.Helpers.inMemoryDatabase());
