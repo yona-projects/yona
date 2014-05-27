cccd4e02df106f9be4af61ba1640a641a1dd05f4
Keesun Baik <keesun.baik@nhn.com>
2014-05-27 15:42:51 +0900

    PullRequest: fixed to show reviewer's name instead of null
    added user.refresh() before using it.

--- app/utils/TemplateHelper.scala
+++ app/utils/TemplateHelper.scala
@@ -138,6 +138,7 @@ object TemplateHelper {
   }
 
   def getUserAvatar(user: models.User, avatarSize:String = "small") = {
+    user.refresh();
     var userInfoURL = routes.UserApp.userInfo(user.loginId).toString()
 
     "<a href=\"" + userInfoURL + "\" class=\"usf-group\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"" + user.name + "\"><img src=\"" + user.avatarUrl + "\" class=\"avatar-wrap " + avatarSize + "\"></a>"
