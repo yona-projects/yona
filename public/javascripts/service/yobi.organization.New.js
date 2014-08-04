0251d93ea851af311a6ee8f7f9d181f168283c12
Jihan Kim <kim.jihan@navercorp.com>
2014-08-04 11:17:34 +0900

    Use ui.Spinner to integrate spinner UI

--- public/javascripts/service/yobi.organization.New.js
+++ public/javascripts/service/yobi.organization.New.js
@@ -89,24 +89,7 @@
                 $('span.warning').hide();
                 $('span.msg').html(aErrors[0].message).show();
             } else {
-                new Spinner({
-                    lines: 13, // The number of lines to draw
-                    length: 10, // The length of each line
-                    width: 5, // The line thickness
-                    radius: 10, // The radius of the inner circle
-                    corners: 1, // Corner roundness (0..1)
-                    rotate: 0, // The rotation offset
-                    direction: 1, // 1: clockwise, -1: counterclockwise
-                    color: '#000', // #rgb or #rrggbb
-                    speed: 1, // Rounds per second
-                    trail: 60, // Afterglow percentage
-                    shadow: false, // Whether to render a shadow
-                    hwaccel: false, // Whether to use hardware acceleration
-                    className: 'spinner', // The CSS class to assign to the spinner
-                    zIndex: 2e9, // The z-index (defaults to 2000000000)
-                    top: 'auto', // Top position relative to parent in px
-                    left: 'auto' // Left position relative to parent in px
-                }).spin(document.forms[htVar.sFormName]);
+                yobi.ui.Spinner.show();
             }
         }
 
