6dd6fe1fc6203ce0c27e0a05c7374707eb229bb9
Yi EungJun <eungjun.yi@navercorp.com>
2014-09-16 18:50:10 +0900

    NotifiationMail: Fix ugly footer in reply
    If the recipient read a notification mail in text/plain, he or she may
    see so many blank lines in the footer, if the mail service takes all of
    the newlines as they come while transcoding HTML to TEXT.
    
    This commit fixes it by removing newlines from the footer.

--- app/models/NotificationMail.java
+++ app/models/NotificationMail.java
@@ -293,12 +293,22 @@ public class NotificationMail extends Model {
 
     private static String getHtmlMessage(Lang lang, String message, String urlToView, Resource resource) {
         String content = getRenderedHTMLWithTemplate(lang, Markdown.render(message), urlToView, resource);
+
         Document doc = Jsoup.parse(content);
 
         handleLinks(doc);
         handleImages(doc);
 
-        return doc.html();
+        // Do not add "pretty" spaces. Some mail services render a text/plain
+        // alternative body in an ugly way. If you are using the such email
+        // service, forward or reply this notification email and the recipients
+        // read the email in plain text, they will see ugly whitespaces in the
+        // footer.
+        doc.outputSettings().prettyPrint(false);
+
+        String html = doc.html();
+
+        return html;
     }
 
     private static String getRenderedHTMLWithTemplate(Lang lang, String message, String urlToView, Resource resource){
