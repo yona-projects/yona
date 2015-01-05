a4dee0c0ac52712d0cab40698e3ca00635ef363e
hyeok.oh <heyok.oh@navercorp.com>
2015-01-05 19:23:32 +0900

    그룹 이름 변경 시 예외가 발생합니다. issue #810
    그룹 이름 변경 하는 부분 수정하였습니다.

--- public/javascripts/service/yobi.organization.Setting.js
+++ public/javascripts/service/yobi.organization.Setting.js
@@ -21,16 +21,28 @@
 (function(ns){
 
     var oNS = $yobi.createNamespace(ns);
-    oNS.container[oNS.name] = function(){
+    oNS.container[oNS.name] = function(htOptions){
 
+        var htVar = {};
         var htElement = {};
 
         /**
          * initialize
          */
-        function _init(){
+        function _init(htOptions){
+            _initVar(htOptions);
             _initElement();
             _attachEvent();
+
+            _initFormValidator();
+        }
+
+        /**
+         * initialize variables
+         */
+        function _initVar(htOptions){
+            htVar.sFormName = htOptions.sFormName || "update-org";
+            htVar.rxOrgName = /^[a-zA-Z0-9-]+([_.][a-zA-Z0-9-]+)*$/;
         }
 
         /**
@@ -60,7 +72,43 @@
             htElement.welForm.submit();
         }
 
-        _init();
+        /**
+         * initialize formValidator
+         * @require validate.js
+         */
+        function _initFormValidator(){
+            // name : name of input element
+            // rules: rules to apply to the input element.
+            var aRules = [];
+
+            htVar.oValidator = new FormValidator(htVar.sFormName, aRules, function(aErrors){
+                var oForm = $(document.forms[htVar.sFormName]);
+                var oElement = oForm.find("input[name=name]");
+                var sOrgName = oElement.val();
+                if(!htVar.rxOrgName.test(sOrgName)){
+                    aErrors.push({
+                        id: oElement.attr("id"),
+                        name: oElement.attr("name"),
+                        message: Messages("organization.name.alert")
+                    });
+                }
+                _onFormValidate(aErrors);
+            });
+        }
+
+        /**
+         * handler for validation errors.
+         */
+        function _onFormValidate(aErrors){
+            if(aErrors.length > 0){
+                $('span.warning').hide();
+                $('span.msg').html(aErrors[0].message).show();
+            } else {
+                yobi.ui.Spinner.show();
+            }
+        }
+
+        _init(htOptions || {});
     };
 
 })("yobi.organization.Setting");
