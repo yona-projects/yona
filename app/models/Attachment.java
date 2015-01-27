naver/yobi

Attachment: Support streamming
Attachment can read contents of a file to attach from input stream.

app/models/Attachment.java
@@ -20,10 +20,23 @@
  */
 package models;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import controllers.AttachmentApp;
+import models.enumeration.ResourceType;
+import models.resource.GlobalResource;
+import models.resource.Resource;
+import models.resource.ResourceConvertible;
+import org.apache.commons.io.FileUtils;
+import play.data.validation.Constraints;
+import play.db.ebean.Model;
+import play.libs.Akka;
+import scala.concurrent.duration.Duration;
+import java.nio.file.NotDirectoryException;
+import utils.FileUtil;
+import utils.JodaDateUtil;
+
+import javax.annotation.Nullable;
+import javax.persistence.*;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
@@ -33,30 +46,6 @@ import java.util.Date;
 import java.util.Formatter;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-import javax.persistence.*;
-
-import controllers.AttachmentApp;
-import models.resource.GlobalResource;
-import models.resource.Resource;
-import models.resource.ResourceConvertible;
-
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
-import org.apache.tika.Tika;
-
-import models.enumeration.ResourceType;
-
-import org.apache.tika.metadata.Metadata;
-import org.apache.tika.mime.MediaType;
-import play.data.validation.*;
-
-import play.db.ebean.Model;
-import play.libs.Akka;
-import scala.concurrent.duration.Duration;
-import java.nio.file.NotDirectoryException;
-import utils.FileUtil;
-import utils.JodaDateUtil;
 
 @Entity
 public class Attachment extends Model implements ResourceConvertible {
@@ -208,32 +197,25 @@ public class Attachment extends Model implements ResourceConvertible {
      * @throws NoSuchAlgorithmException
      * @throws IOException
      */
-    private static String moveFileIntoUploadDirectory(File file)
+    private static File moveFileIntoUploadDirectory(File file)
             throws NoSuchAlgorithmException, IOException {
         // Compute sha1 checksum.
-        MessageDigest algorithm = MessageDigest.getInstance("SHA1");
+        InputStream is = new FileInputStream(file);
         byte buf[] = new byte[10240];
-        FileInputStream fis = new FileInputStream(file);
-        for (int size = 0; size >= 0; size = fis.read(buf)) {
-            algorithm.update(buf, 0, size);
+        MessageDigest algorithm = MessageDigest.getInstance("SHA1");
+        for (int size1 = 0; size1 >= 0; size1 = is.read(buf)) {
+            algorithm.update(buf, 0, size1);
         }
-        Formatter formatter = new Formatter();
-        for (byte b : algorithm.digest()) {
-            formatter.format("%02x", b);
-        }
-        String hash = formatter.toString();
-        formatter.close();
-        fis.close();
+        is.close();
+        String hash = toHex(algorithm.digest());
 
+        return moveFileIntoUploadDirectory(file, hash);
+    }
+
+    private static File moveFileIntoUploadDirectory(File file, String hash)
+            throws NoSuchAlgorithmException, IOException {
         // Store the file.
-        // Before do that, create upload directory if it doesn't exist.
-        File uploads = new File(uploadDirectory);
-        uploads.mkdirs();
-        if (!uploads.isDirectory()) {
-            throw new NotDirectoryException(
-                    "'" + uploads.getAbsolutePath() + "' is not a directory.");
-        }
-        File attachedFile = new File(uploadDirectory, hash);
+        File attachedFile = new File(createUploadDirectory(), hash);
         boolean isMoved = file.renameTo(attachedFile);
 
         if(!isMoved){
@@ -241,9 +223,7 @@ public class Attachment extends Model implements ResourceConvertible {
             file.delete();
         }
 
-        // Close all resources.
-
-        return hash;
+        return attachedFile;
     }
 
     /**
@@ -269,36 +249,8 @@ public class Attachment extends Model implements ResourceConvertible {
      */
     @Transient
     public boolean store(File file, String name, Resource container) throws IOException, NoSuchAlgorithmException {
-        // Store the file as its SHA1 hash in filesystem, and record its
-        // metadata - containerType, containerId, size and hash - in Database.
-        this.containerType = container.getType();
-        this.containerId = container.getId();
-        this.createdDate = JodaDateUtil.now();
-
-        if (name == null) {
-            this.name = file.getName();
-        } else {
-            this.name = name;
-        }
-
-        if (this.mimeType == null) {
-            this.mimeType = FileUtil.detectMediaType(file, name).toString();
-        }
-
-        // the size must be set before it is moved.
-        this.size = file.length();
-        this.hash = Attachment.moveFileIntoUploadDirectory(file);
-
-        // Add the attachment into the Database only if there is no same record.
-        Attachment sameAttach = Attachment.findBy(this);
-        if (sameAttach == null) {
-            super.save();
-            return true;
-        } else {
-            this.id = sameAttach.id;
-            return false;
-        }
-   }
+        return save(moveFileIntoUploadDirectory(file), name, container);
+    }
 
     /**
      * Gets a file which mathces the hash from the Upload Directory.
@@ -510,4 +462,86 @@ public class Attachment extends Model implements ResourceConvertible {
                 ", createdDate=" + createdDate +
                 '}';
     }
+
+    public boolean store(InputStream inputStream, @Nullable String fileName,
+                         Resource container) throws
+            IOException, NoSuchAlgorithmException {
+        byte buf[] = new byte[10240];
+
+        // Compute hash and store the stream as a temp file
+        MessageDigest algorithm = MessageDigest.getInstance("SHA1");
+        int size1;
+        File tmpFile = File.createTempFile("yobi", null);
+        FileOutputStream fos = new FileOutputStream(tmpFile);
+        while ((size1 = inputStream.read(buf)) != -1) {
+            algorithm.update(buf, 0, size1);
+            fos.write(buf, 0, size1);
+        }
+        String hash = toHex(algorithm.digest());
+
+        // Close resources
+        fos.flush();
+        fos.close();
+        inputStream.close();
+
+        // Save this attachment with metadata
+        return save(moveFileIntoUploadDirectory(tmpFile, hash), fileName, container);
+    }
+
+    /**
+     * Save this attachment with metadata from the given arguments.
+     *
+     * @param file       the file to be attached whose name is hash of the contents
+     * @param fileName   the name of this attachment
+     * @param container  the container to which the file attached
+     * @return
+     * @throws IOException
+     */
+    private boolean save(File file, String fileName, Resource container) throws
+            IOException {
+        // Store the file as its SHA1 hash in filesystem, and record its
+        // metadata - containerType, containerId, createdDate, name, size, hash and
+        // mimeType - in Database.
+        this.containerType = container.getType();
+        this.containerId = container.getId();
+        this.createdDate = JodaDateUtil.now();
+        this.name = fileName;
+        this.hash = file.getName();
+        this.size = file.length();
+        if (this.mimeType == null) {
+            this.mimeType = FileUtil.detectMediaType(file, name).toString();
+        }
+
+        // Add the attachment into the Database only if there is no same record.
+        Attachment sameAttach = Attachment.findBy(this);
+        if (sameAttach == null) {
+            super.save();
+            return true;
+        } else {
+            this.id = sameAttach.id;
+            return false;
+        }
+    }
+
+    private static String toHex(byte[] bytes) {
+        Formatter formatter = new Formatter();
+        for (byte b : bytes) {
+            formatter.format("%02x", b);
+        }
+        String hex = formatter.toString();
+        formatter.close();
+        return hex;
+    }
+
+    // Create the upload directory if it doesn't exist.
+    private static File createUploadDirectory() throws NotDirectoryException {
+        File uploads = new File(uploadDirectory);
+        uploads.mkdirs();
+        if (!uploads.isDirectory()) {
+            throw new NotDirectoryException(
+                    "'" + uploads.getAbsolutePath() + "' is not a directory.");
+        }
+        return uploads;
+    }
+
 }

yona-projects/yona

app/models/Attachment.java
@@ -20,11 +20,24 @@
  */
 package models;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import controllers.AttachmentApp;
+import models.enumeration.ResourceType;
+import models.resource.GlobalResource;
+import models.resource.Resource;
+import models.resource.ResourceConvertible;
+import org.apache.commons.io.FileUtils;
+import play.data.validation.Constraints;
+import play.db.ebean.Model;
+import play.libs.Akka;
+import scala.concurrent.duration.Duration;
+import utils.FileUtil;
+import utils.JodaDateUtil;
+
+import javax.annotation.Nullable;
+import javax.persistence.*;
+import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.NotDirectoryException;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -33,30 +46,6 @@ import java.util.Date;
 import java.util.Formatter;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-import javax.persistence.*;
-
-import controllers.AttachmentApp;
-import models.resource.GlobalResource;
-import models.resource.Resource;
-import models.resource.ResourceConvertible;
-
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
-import org.apache.tika.Tika;
-
-import models.enumeration.ResourceType;
-
-import org.apache.tika.metadata.Metadata;
-import org.apache.tika.mime.MediaType;
-import play.data.validation.*;
-
-import play.db.ebean.Model;
-import play.libs.Akka;
-import scala.concurrent.duration.Duration;
-import java.nio.file.NotDirectoryException;
-import utils.FileUtil;
-import utils.JodaDateUtil;
 
 @Entity
 public class Attachment extends Model implements ResourceConvertible {
@@ -208,32 +197,25 @@ public class Attachment extends Model implements ResourceConvertible {
      * @throws NoSuchAlgorithmException
      * @throws IOException
      */
-    private static String moveFileIntoUploadDirectory(File file)
+    private static File moveFileIntoUploadDirectory(File file)
             throws NoSuchAlgorithmException, IOException {
         // Compute sha1 checksum.
-        MessageDigest algorithm = MessageDigest.getInstance("SHA1");
+        InputStream is = new FileInputStream(file);
         byte buf[] = new byte[10240];
-        FileInputStream fis = new FileInputStream(file);
-        for (int size = 0; size >= 0; size = fis.read(buf)) {
-            algorithm.update(buf, 0, size);
+        MessageDigest algorithm = MessageDigest.getInstance("SHA1");
+        for (int readSize = 0; readSize >= 0; readSize = is.read(buf)) {
+            algorithm.update(buf, 0, readSize);
         }
-        Formatter formatter = new Formatter();
-        for (byte b : algorithm.digest()) {
-            formatter.format("%02x", b);
-        }
-        String hash = formatter.toString();
-        formatter.close();
-        fis.close();
+        is.close();
+        String hash = toHex(algorithm.digest());
 
+        return moveFileIntoUploadDirectory(file, hash);
+    }
+
+    private static File moveFileIntoUploadDirectory(File file, String hash)
+            throws NoSuchAlgorithmException, IOException {
         // Store the file.
-        // Before do that, create upload directory if it doesn't exist.
-        File uploads = new File(uploadDirectory);
-        uploads.mkdirs();
-        if (!uploads.isDirectory()) {
-            throw new NotDirectoryException(
-                    "'" + uploads.getAbsolutePath() + "' is not a directory.");
-        }
-        File attachedFile = new File(uploadDirectory, hash);
+        File attachedFile = new File(createUploadDirectory(), hash);
         boolean isMoved = file.renameTo(attachedFile);
 
         if(!isMoved){
@@ -241,9 +223,7 @@ public class Attachment extends Model implements ResourceConvertible {
             file.delete();
         }
 
-        // Close all resources.
-
-        return hash;
+        return attachedFile;
     }
 
     /**
@@ -269,36 +249,8 @@ public class Attachment extends Model implements ResourceConvertible {
      */
     @Transient
     public boolean store(File file, String name, Resource container) throws IOException, NoSuchAlgorithmException {
-        // Store the file as its SHA1 hash in filesystem, and record its
-        // metadata - containerType, containerId, size and hash - in Database.
-        this.containerType = container.getType();
-        this.containerId = container.getId();
-        this.createdDate = JodaDateUtil.now();
-
-        if (name == null) {
-            this.name = file.getName();
-        } else {
-            this.name = name;
-        }
-
-        if (this.mimeType == null) {
-            this.mimeType = FileUtil.detectMediaType(file, name).toString();
-        }
-
-        // the size must be set before it is moved.
-        this.size = file.length();
-        this.hash = Attachment.moveFileIntoUploadDirectory(file);
-
-        // Add the attachment into the Database only if there is no same record.
-        Attachment sameAttach = Attachment.findBy(this);
-        if (sameAttach == null) {
-            super.save();
-            return true;
-        } else {
-            this.id = sameAttach.id;
-            return false;
-        }
-   }
+        return save(moveFileIntoUploadDirectory(file), name, container);
+    }
 
     /**
      * Gets a file which mathces the hash from the Upload Directory.
@@ -510,4 +462,87 @@ public class Attachment extends Model implements ResourceConvertible {
                 ", createdDate=" + createdDate +
                 '}';
     }
+
+    public boolean store(InputStream inputStream, @Nullable String fileName,
+                         Resource container) throws
+            IOException, NoSuchAlgorithmException {
+        byte buf[] = new byte[10240];
+
+        // Compute hash and store the stream as a temp file
+        MessageDigest algorithm = MessageDigest.getInstance("SHA1");
+        String tempFileHash;
+        File tmpFile = File.createTempFile("yobi", null);
+        FileOutputStream fos = new FileOutputStream(tmpFile);
+        try {
+            int readSize;
+            while ((readSize = inputStream.read(buf)) != -1) {
+                algorithm.update(buf, 0, readSize);
+                fos.write(buf, 0, readSize);
+            }
+            tempFileHash = toHex(algorithm.digest());
+            fos.flush();
+        } finally {
+            fos.close();
+        }
+
+        // Save this attachment with metadata
+        return save(moveFileIntoUploadDirectory(tmpFile, tempFileHash), fileName, container);
+    }
+
+    /**
+     * Save this attachment with metadata from the given arguments.
+     *
+     * @param file       the file to be attached whose name is hash of the contents
+     * @param fileName   the name of this attachment
+     * @param container  the container to which the file attached
+     * @return
+     * @throws IOException
+     */
+    private boolean save(File file, String fileName, Resource container) throws
+            IOException {
+        // Store the file as its SHA1 hash in filesystem, and record its
+        // metadata - containerType, containerId, createdDate, name, size, hash and
+        // mimeType - in Database.
+        this.containerType = container.getType();
+        this.containerId = container.getId();
+        this.createdDate = JodaDateUtil.now();
+        this.name = fileName;
+        this.hash = file.getName();
+        this.size = file.length();
+        if (this.mimeType == null) {
+            this.mimeType = FileUtil.detectMediaType(file, name).toString();
+        }
+
+        // Add the attachment into the Database only if there is no same record.
+        Attachment sameAttach = Attachment.findBy(this);
+        if (sameAttach == null) {
+            super.save();
+            return true;
+        } else {
+            this.id = sameAttach.id;
+            return false;
+        }
+    }
+
+    private static String toHex(byte[] bytes) {
+        Formatter formatter = new Formatter();
+        for (byte b : bytes) {
+            formatter.format("%02x", b);
+        }
+        String hex = formatter.toString();
+        formatter.close();
+        return hex;
+    }
+
+    // Create the upload directory if it doesn't exist.
+    private static File createUploadDirectory() throws NotDirectoryException {
+        File uploads = new File(uploadDirectory);
+        uploads.mkdirs();
+        if (!uploads.isDirectory()) {
+            throw new NotDirectoryException(
+                    "'" + uploads.getAbsolutePath() + "' is not a directory.");
+        }
+        return uploads;
+    }
+
 }