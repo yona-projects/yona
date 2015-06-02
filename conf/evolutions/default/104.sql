yona-projects/yona

commit fd191e6185bac7e4cf3b6d244d153b7d95592996

project-transfer: preserve previous links after project was renamed or moved
When a project is renamed or moved to another place,
previous links are broken. So, it is required
to preserve previous links not to cause confusion.

Notes:
  Once you rename or move a project, only links related to the first place of project
  are preserved for 24 hours. Which means that no matter how many times
  you change project's name or transfer it, just only first location
  before the change started will be preserved for 24 hours

conf/evolutions/default/104.sql
@@ -1,0 +1,15 @@
+# --- !Ups
+
+ALTER TABLE PROJECT ADD previous_name VARCHAR(255);
+ALTER TABLE PROJECT ADD previous_owner_login_id VARCHAR(255);
+ALTER TABLE PROJECT ADD previous_name_changed_time BIGINT;
+
+CREATE INDEX ix_project_previous_01 ON project(previous_owner_login_id, previous_name);
+
+# --- !Downs
+
+ALTER TABLE project DROP COLUMN IF EXISTS previous_name;
+ALTER TABLE project DROP COLUMN IF EXISTS previous_owner_login_id;
+ALTER TABLE project DROP COLUMN IF EXISTS previous_name_changed_time;
+
+DROP INDEX IF EXISTS ix_project_previous_01;

app/actions/AbstractProjectCheckAction.java
@@ -29,11 +29,11 @@ import play.i18n.Messages;
 import play.mvc.Action;
 import play.mvc.Http.Context;
 import play.mvc.Result;
-import play.mvc.Result;
 import play.libs.F.Promise;
 import utils.AccessControl;
 import utils.AccessLogger;
 import utils.ErrorViews;
+import utils.RedirectUtil;
 
 import static play.mvc.Controller.flash;
 
@@ -58,6 +58,11 @@ public abstract class AbstractProjectCheckAction<T> extends Action<T> {
         Promise<Result> promise;
 
         if (project == null) {
+            Project previousProject = Project.findByPreviousPlaceOf(ownerLoginId, projectName);
+            if (previousProject != null) {
+                return RedirectUtil.redirect(previousProject);
+            }
+
             if (UserApp.currentUser() == User.anonymous){
                 flash("failed", Messages.get("error.auth.unauthorized.waringMessage"));
                 promise = Promise.pure((Result) forbidden(ErrorViews.Forbidden.render("error.forbidden.or.notfound", context.request().path())));

app/actions/NullProjectCheckAction.java
@@ -28,10 +28,10 @@ import play.i18n.Messages;
 import play.mvc.Action;
 import play.mvc.Http;
 import play.mvc.Result;
-import play.mvc.Result;
 import play.libs.F.Promise;
 import utils.AccessLogger;
 import utils.ErrorViews;
+import utils.RedirectUtil;
 
 import static play.mvc.Controller.flash;
 
@@ -54,6 +54,10 @@ public class NullProjectCheckAction extends Action<Void> {
 
         if (project == null) {
             Promise<Result> promise;
+            Project previousProject = Project.findByPreviousPlaceOf(ownerLoginId, projectName);
+            if (previousProject != null) {
+                return RedirectUtil.redirect(previousProject);
+            }
 
             if (UserApp.currentUser() == User.anonymous){
                 flash("failed", Messages.get("error.auth.unauthorized.waringMessage"));

app/actions/support/PathParser.java
@@ -24,6 +24,7 @@ import org.apache.commons.lang3.StringUtils;
 import play.mvc.Http;
 
 import javax.annotation.Nonnull;
+import java.util.Arrays;
 
 /**
  * Parse URLs related with Project.
@@ -71,4 +72,11 @@ public class PathParser {
         return DELIM + StringUtils.join(this.pathSegments, DELIM);
     }
 
+    public String restOfPathExceptOwnerAndProjectName() {
+        if(pathSegments != null && pathSegments.length > 2){
+            return StringUtils.join(Arrays.copyOfRange(pathSegments, 2, pathSegments.length), "/");
+        } else {
+            return "";
+        }
+    }
 }

app/controllers/GitApp.java
@@ -89,7 +89,12 @@ public class GitApp extends Controller {
         Project project = Project.findByOwnerAndProjectName(ownerName, projectName);
 
         if (project == null) {
-            return notFound();
+            Project previousProject = Project.findByPreviousPlaceOf(ownerName, projectName);
+            if (previousProject != null) {
+                project = previousProject;
+            } else {
+                return notFound();
+            }
         }
 
         if (!project.vcs.equals(RepositoryService.VCS_GIT)) {
@@ -146,5 +151,4 @@ public class GitApp extends Controller {
             throws UnsupportedOperationException, IOException, ServletException {
         return GitApp.service(ownerName, projectName, service, false);
     }
-
 }

app/controllers/ProjectApp.java
@@ -263,6 +263,7 @@ public class ProjectApp extends Controller {
         }
 
         if (!project.name.equals(updatedProject.name)) {
+            updatedProject.recordRenameOrTransferHistoryIfLastChangePassed24HoursFrom(project);
             if (!repository.renameTo(updatedProject.name)) {
                 throw new FileOperationException("fail repository rename to " + project.owner + "/" + updatedProject.name);
             }
@@ -592,6 +593,7 @@ public class ProjectApp extends Controller {
         Organization newOwnerOrg = Organization.findByName(pt.destination);
 
         // Change the project's information.
+        project.recordRenameOrTransferHistoryIfLastChangePassed24HoursFrom(project);
         project.owner = pt.destination;
         project.name = newProjectName;
         if (newOwnerOrg != null) {

app/controllers/SvnApp.java
@@ -108,7 +108,12 @@ public class SvnApp extends Controller {
         Project project = Project.findByOwnerAndProjectName(userName, projectName);
 
         if (project == null) {
-            return notFound();
+            Project previousProject = Project.findByPreviousPlaceOf(userName, projectName);
+            if (previousProject != null) {
+                project = previousProject;
+            } else {
+                return notFound();
+            }
         }
 
         if (!project.vcs.equals(RepositoryService.VCS_SUBVERSION)) {

app/models/Project.java
@@ -29,6 +29,7 @@ import models.enumeration.ResourceType;
 import models.enumeration.RoleType;
 import models.resource.GlobalResource;
 import models.resource.Resource;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.NoHeadException;
@@ -43,6 +44,7 @@ import utils.FileUtil;
 import utils.JodaDateUtil;
 import validation.ExConstraints;
 
+import javax.annotation.Nonnull;
 import javax.persistence.*;
 import javax.servlet.ServletException;
 import java.io.IOException;
@@ -131,6 +133,10 @@ public class Project extends Model implements LabelOwner {
 
     @OneToOne(mappedBy = "project", cascade = CascadeType.ALL)
     public ProjectMenuSetting menuSetting;
+
+    private String previousOwnerLoginId;
+    private String previousName;
+    private Long previousNameChangedTime;
 
     /**
      * @see {@link User#SITE_MANAGER_ID}
@@ -578,6 +584,27 @@ public class Project extends Model implements LabelOwner {
         return menuSetting == null || menuSetting.code;
     }
 
+    /**
+     * When a project is renamed or transferred, record it's previous location and change time
+     *
+     * @param project
+     */
+    public void recordRenameOrTransferHistoryIfLastChangePassed24HoursFrom(@Nonnull Project project) {
+        if(isRenamedOrTransferredIn24Hours(project)) {
+            this.previousNameChangedTime = DateTime.now().getMillis();
+            this.previousName = project.name;
+            this.previousOwnerLoginId = project.owner;
+        }
+    }
+
+    private static boolean isRenamedOrTransferredIn24Hours(@Nonnull Project project) {
+        return project.previousNameChangedTime == null || hasPassed24hoursFrom(project.previousNameChangedTime);
+    }
+
+    private static boolean hasPassed24hoursFrom(Long time) {
+        return new Duration(DateTime.now().getMillis() - time).getStandardHours() > 24;
+    }
+
     public enum State {
         PUBLIC, PRIVATE, ALL
     }
@@ -778,6 +805,51 @@ public class Project extends Model implements LabelOwner {
             return RepositoryService.VCS_SUBVERSION;
         } else {
             return RepositoryService.VCS_GIT;
+        }
+    }
+
+    /**
+     * Find project with previous owner and previous project name
+     *
+     * when to use:
+     *  When specific project can't be found.
+     *
+     *  In some cases, it is reasonable to assume that project was moved or transferred.
+     *  In that case, try this method.
+     *
+     *  This method is intended to be used at controllers.
+     *
+     * @param previousOwnerLoginid
+     * @param previousName
+     * @return
+     */
+
+    public static Project findByPreviousPlaceOf(String previousOwnerLoginid, String previousName) {
+        List<Project> projects = find.where().ieq("previousOwnerLoginId", previousOwnerLoginid).ieq("previousName", previousName)
+            .setOrderBy("previousNameChangedTime desc").findList();
+        if(CollectionUtils.isEmpty(projects)){
+            return null;
+        }
+        return projects.get(0);  // Choose latest
+    }
+
+    public boolean hasOldPlace(){
+        if(StringUtils.isBlank(this.previousName)){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public String getOldPlace(){
+        if(this.previousOwnerLoginId == null){
+            return "";
+        }
+
+        if(hasOldPlace()){
+            return this.previousOwnerLoginId + "/" + this.previousName;
+        } else {
+            return "";
         }
     }
 }

commit 4cfd6b24122801830f86bbda09802c9e9a7a1354

Fix sql error while running downscript of 104.sql
This bug is caused by fd191e6.

conf/evolutions/default/104.sql
@@ -8,8 +8,8 @@ CREATE INDEX ix_project_previous_01 ON project(previous_owner_login_id, previous
 
 # --- !Downs
 
+DROP INDEX IF EXISTS ix_project_previous_01;
+
 ALTER TABLE project DROP COLUMN IF EXISTS previous_name;
 ALTER TABLE project DROP COLUMN IF EXISTS previous_owner_login_id;
 ALTER TABLE project DROP COLUMN IF EXISTS previous_name_changed_time;
-
-DROP INDEX IF EXISTS ix_project_previous_01;