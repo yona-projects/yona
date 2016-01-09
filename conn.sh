2b025b8c5c8f671c642d5eff505393835b136ae6
Suwon Chae <doortts@gmail.com>
2016-01-09 12:34:59 +0900

    h2db: Add local DB connection script for *nix

---
+++ conn.sh
@@ -0,0 +1,1 @@
+java -cp ~/.ivy2/cache/com.h2database/h2/jars/h2-1.3.176.jar org.h2.tools.Shell -user "" -password "" -url "jdbc:h2:file:./yobi;CACHE_SIZE=131072;MODE=PostgreSQL;MVCC=FALSE;MV_STORE=FALSE"
