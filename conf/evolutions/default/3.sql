c85ddc0ab292fc9ea49a4cbcc9294007a12c5a42
Suwon Chae <doortts@gmail.com>
2016-02-16 02:02:02 +0900

    perf: Increase performance with indexes
    It was found that there are full search queries.
    Especially more issue/post watch and unwatch users are increased
    more slow to show page. It fix by index creation.
    
    Finally it is possible to increase 20% faster overall
    accompany with CacheStore

---
+++ conf/evolutions/default/3.sql
@@ -0,0 +1,12 @@
+# --- !Ups
+create unique index uq_n4user_1 on n4user (login_id);
+create index ix_notification_event_created on notification_event (created DESC)
+create index ix_watch_resource_id_resource_type on watch(resource_id, resource_type);
+create index ix_unwatch_resource_id_resource_type on unwatch(resource_id, resource_type);
+
+
+# --- !Downs
+drop index uq_n4user_1 on n4user;
+drop index ix_notification_event_created on notification_event;
+drop index ix_watch_resource_id_resource_type on watch;
+drop index ix_unwatch_resource_id_resource_type on unwatch;
