5e2428a178ff122d647a924b14afebdd6c1bc4c4
Suwon Chae <doortts@gmail.com>
2016-02-16 21:36:21 +0900

    perf: Cache markdown rendering results
    It is found that it is very costly job to render markdown
    and sanitize it. So introduce two things.
    LRU cache and compressing rendered text.
    
    LRU cache is driven by google Guava.
    Compressing text is accomplished using native java sdk.
    
    Now, view page loading speed is about 15x faster!!
    and cache size is 30% smaller.

---
+++ app/utils/ZipUtil.java
@@ -0,0 +1,38 @@
+package utils;
+
+import java.io.*;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.InflaterInputStream;
+
+/**
+ * ZipUtil
+ *
+ * Intended to compress string contents
+ */
+public class ZipUtil {
+    public static byte[] compress(String text) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            OutputStream out = new DeflaterOutputStream(baos);
+            out.write(text.getBytes("UTF-8"));
+            out.close();
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+        return baos.toByteArray();
+    }
+
+    public static String decompress(byte[] bytes) {
+        InputStream in = new InflaterInputStream(new ByteArrayInputStream(bytes));
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            byte[] buffer = new byte[8192];
+            int len;
+            while((len = in.read(buffer))>0)
+                baos.write(buffer, 0, len);
+            return new String(baos.toByteArray(), "UTF-8");
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+    }
+}
