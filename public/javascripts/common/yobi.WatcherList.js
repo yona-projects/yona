ac9b65d2b6400cd5d99f18ec8dbe7a5131dc4a99
Suwon Chae <doortts@gmail.com>
2017-01-16 21:28:22 +0900

    watcher-list: Remove watcher list button in case of only me

--- public/javascripts/common/yobi.WatcherList.js
+++ public/javascripts/common/yobi.WatcherList.js
@@ -2,8 +2,8 @@ var apiUrlMemo;
 $(".show-watchers").on("click", function () {
     $(".watcher-list").toggle();
 });
-function watcherListApi(apiUrl){
 
+function watcherListApi(apiUrl, currentUserInfoUrl){
     if (apiUrl) {
         apiUrlMemo = apiUrl;
     }
@@ -11,7 +11,12 @@ function watcherListApi(apiUrl){
     $.get(apiUrl || apiUrlMemo)
         .done(function (data) {
             var watcherList = "";
-            if( data.watchersInList > 0 ){
+            if ( data.watchersInList && data.watchersInList === 1 && data.watchers[0].url === currentUserInfoUrl) {
+                $(".show-watchers").css("display", "none");
+                return;
+            }
+
+            if( data.watchersInList > 1 ){
                 $(".watcherCount").text(" " + data.totalWatchers);
                 $(".show-watchers").css("display", "inline-block");
                 data.watchers.forEach(function (watcher) {
