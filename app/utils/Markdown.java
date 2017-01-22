1ea8e45f7a26c01312dd66186e495cea6088c6d9
Suwon Chae <doortts@gmail.com>
2017-01-22 16:18:14 +0900

    markdown: Fix repository file reference link interpretation error

--- app/utils/Markdown.java
+++ app/utils/Markdown.java
@@ -1,23 +1,9 @@
 /**
- * Yobi, Project Hosting SW
- *
- * Copyright 2013 NAVER Corp.
- * http://yobi.io
- *
- * @author Keesun Baik
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ * Yona, 21st Century Project Hosting SW
+ * <p>
+ * Copyright Yona & Yobi Authors & NAVER Corp.
+ * https://yona.io
+ **/
 package utils;
 
 import models.Project;
@@ -238,6 +224,7 @@ public class Markdown {
         if (StringUtils.isNotEmpty(root)) {
             root = "/" + root;
         }
-        return text.replaceAll("\\[(?<text>[^\\]]*)\\]\\(\\.?+/?+(?<link>[^\\)]*)\\)", "[$1](/" + root + project.owner + "/" + project.name + "/files/" + project.defaultBranch().replaceAll("refs/heads/", "") + "/$2)");
+        final String regexString = "\\[(?<text>[^\\]]*)\\]\\(\\/?(?!https\\:|http\\:|ftp\\:|file\\:)(?<link>[^\\)]*)\\)";
+        return text.replaceAll(regexString, "[$1](/" + root + project.owner + "/" + project.name + "/files/" + project.defaultBranch().replaceAll("refs/heads/", "") + "/$2)");
     }
 }
