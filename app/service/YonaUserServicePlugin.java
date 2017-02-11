af6b0dffd78743f94a581fd750c5f8dc969b88b2
Suwon Chae <doortts@gmail.com>
2017-02-11 17:59:27 +0900

    social-login: Provide option which sync with local user name

--- app/service/YonaUserServicePlugin.java
+++ app/service/YonaUserServicePlugin.java
@@ -3,10 +3,13 @@ package service;
 import com.feth.play.module.pa.service.UserServicePlugin;
 import com.feth.play.module.pa.user.AuthUser;
 import com.feth.play.module.pa.user.AuthUserIdentity;
+import com.feth.play.module.pa.user.BasicIdentity;
+import models.User;
 import models.UserCredential;
 import play.Application;
 
 public class YonaUserServicePlugin extends UserServicePlugin {
+    private static boolean useSocialNameSync = play.Configuration.root().getBoolean("application.use.social.login.name.sync", false);
 
 	public YonaUserServicePlugin(final Application app) {
 		super(app);
@@ -29,10 +32,28 @@ public class YonaUserServicePlugin extends UserServicePlugin {
 		// ...and dont forget to sync the cache when users get deactivated/deleted
 		final UserCredential u = UserCredential.findByAuthUserIdentity(identity);
 		if(u != null) {
+			if(identity instanceof BasicIdentity){
+				BasicIdentity authUser = ((BasicIdentity) identity);
+				setStatusLoggedIn(u, authUser);
+				if(useSocialNameSync && !u.name.equals(authUser.getName())){
+					updateLocalUserName(u, authUser);
+				}
+			}
 			return u.id;
 		} else {
 			return null;
 		}
+	}
+
+	private void updateLocalUserName(UserCredential u, BasicIdentity authUser) {
+		u.name = authUser.getName();
+		u.update();
+
+		User localUser = User.findByEmail(authUser.getEmail());
+		if(localUser != null){
+            localUser.name = authUser.getName();
+            localUser.update();
+        }
 	}
 
 	@Override
