1d34a92dbcf32fd73a6c559dcfe16d3d5785708d
Suwon Chae <doortts@gmail.com>
2017-02-22 21:45:24 +0900

    project: Handle spaces in name when creating project

--- public/javascripts/service/yobi.project.New.js
+++ public/javascripts/service/yobi.project.New.js
@@ -1,23 +1,9 @@
 /**
- * Yobi, Project Hosting SW
- *
- * Copyright 2012 NAVER Corp.
- * http://yobi.io
- *
- * @author Hwi Ahn
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ * Yona, 21st Century Project Hosting SW
+ * <p>
+ * Copyright Yona & Yobi Authors & NAVER Corp.
+ * https://yona.io
+ **/
 (function(ns){
 
     var oNS = $yobi.createNamespace(ns);
@@ -78,6 +64,11 @@
             htElement.welMenuSettingCode.on('click', _onClickMenuSettingCode);
             htElement.welMenuSettingPullRequest.on('click', _onClickMenuSettingPullRequest);
             htElement.welMenuSettingReview.on('click', _onClickMenuSettingReview);
+            htElement.welInputProjectName.on('focusout', _onFocusoutProjectName);
+        }
+
+        function _onFocusoutProjectName(){
+            htElement.welInputProjectName.val(htElement.welInputProjectName.val().trim().replace(/ /g, '-'));
         }
 
         function _onChangeRepoAuthCheck(){
