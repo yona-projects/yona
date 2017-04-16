45df72fddbc3be188167e97bdc8d1f186cb638aa
Suwon Chae <doortts@gmail.com>
2017-04-17 02:53:53 +0900

    projects: Show no projects message
    See: Yona Github issue #200 #204

--- app/controllers/ProjectApp.java
+++ app/controllers/ProjectApp.java
@@ -1,23 +1,9 @@
 /**
- * Yobi, Project Hosting SW
- *
- * Copyright 2012 NAVER Corp.
- * http://yobi.io
- *
- * @author Sangcheol Hwang
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ * Yona, 21st Century Project Hosting SW
+ * <p>
+ * Copyright Yona & Yobi Authors & NAVER Corp.
+ * https://yona.io
+ **/
 package controllers;
 
 import actions.DefaultProjectCheckAction;
@@ -1001,9 +987,6 @@ public class ProjectApp extends Controller {
 
         el.orderBy("createdDate desc");
         Page<Project> projects = getProjectPage(pageNum, el);
-        if (projects.getList().size() == 0) {
-            return notFound(ErrorViews.NotFound.render("error.notfound"));
-        }
 
         return ok(views.html.project.list.render("title.projectList", projects, query));
     }
