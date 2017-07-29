fa2f3396fa599d5a7fe3ae85c15ee93d24869375
Suwon Chae <doortts@gmail.com>
2017-07-29 21:36:36 +0900

    Revert "mail: Remove header-anchor link from mail body"
    This reverts commit ae653a2012e55bfb5e6efcb7bb5c249db712b15b.

--- app/models/NotificationMail.java
+++ app/models/NotificationMail.java
@@ -546,7 +546,7 @@ public class NotificationMail extends Model {
                 IssueComment issueComment = IssueComment.find.byId(Long.valueOf(resource.getId()));
                 resource = issueComment.issue.asResource();
             }
-            email.setHtmlMsg(removeHeadAnchor(getHtmlMessage(lang, message, urlToView, resource, acceptsReply)));
+            email.setHtmlMsg(getHtmlMessage(lang, message, urlToView, resource, acceptsReply));
             email.setTextMsg(getPlainMessage(lang, message, Url.create(urlToView), acceptsReply));
 
             email.addReferences();
@@ -563,10 +563,6 @@ public class NotificationMail extends Model {
             Logger.warn("Failed to send a notification: "
                     + email + "\n" + ExceptionUtils.getStackTrace(e));
         }
-    }
-
-    private static String removeHeadAnchor(String htmlText) {
-        return htmlText.replaceAll("head-anchor\">#</a>", "></a>");
     }
 
     @Nullable
