2fc76d999ff9842d2468133280de08f79a5f58b7
Mijeong Park <p.mj@naverlabs.com>
2018-01-26 18:19:57 +0900

    util: Add function for getting diff(HTML format) between two values

---
+++ app/utils/DiffUtil.java
@@ -0,0 +1,67 @@
+/**
+ * Yona, 21st Century Project Hosting SW
+ * <p>
+ * Copyright Yona & Yobi Authors & NAVER Corp.
+ * https://yona.io
+ **/
+package utils;
+
+import java.util.LinkedList;
+import java.util.Optional;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+import utils.diff_match_patch.Diff;
+
+public class DiffUtil {
+
+    public static final int EQUAL_TEXT_ELLIPSIS_SIZE = 100;
+    public static final int EQUAL_TEXT_BASE_SIZE = 50;
+    public static final int DIFF_EDITCOST = 8;
+
+    public static String getDiffText(String oldValue, String newValue) {
+
+        oldValue = Optional.ofNullable(oldValue).orElse("");
+        newValue = Optional.ofNullable(newValue).orElse("");
+
+        diff_match_patch dmp = new diff_match_patch();
+        dmp.Diff_EditCost = DIFF_EDITCOST;
+        String diffString = "";
+
+        LinkedList<diff_match_patch.Diff> diffs = dmp.diff_main(oldValue, newValue);
+        dmp.diff_cleanupEfficiency(diffs);
+
+        for (Diff diff: diffs) {
+            switch (diff.operation) {
+                case DELETE:
+                    diffString += "<span style='background-color: #fda9a6;padding: 2px 0;'>";
+                    diffString += StringEscapeUtils.escapeHtml4(diff.text);
+                    diffString += "</span>";
+                    break;
+                case EQUAL:
+                    int textLength = diff.text.length();
+                    if (textLength > EQUAL_TEXT_ELLIPSIS_SIZE) {
+                        diffString += StringEscapeUtils.escapeHtml4(diff.text.substring(0, EQUAL_TEXT_BASE_SIZE));
+
+                        diffString += "<span style='color: #bdbdbd;font-size: 16px;font-family: serif;'>...\n";
+                        diffString += "......\n";
+                        diffString += "......\n";
+                        diffString += "...</span>";
+
+                        diffString += StringEscapeUtils.escapeHtml4(diff.text.substring(textLength - EQUAL_TEXT_BASE_SIZE));
+                    } else {
+                        diffString += StringEscapeUtils.escapeHtml4(diff.text);
+                    }
+                    break;
+                case INSERT:
+                    diffString += "<span style='background-color: #abdd52;padding: 2px 0;'>";
+                    diffString += StringEscapeUtils.escapeHtml4(diff.text);
+                    diffString += "</span>";
+                    break;
+                default:
+                        break;
+            }
+        }
+
+        return diffString.replaceAll("\n", "&nbsp<br/>\n");
+    }
+}
---
+++ test/utils/DiffUtilTest.java
@@ -0,0 +1,106 @@
+/**
+ * Yona, 21st Century Project Hosting SW
+ * <p>
+ * Copyright Yona & Yobi Authors & NAVER Corp.
+ * https://yona.io
+ **/
+package utils;
+
+import org.junit.Test;
+import static org.fest.assertions.Assertions.assertThat;
+
+public class DiffUtilTest {
+
+    String DIFF_DELETE_PREFIX = "<span style='background-color: #fda9a6;padding: 2px 0;'>";
+    String DIFF_DELETE_POSTFIX = "</span>";
+
+    String DIFF_INSERT_PREFIX = "<span style='background-color: #abdd52;padding: 2px 0;'>";
+    String DIFF_INSERT_POSTFIX = "</span>";
+
+    String DIFF_EQUAL_PREFIX = "<span style='color: #bdbdbd;font-size: 16px;font-family: serif;'>...&nbsp<br/>\n"
+            + "......&nbsp<br/>\n"
+            + "......&nbsp<br/>\n"
+            + "...</span>";
+
+    @Test
+    public void getDiffText_oldValueIsNull_returnString() {
+
+        // GIVEN
+        String oldValue = null;
+        String newValue = "new value";
+        String expectedResult = DIFF_INSERT_PREFIX + newValue + DIFF_INSERT_POSTFIX;
+
+        // WHEN
+        String result = DiffUtil.getDiffText(oldValue, newValue);
+
+        // THEN
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void getDiffText_newValueIsNull_returnString() {
+
+        // GIVEN
+        String oldValue = "oldValue";
+        String newValue = null;
+        String expectedResult = DIFF_DELETE_PREFIX + oldValue + DIFF_DELETE_POSTFIX;
+
+        // WHEN
+        String result = DiffUtil.getDiffText(oldValue, newValue);
+
+        // THEN
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void getDiffText_equalValuesMoreThanSize100_returnString() {
+
+        // GIVEN
+        String oldValue = "12345678901234567890123456789012345678901234567890123456789012345678901234567890"
+                + "123456789012345678901234567890";
+        String newValue = "12345678901234567890123456789012345678901234567890123456789012345678901234567890"
+                + "123456789012345678901234567890";
+        int textLength = oldValue.length();
+        String expectedResult = oldValue.substring(0, 50) + DIFF_EQUAL_PREFIX + oldValue.substring(textLength - 50);
+
+        // WHEN
+        String result = DiffUtil.getDiffText(oldValue, newValue);
+
+        // THEN
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void getDiffText_equalValuesLessThanSize100_returnString() {
+
+        // GIVEN
+        String oldValue = "12345678901234567890";
+        String newValue = "12345678901234567890";
+        String expectedResult = oldValue;
+
+        // WHEN
+        String result = DiffUtil.getDiffText(oldValue, newValue);
+
+        // THEN
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void getDiffText_deleteAndInsertValue_returnString() {
+
+        // GIVEN
+        String oldValue = "Hi, there?";
+        String newValue = "Hello, mijeong?";
+        String expectedResult = oldValue.substring(0, 1)
+                + DIFF_DELETE_PREFIX + oldValue.substring(1, oldValue.length() - 1) + DIFF_DELETE_POSTFIX
+                + DIFF_INSERT_PREFIX + newValue.substring(1, newValue.length() - 1) + DIFF_DELETE_POSTFIX
+                + oldValue.substring(oldValue.length() - 1);
+
+        // WHEN
+        String result = DiffUtil.getDiffText(oldValue, newValue);
+
+        // THEN
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+}
