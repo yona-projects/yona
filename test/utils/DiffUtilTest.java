c21984216914656d8d3947e8ae3ae966c199e3d7
Mijeong Park <p.mj@naverlabs.com>
2018-01-28 14:56:58 +0900

    util: Add function for getting diff(Plain format) between two values

--- test/utils/DiffUtilTest.java
+++ test/utils/DiffUtilTest.java
@@ -22,6 +22,16 @@ public class DiffUtilTest {
             + "......&nbsp<br/>\n"
             + "...</span>";
 
+    String DIFF_NEW_LINE = "\n";
+
+    String DIFF_DELETE_PLAIN_PREFIX = "--- ";
+
+    String DIFF_INSERT_PLAIN_PREFIX = "+++ ";
+
+    String DIFF_EQUAL_PLAIN_PREFIX = "......\n"
+            + "......\n"
+            + "...\n";
+
     @Test
     public void getDiffText_oldValueIsNull_returnString() {
 
@@ -103,4 +113,85 @@ public class DiffUtilTest {
         assertThat(result).isEqualTo(expectedResult);
     }
 
+    @Test
+    public void getDiffPlainText_oldValueIsNull_returnString() {
+
+        // GIVEN
+        String oldValue = null;
+        String newValue = "new value";
+        String expectedResult = DIFF_INSERT_PLAIN_PREFIX + newValue + DIFF_NEW_LINE;
+
+        // WHEN
+        String result = DiffUtil.getDiffPlainText(oldValue, newValue);
+
+        // THEN
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void getDiffPlainText_newValueIsNull_returnString() {
+
+        // GIVEN
+        String oldValue = "oldValue";
+        String newValue = null;
+        String expectedResult = DIFF_DELETE_PLAIN_PREFIX + oldValue + DIFF_NEW_LINE;
+
+        // WHEN
+        String result = DiffUtil.getDiffPlainText(oldValue, newValue);
+
+        // THEN
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void getDiffPlainText_equalValuesMoreThanSize100_returnString() {
+
+        // GIVEN
+        String oldValue = "12345678901234567890123456789012345678901234567890123456789012345678901234567890"
+                + "123456789012345678901234567890";
+        String newValue = "12345678901234567890123456789012345678901234567890123456789012345678901234567890"
+                + "123456789012345678901234567890";
+        int textLength = oldValue.length();
+        String expectedResult = oldValue.substring(0, 50) + DIFF_EQUAL_PLAIN_PREFIX
+                + oldValue.substring(textLength - 50) + DIFF_NEW_LINE;
+
+        // WHEN
+        String result = DiffUtil.getDiffPlainText(oldValue, newValue);
+
+        // THEN
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void getDiffPlainText_equalValuesLessThanSize100_returnString() {
+
+        // GIVEN
+        String oldValue = "12345678901234567890";
+        String newValue = "12345678901234567890";
+        String expectedResult = oldValue + DIFF_NEW_LINE;
+
+        // WHEN
+        String result = DiffUtil.getDiffPlainText(oldValue, newValue);
+
+        // THEN
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void getDiffPlainText_deleteAndInsertValue_returnString() {
+
+        // GIVEN
+        String oldValue = "Hi, there?";
+        String newValue = "Hello, mijeong?";
+        String expectedResult = oldValue.substring(0, 1) + DIFF_NEW_LINE
+                + DIFF_DELETE_PLAIN_PREFIX + oldValue.substring(1, oldValue.length() - 1) + DIFF_NEW_LINE
+                + DIFF_INSERT_PLAIN_PREFIX + newValue.substring(1, newValue.length() - 1) + DIFF_NEW_LINE
+                + oldValue.substring(oldValue.length() - 1) + DIFF_NEW_LINE;
+
+        // WHEN
+        String result = DiffUtil.getDiffPlainText(oldValue, newValue);
+
+        // THEN
+        assertThat(result).isEqualTo(expectedResult);
+    }
 }
