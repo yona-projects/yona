b6eb3a57dbb84399ae20199ee932563970abeaa2
Mijeong Park <p.mj@naverlabs.com>
2018-01-31 00:07:34 +0900

    noti: Add method for getting noti message with Plain format

--- app/notification/MergedNotificationEvent.java
+++ app/notification/MergedNotificationEvent.java
@@ -1,23 +1,9 @@
 /**
- * Yobi, Project Hosting SW
- *
- * Copyright 2015 NAVER Corp.
- * http://yobi.io
- *
- * @author Yi EungJun
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ * Yona, 21st Century Project Hosting SW
+ * <p>
+ * Copyright Yona & Yobi Authors & NAVER Corp. & NAVER LABS Corp.
+ * https://yona.io
+ **/
 package notification;
 
 import com.google.common.base.Joiner;
@@ -60,6 +46,15 @@ public class MergedNotificationEvent implements INotificationEvent {
         List<String> messages = new ArrayList<>();
         for(INotificationEvent event : messageSources) {
             messages.add(event.getMessage(lang));
+        }
+        return Joiner.on("\n\n---\n\n").join(messages);
+    }
+
+    @Override
+    public String getPlainMessage(Lang lang) {
+        List<String> messages = new ArrayList<>();
+        for(INotificationEvent event : messageSources) {
+            messages.add(event.getPlainMessage(lang));
         }
         return Joiner.on("\n\n---\n\n").join(messages);
     }
