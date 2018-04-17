7e73ee28eaaec65b2627fa45292c0b09e83010d3
Wonju Jeon <wonju.jeon@naverlabs.com>
2018-04-17 17:10:51 +0900

    editor: Support draft saving feature

--- public/javascripts/common/yobi.CommentForm.js
+++ public/javascripts/common/yobi.CommentForm.js
@@ -1,23 +1,10 @@
 /**
- * Yobi, Project Hosting SW
- *
- * Copyright 2014 NAVER Corp.
- * http://yobi.io
- *
- * @author Jihan Kim
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ * Yona, 21st Century Project Hosting SW
+ * <p>
+ * Copyright Yona & Yobi Authors & NAVER Corp. & NAVER LABS Corp.
+ * https://yona.io
+ **/
+
 $(function(){
 
     "use strict";
@@ -48,6 +35,7 @@ $(function(){
         elements.commentForm.on("submit", onSubmitCommentForm);
         $(window).on("keydown",  onKeydownWindow);
         $(window).on("beforeunload", onBeforeUnloadWindow);
+        temprarySaveHandler(elements.textarea);
     }
 
     /**
@@ -68,6 +56,8 @@ $(function(){
 
         elements.commentForm.data("onsubmit", true);
         NProgress.start();
+
+        removeCurrentPageTemprarySavedContent();
         return true;
     }
 
