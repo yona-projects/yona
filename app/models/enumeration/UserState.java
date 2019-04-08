371666b30a387427064a0e77ca4b49113147f663
Hyeonjae Park <hyunjae.park@naverlabs.com>
2019-04-08 12:55:30 +0900

    admin: Update user state api

--- app/models/enumeration/UserState.java
+++ app/models/enumeration/UserState.java
@@ -1,17 +1,17 @@
 /**
  * Yobi, Project Hosting SW
- *
+ * <p>
  * Copyright 2013 NAVER Corp.
  * http://yobi.io
  *
  * @author Yi EungJun
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,5 +21,24 @@
 package models.enumeration;
 
 public enum UserState {
-    ACTIVE, LOCKED, DELETED, GUEST, SITE_ADMIN
+    ACTIVE("ACTIVE"), LOCKED("LOCKED"), DELETED("DELETED"), GUEST("GUEST"), SITE_ADMIN("SITE_ADMIN");
+
+    private String state;
+
+    UserState(String state) {
+        this.state = state;
+    }
+
+    public String state() {
+        return this.state;
+    }
+
+    public static UserState getValue(String value) {
+        for (UserState userState : UserState.values()) {
+            if (userState.state().equals(value)) {
+                return userState;
+            }
+        }
+        return null;
+    }
 }
