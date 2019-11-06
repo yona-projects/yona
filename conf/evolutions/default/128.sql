37f4ba897ee7a9e0c6b3884f5eb301354f66a6a2
Wonjun Hong <hong.wonjun@gmail.com>
2019-11-06 14:25:51 +0900

    Webhook Thread 를 저장하기 위한 Table 구성 Query

---
+++ conf/evolutions/default/128.sql
@@ -0,0 +1,20 @@
+# --- !Ups
+CREATE TABLE webhook_thread (
+  id                        BIGINT AUTO_INCREMENT NOT NULL,
+  webhook_id                BIGINT,
+  resource_type             VARCHAR(20),
+  resource_id               VARCHAR(255),
+  thread_id                 VARCHAR(2000),
+  created_at                DATETIME,
+  CONSTRAINT ck_webhook_thread_resource_type CHECK (resource_type IN ('ISSUE','BOARD','PULL_REQUEST')),
+  CONSTRAINT pk_webhook_thread PRIMARY KEY (id),
+  CONSTRAINT fk_webhook_thread_webhook FOREIGN KEY (webhook_id) REFERENCES webhook (id) ON DELETE CASCADE
+  )
+  row_format=compressed, key_block_size=8
+;
+
+CREATE index ix_webhook_thread_webhook_1 ON webhook_thread (webhook_id);
+CREATE index ix_webhook_thread_resource_2 ON webhook_thread (resource_type, resource_id);
+
+# --- !Downs
+DROP TABLE webhook_thread;
