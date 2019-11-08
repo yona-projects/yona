88dcc67c03f6747fbf3644ddb2fdb751c509061e
Wonjun Hong <hong.wonjun@gmail.com>
2019-11-08 10:47:09 +0900

    버전업시 기존에 이미 디비에 입력된 케이스로 오류가 나기 때문에 해당 파일은 추후 해당 문제를 해결할 수 있는 Query 를 추가하여 다시 Commit 할 예정

--- conf/evolutions/default/28.sql
+++
@@ -1,5 +0,0 @@
-# --- !Ups
-ALTER TABLE project_transfer ADD uq_project_transfer_1 UNIQUE (sender_id, destination, project_id);
-
-# --- !Downs
-ALTER TABLE project_transfer DROP INDEX uq_project_transfer_1
