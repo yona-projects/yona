aeb41d7e08b723d5298adf1052a2318781da8fb6
Suwon Chae <doortts@gmail.com>
2020-07-08 12:26:46 +0900

    fixup! issue: Show last modified time and editor

--- conf/evolutions/default/130.sql
+++ conf/evolutions/default/130.sql
@@ -1,5 +1,8 @@
 # --- !Ups
 ALTER TABLE issue ADD COLUMN updated_by_author_id bigint;
+ALTER TABLE posting ADD COLUMN updated_by_author_id bigint;
 
 # --- !Downs
 ALTER TABLE issue DROP COLUMN updated_by_author_id;
+ALTER TABLE posting DROP COLUMN updated_by_author_id;
+
