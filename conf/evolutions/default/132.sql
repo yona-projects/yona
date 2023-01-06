b7815e0bddec25ccf8d8b587d9783d490d2f5d04
Suwon Chae <sw.chae@naverlabs.com>
2023-01-06 20:27:59 +0900

    sql: Modify index

--- conf/evolutions/default/132.sql
+++ conf/evolutions/default/132.sql
@@ -1,8 +1,10 @@
 # --- !Ups
 ALTER TABLE issue ADD COLUMN is_draft tinyint(1) default 0;
 CREATE index ix_issue_is_draft_1 ON issue (weight, is_draft, number, created_date);
+CREATE index ix_issue_is_draft_2 ON issue (is_draft, author_login_id, project_id);
 
 # --- !Downs
-drop index ix_issue_is_draft_1 on issue;
+drop index IF EXISTS ix_issue_is_draft_1;
+drop index IF EXISTS ix_issue_is_draft_2;
 ALTER TABLE issue DROP COLUMN is_draft;
 
